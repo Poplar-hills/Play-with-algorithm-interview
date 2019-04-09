05d6c59b58a6d5bf36a8a38130e60fd644730bdb
Max Jiang <maxjiang23326@gmail.com>
2019-04-09 23:05:42 +0800

    feat: add 2nd solution to L283

--- src/main/java/Array/L283_MoveZeroes.java
+++ src/main/java/Array/L283_MoveZeroes.java
@@ -1,85 +1,55 @@
 package Array;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static Utils.Helpers.log;
-import static Utils.Helpers.swap;
 
 public class L283_MoveZeroes {
-    public static void moveZerosV1(int[] arr) {
-        int[] aux = new int[arr.length];
+    public static void moveZerosV1(int[] arr) {  // 解法1: 时间复杂度为 O(n)，空间复杂度为 O(n)
+        int[] aux = new int[arr.length];  // 辅助空间
 
         int j = 0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] != 0)
-                aux[j++] = arr[i];
+                aux[j++] = arr[i];  // 将非零元素顺序放入辅助空间中，后面未放值的位置默认为零
         }
 
         for (int i = 0; i < arr.length; i++)
             arr[i] = aux[i];
     }
 
-    public static void moveZerosV2(int[] arr) {
-//        int i = 0;  // zero element index
-//        int j = 0;  // non-zero element index
-//        while (j < arr.length) {
-//            if (arr[i] == 0) {
-//                while (j < arr.length && arr[j] == 0)
-//                    j++;
-//                if (j < arr.length)
-//                    swap(arr, i, j);
-//            }
-//            i++;
-//        }
+    public static void moveZerosV2(int[] arr) {  // 解法2：时间复杂度还是 O(n)，空间复杂度 O(1)
+        int j = 0;
 
-        int i = 0, firstZeroIdx = -1, nonZeroIdx = -1;
-        while (i < arr.length) {
-            if (arr[i] == 0)
-            i++;
-        }
+        for (int i = 0; i < arr.length; i++)  // 将所有非零元素放到前面
+            if (arr[i] != 0)
+                arr[j++] = arr[i];
+
+        for (int i = j; i < arr.length; i++)  // 后面补零
+            arr[i] = 0;
     }
 
-    /*
-    *    0, 1, 4, 0, 0, 0, 3, 8   init
-    * nf
-    *    0, 1, 4, 0, 0, 0, 3, 8   i = 0
-    * n  f
-    *    0, 1, 4, 0, 0, 0, 3, 8   i = 1
-    *    f  n
-    *    1, 0, 4, 0, 0, 0, 3, 8
-    *    n  f
-    *    1, 0, 4, 0, 0, 0, 3, 8   i = 2
-    *       f  n
-    *    1, 4, 0, 0, 0, 0, 3, 8
-    *       n  f
-    *    1, 4, 0, 0, 0, 0, 3, 8   i = 3
-    *       n  f
-    *    ...
-    *    1, 4, 0, 0, 0, 0, 3, 8   i = 5
-    *       n  f
-    *    1, 4, 0, 0, 0, 0, 3, 8   i = 6
-    *          f           n
-    *    1, 4, 3, 0, 0, 0, 0, 8
-    *          n  f
-    *    1, 4, 3, 0, 0, 0, 0, 8   i = 7
-    *             f           n
-    *    1, 4, 3, 8, 0, 0, 0, 0
-    *             n  f
-    * */
-
     public static void main(String[] args) {
+//        int[] arr1 = new int[] {0, 1, 4, 0, 0, 0, 3, 8};
+//        moveZerosV1(arr1);
+//        log(arr1);
+//
+//        int[] arr2 = new int[] {0, 1};
+//        moveZerosV1(arr2);
+//        log(arr2);
+//
+//        int[] arr3 = new int[] {1, 0};
+//        moveZerosV1(arr3);
+//        log(arr3);
+
         int[] arr1 = new int[] {0, 1, 4, 0, 0, 0, 3, 8};
-        moveZerosV1(arr1);
+        moveZerosV2(arr1);
         log(arr1);
 
         int[] arr2 = new int[] {0, 1};
-        moveZerosV1(arr2);
+        moveZerosV2(arr2);
         log(arr2);
 
         int[] arr3 = new int[] {1, 0};
-        moveZerosV1(arr3);
+        moveZerosV2(arr3);
         log(arr3);
     }
-
 }
