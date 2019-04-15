eed0621bd13cf98c919d76ac528c8159f296dac5
Max Jiang <maxjiang23326@gmail.com>
2019-04-15 13:34:36 +0800

    feat: add a solution to L215

--- src/main/java/Array/L215_LargestElementInArray.java
+++ src/main/java/Array/L215_LargestElementInArray.java
@@ -1,4 +1,25 @@
 package Array;
 
+import java.util.PriorityQueue;
+
+import static Utils.Helpers.log;
+
 public class L215_LargestElementInArray {
+    public static int kthLargest1(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(k + 1);
+        for (int n : nums) {
+            pq.add(n);
+            if (pq.size() > k)
+                pq.poll();
+        }
+        return pq.poll();
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = new int[] {3, 2, 1, 5, 6, 4};
+        log(kthLargest1(arr1, 2));  // expects 5
+
+        int[] arr2 = new int[] {3, 2, 3, 1, 2, 4, 5, 5, 6};
+        log(kthLargest1(arr2, 4));  // expects 4
+    }
 }
