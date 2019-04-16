79824921979b99bee79b62cfcb1442442fa4090c
Max Jiang <maxjiang23326@gmail.com>
2019-04-16 08:04:47 +0800

    feat: add another solution for L167

--- src/main/java/Array/L167_TowSumII.java
+++ src/main/java/Array/L167_TowSumII.java
@@ -13,9 +13,13 @@ import static Utils.Helpers.log;
 * */
 
 public class L167_TowSumII {
+    /*
+    * 解法一：二分查找思路
+    * - 遍历一边数组，每次遍历进行一次二分查找，因此总体是 O(nlogn)。
+    * */
     public static int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
-            int p = binarySearch(nums, target - nums[i], i + 1, nums.length - 1);
+            int p = binarySearch(nums, target - nums[i], i + 1, nums.length - 1);  // 在后面的元素中二分查找 target - nums[i]
             if (p != -1) return new int[] {i + 1, p + 1};
         }
         return null;
@@ -29,10 +33,31 @@ public class L167_TowSumII {
         return mid;
     }
 
+    /*
+    * 解法2：指针对撞
+    * - 只需遍历一次，因此复杂度为 O(n)
+    * - 两路、三路快排实际上也是指针对撞的一种应用
+    * */
+    public static int[] twoSum2(int[] nums, int target) {
+        int i = 0, j = nums.length - 1;
+        while (i != j) {
+            int sum = nums[i] + nums[j];
+            if (sum < target) i++;
+            else if (sum > target) j--;
+            else return new int[] {i + 1, j + 1};
+        }
+        return null;
+    }
+
     public static void main(String[] args) {
         int[] arr1 = new int[] {2, 7, 11, 15};
-        int[] arr2 = new int[] {3, 2, 4};  // 注意 target 不能是重复元素相加
+        int[] arr2 = new int[] {2, 3, 4};  // 注意 target 不能是重复元素相加
         log(twoSum(arr1, 9));       // expects [1, 2]（注意返回的得是索引+1）
-        log(twoSum(arr2, 6));       // expects [2, 3]
+        log(twoSum(arr2, 6));       // expects [1, 3]
+
+        int[] arr3 = new int[] {2, 7, 11, 15};
+        int[] arr4 = new int[] {2, 3, 4};
+        log(twoSum2(arr3, 9));       // expects [1, 2]（注意返回的得是索引+1）
+        log(twoSum2(arr4, 6));       // expects [2, 3]
     }
 }
