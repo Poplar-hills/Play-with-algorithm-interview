9d33919711309ef27c768e9f5d60fe3b5de30c93
Max Jiang <maxjiang23326@gmail.com>
2019-04-22 08:37:58 +0800

    feat: add yet another solution for L349

--- src/main/java/LookUp/L349_IntersectionOfTwoArrays.java
+++ src/main/java/LookUp/L349_IntersectionOfTwoArrays.java
@@ -1,7 +1,6 @@
 package LookUp;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 import static Utils.Helpers.log;
 
@@ -43,11 +42,38 @@ public class L349_IntersectionOfTwoArrays {
         return res;
     }
 
+    public static int[] intersection3(int[] nums1, int[] nums2) {  // 解法3：二分查找，O(nlogn)
+        Set<Integer> set = new HashSet<>();
+        Arrays.sort(nums2);   // 要先排序才能对 nums2 进行二分查找，O(nlogn)
+
+        for (int n : nums1)   // 再进行 n 次 O(logn) 的查找
+            if (binarySearch(nums2, 0, nums2.length - 1, n))
+                set.add(n);
+
+        int i = 0;
+        int[] res = new int[set.size()];
+        for (int n : set) res[i++] = n;
+        return res;
+    }
+
+    private static boolean binarySearch(int[] arr, int l, int r, int n) {
+        if (l > r) return false;
+        int mid = (r - l) / 2 + l;
+        if (n > arr[mid])
+            return binarySearch(arr, mid + 1, r, n);
+        else if (n < arr[mid])
+            return binarySearch(arr, l, mid - 1, n);
+        return true;
+    }
+
     public static void main(String[] args) {
         log(intersection(new int[] {1, 2, 2, 1}, new int[] {2, 2}));
         log(intersection(new int[] {4, 9, 5}, new int[] {9, 4, 9, 8, 4}));
 
         log(intersection2(new int[] {1, 2, 2, 1}, new int[] {2, 2}));
         log(intersection2(new int[] {4, 9, 5}, new int[] {9, 4, 9, 8, 4}));
+
+        log(intersection3(new int[] {1, 2, 2, 1}, new int[] {2, 2}));
+        log(intersection3(new int[] {4, 9, 5}, new int[] {9, 4, 9, 8, 4}));
     }
 }
