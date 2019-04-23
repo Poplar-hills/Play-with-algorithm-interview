c2aa7e52c71007a2487ce9ad6dce5e8e9cb01684
Max Jiang <maxjiang23326@gmail.com>
2019-04-23 12:43:11 +0800

    feat: add 3 solution for L242

--- src/main/java/LookUp/L242_ValidAnagram.java
+++ src/main/java/LookUp/L242_ValidAnagram.java
@@ -1,9 +1,9 @@
 package LookUp;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 import static Utils.Helpers.log;
 
@@ -14,7 +14,7 @@ import static Utils.Helpers.log;
 * */
 
 public class L242_ValidAnagram {
-    public static boolean isAnagram(String s, String t) {  // 解法1
+    public static boolean isAnagram(String s, String t) {  // 解法1：使用 map，时间复杂度 O(n)，空间复杂度 O(n)
         if (t.length() != s.length())
             return false;
 
@@ -34,22 +34,32 @@ public class L242_ValidAnagram {
         return map.isEmpty();
     }
 
-    public static boolean isAnagram2(String s, String t) {  // 解法2
+    public static boolean isAnagram2(String s, String t) {  // 解法2：使用 map，时间复杂度 O(n)，空间复杂度 O(n)
         if (t.length() != s.length())
             return false;
 
         Map<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < s.length(); i++) {  // O(n)
+        for (int i = 0; i < s.length(); i++) {  // O(n)，因为之前已经处理过长度不等的情况，因此这里可以一次遍历处理两个字符串
             char sc = s.charAt(i);
-            map.put(sc, map.getOrDefault(sc, 0) + 1);
+            map.put(sc, map.getOrDefault(sc, 0) + 1);  // 一个加
             char tc = t.charAt(i);
-            map.put(tc, map.getOrDefault(tc, 0) - 1);
+            map.put(tc, map.getOrDefault(tc, 0) - 1);  // 一个减
         }
 
-        for (int n : map.values())  // O(n)
+        for (int n : map.values())  // O(n)，这里需要遍历 map
             if (n != 0)
                 return false;
         return true;
+    }
+
+    public static boolean isAnagram3(String s, String t) {  // 解法3：排序，时间复杂度 O(nlogn)，空间复杂度 O(1)
+        if (t.length() != s.length())
+            return false;
+        char[] sArr = s.toCharArray();
+        char[] tArr = t.toCharArray();
+        Arrays.sort(sArr);
+        Arrays.sort(tArr);
+        return Arrays.equals(sArr, tArr);
     }
 
     public static void main(String[] args) {
