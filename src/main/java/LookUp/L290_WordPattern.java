c5a4c90b1ddc0e5a5cd20e0cac247fa421083c16
Max Jiang <maxjiang23326@gmail.com>
2019-04-25 10:54:18 +0800

    feat: add another solution for L290

--- src/main/java/LookUp/L290_WordPattern.java
+++ src/main/java/LookUp/L290_WordPattern.java
@@ -40,12 +40,30 @@ public class L290_WordPattern {
         return true;
     }
 
+    /*
+     * 解法2：使用单 map，比较 pattern 中的字符和 str 中的 word 上次在 map 中出现的索引是否相等。
+     * - 时间复杂度 O(n)，空间复杂度 O(n)。
+     * - map.put(a, b) 方法返回更新之前 a 的值。该解法虽然更简洁，但语义不如解法1清晰。
+     * */
+    public static boolean wordPattern2(String pattern, String str) {
+        String[] words = str.split(" ");
+        if (words.length != pattern.length())
+            return false;
+
+        Map map = new HashMap();
+        for (Integer i = 0; i < words.length; i++)
+            if (map.put(pattern.charAt(i), i) != map.put(words[i], i))
+                return false;
+
+        return true;
+    }
+
     public static void main(String[] args) {
-        log(wordPattern("abba", "dog cat cat dog"));   // true
-        log(wordPattern("abba", "dog cat cat fish"));  // false
-        log(wordPattern("aaaa", "dog cat cat dog"));   // false
-        log(wordPattern("aaaa", "dog dog dog dog"));   // true
-        log(wordPattern("abba", "dog dog dog dog"));   // false
-        log(wordPattern("jquery", "jquery"));          // false
+        log(wordPattern2("abba", "dog cat cat dog"));   // true
+        log(wordPattern2("abba", "dog cat cat fish"));  // false
+        log(wordPattern2("aaaa", "dog cat cat dog"));   // false
+        log(wordPattern2("aaaa", "dog dog dog dog"));   // true
+        log(wordPattern2("abba", "dog dog dog dog"));   // false
+        log(wordPattern2("jquery", "jquery"));          // false
     }
 }
