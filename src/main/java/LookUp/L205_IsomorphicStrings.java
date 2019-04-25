c6facec9fcd7c68afa4bf63c7766935bde0b74cd
Max Jiang <maxjiang23326@gmail.com>
2019-04-25 10:58:35 +0800

    feat: add problem L205

---
+++ src/main/java/LookUp/L205_IsomorphicStrings.java
@@ -0,0 +1,21 @@
+package LookUp;
+
+import static Utils.Helpers.log;
+
+/*
+* Isomorphic Strings
+*
+* - 判断两个字符串是否同构（isomorphic）。
+* */
+
+public class L205_IsomorphicStrings {
+    public boolean isIsomorphic(String s, String t) {
+
+    }
+
+    public static void main(String[] args) {
+        log(isIsomorphic("egg", "add"));      // expects true
+        log(isIsomorphic("foo", "bar"));      // expects false
+        log(isIsomorphic("paper", "title"));  // expects true
+    }
+}
