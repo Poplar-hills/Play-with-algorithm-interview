fd782049d1738ce0e73161662b321064d669e5f2
Max Jiang <maxjiang23326@gmail.com>
2019-04-25 19:24:02 +0800

    feat: add another solution for L205

--- src/main/java/LookUp/L205_IsomorphicStrings.java
+++ src/main/java/LookUp/L205_IsomorphicStrings.java
@@ -10,8 +10,9 @@ import static Utils.Helpers.log;
 
 public class L205_IsomorphicStrings {
     /*
-    * 解法1
-    * - 使用两个 map，分别记录 s[i] -> t[i] 和 t[i] -> s[i] 的映射关系，之后在遍历字符串过程中检查每个字符的映射关系是否与之前记录的映射关系一致。
+    * 解法1：双 map
+    * - 思路：判断两个字符串是否是 isomorphic 的，只需判断两者中各字符的相对位置是否相同，因此需要建立 s 中的字符 -> t 中的字符的映射。
+    * - 实现：两个 map 分别记录 s[i] -> t[i] 和 t[i] -> s[i] 的映射关系，之后在遍历字符串过程中检查每个字符的映射关系是否与之前记录的映射关系一致。
     * - 时间复杂度 O(n)，空间复杂度 O(2len(charset))。
     * */
     public static boolean isIsomorphic(String s, String t) {
@@ -27,17 +28,38 @@ public class L205_IsomorphicStrings {
                 sMap[sc] = tc;
                 tMap[tc] = sc;
             }
-            else if (sMap[sc] != tc || tMap[tc] != sc)  // 若 sc 和 tc 在两个 map 中已有记录，但映射关系与之前记录的不一致则说明不是 isomorphic
+            else if (sMap[sc] != tc || tMap[tc] != sc)  // 若已有记录，但映射关系与之前记录的不一致，则说明不是 isomorphic 的
                 return false;
         }
 
         return true;
     }
-g
+
+    /*
+     * 解法2：单 map
+     * - 思路：对于 s 和 t 中的每个字符（如 s[i], t[i]）来说，只需要知道他们上次出现时的索引相同即可判断 s 和 t 是 isomorphic 的。
+     * - 实现：一个 map 分成两部分使用，每部分分别记录 s[i] -> i 和 t[i] -> i 的映射。
+     * - 时间复杂度 O(n)，空间复杂度 O(2len(charset))。
+     * */
+    public static boolean isIsomorphic2(String s, String t) {
+        int[] map = new int[256];
+        for (int i = 0; i < s.length(); i++) {
+            if (map[s.charAt(i)] != map[t.charAt(i) + 128])
+                return false;
+            map[s.charAt(i)] = map[t.charAt(i) + 128] = i + 1;  // i+1 是因为要避免0，因为 int[] 的默认值是0
+        }
+        return true;
+    }
 
     public static void main(String[] args) {
         log(isIsomorphic("egg", "add"));      // expects true
-        log(isIsomorphic("foo", "bar"));      // expects false
         log(isIsomorphic("paper", "title"));  // expects true
+        log(isIsomorphic("foo", "bar"));      // expects false
+        log(isIsomorphic("ab", "aa"));        // expects false
+
+        log(isIsomorphic2("egg", "add"));      // expects true
+        log(isIsomorphic2("paper", "title"));  // expects true
+        log(isIsomorphic2("foo", "bar"));      // expects false
+        log(isIsomorphic2("ab", "aa"));        // expects false
     }
 }
