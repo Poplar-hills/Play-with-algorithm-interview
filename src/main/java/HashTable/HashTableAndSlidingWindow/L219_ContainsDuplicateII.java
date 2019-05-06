7d6aa1fb5b1671a495cd675a0c1fa2c477da29d9
Max Jiang <maxjiang23326@gmail.com>
2019-05-06 17:09:42 +0800

    feat: add yet another solution for L219

--- src/main/java/HashTable/HashTableAndSlidingWindow/L219_ContainsDuplicateII.java
+++ src/main/java/HashTable/HashTableAndSlidingWindow/L219_ContainsDuplicateII.java
@@ -1,7 +1,9 @@
 package HashTable.HashTableAndSlidingWindow;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import static Utils.Helpers.log;
 
@@ -45,7 +47,7 @@ public class L219_ContainsDuplicateII {
 
     /*
      * 解法2：
-     * - 思路：查找表（利用 Java 里 Map 的 put 方法）
+     * - 思路：查找表 Map
      *   - Map 中存储元素 -> 索引的映射。
      *   - put 方法返回该 key 更新之前的 value，若之前 key 不存在则返回 null。利用这一点，在每次取到新元素时，用 put 存储该元素，
      *     并查询上次存储该元素的索引，若索引存且与新元素索引之差小于 k，则 return true。
@@ -61,11 +63,27 @@ public class L219_ContainsDuplicateII {
         return false;
     }
 
+    /*
+    * 解法3：查找表 Set
+    * - 思路：创建一个 Set，只保存 i - k 范围内的元素。
+    * - 时间复杂度 O(n)，空间复杂度 O(n)。
+    * */
+    public static boolean containsNearbyDuplicate3(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) return true;
+            set.add(nums[i]);
+            if (set.size() == k + 1)  // set 中元素个数达到 k+1 之前只添加，不删除
+                set.remove(nums[i - k]);
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
-        log(containsNearbyDuplicate2(new int[] {1, 0, 1, 1}, 1));        // expects true
-        log(containsNearbyDuplicate2(new int[] {4, 1, 2, 3, 1}, 3));     // expects true
-        log(containsNearbyDuplicate2(new int[] {1, 2, 3, 1, 2, 3}, 2));  // expects false
-        log(containsNearbyDuplicate2(new int[] {1}, 1));                 // expects false
-        log(containsNearbyDuplicate2(new int[] {99, 99}, 2));            // expects true
+        log(containsNearbyDuplicate3(new int[] {1, 0, 1, 1}, 1));        // expects true
+        log(containsNearbyDuplicate3(new int[] {4, 1, 2, 3, 1}, 3));     // expects true
+        log(containsNearbyDuplicate3(new int[] {1, 2, 3, 1, 2, 3}, 2));  // expects false
+        log(containsNearbyDuplicate3(new int[] {1}, 1));                 // expects false
+        log(containsNearbyDuplicate3(new int[] {99, 99}, 2));            // expects true
     }
 }
