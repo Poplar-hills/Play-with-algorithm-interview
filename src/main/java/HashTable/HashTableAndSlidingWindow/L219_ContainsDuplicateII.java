8482be57e0a75fc60e6c6edb671c7eeac48cb750
Max Jiang <maxjiang23326@gmail.com>
2019-05-06 20:59:23 +0800

    docs: add comments

--- src/main/java/HashTable/HashTableAndSlidingWindow/L219_ContainsDuplicateII.java
+++ src/main/java/HashTable/HashTableAndSlidingWindow/L219_ContainsDuplicateII.java
@@ -45,35 +45,35 @@ public class L219_ContainsDuplicateII {
     }
 
     /*
-     * 解法2：查找表 Map
-     * - 思路：
-     *   - 本题的关键点是重复元素的索引要在 k 之内，因此可以使用 Map 存储元素 -> 索引的映射。
-     *   - 再利用 put 方法返回该 key 更新之前的 value，若之前 key 不存在则返回 null。利用这一点，在每次取到新元素时，
-     *     用 put 存储该元素，并查询上次存储该元素的索引，若索引存且与新元素索引之差小于 k，则 return true。
+     * 解法2：查找表 + 滑动窗口
+     * - 思路：使用 Set 既作为查找表（回答有没有的问题用 Set 即可）同时也作为滑动窗口（其中只保存 i - k 范围内的元素）。
      * - 时间复杂度 O(n)，空间复杂度 O(n)。
      * */
     public static boolean containsNearbyDuplicate2(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            Integer index = map.put(nums[i], i);
-            if (index != null && i - index <= k)
-                return true;
-        }
-        return false;
-    }
-
-    /*
-    * 解法3：查找表 Set
-    * - 思路：创建一个 Set，只保存 i - k 范围内的元素。
-    * - 时间复杂度 O(n)，空间复杂度 O(n)。
-    * */
-    public static boolean containsNearbyDuplicate3(int[] nums, int k) {
         Set<Integer> set = new HashSet<>();
         for (int i = 0; i < nums.length; i++) {
             if (set.contains(nums[i])) return true;
             set.add(nums[i]);
             if (set.size() == k + 1)  // set 中元素个数达到 k+1 之前只添加，不删除
                 set.remove(nums[i - k]);
+        }
+        return false;
+    }
+
+    /*
+     * 解法3：查找表 Map
+     * - 思路：
+     *   - 本题的关键点是重复元素的索引要在 k 之内，因此可以使用 Map 存储元素 -> 索引的映射。
+     *   - 再利用 put 方法返回该 key 更新之前的 value，若之前 key 不存在则返回 null。利用这一点，在每次取到新元素时，
+     *     用 put 存储该元素，并查询上次存储该元素的索引，若索引存且与新元素索引之差小于 k，则 return true。
+     * - 时间复杂度 O(n)，空间复杂度 O(n)。
+     * */
+    public static boolean containsNearbyDuplicate3(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            Integer index = map.put(nums[i], i);
+            if (index != null && i - index <= k)
+                return true;
         }
         return false;
     }
