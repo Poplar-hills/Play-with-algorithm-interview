4f537b978266ef71aead609d00796b36df2a8114
Max Jiang <maxjiang23326@gmail.com>
2019-05-08 11:01:20 +0800

    feat: add a solution for L206

---
+++ src/main/java/LinkedList/L206_ReverseLinkedList.java
@@ -0,0 +1,60 @@
+package LinkedList;
+
+/*
+* Reverse Linked List
+*
+* -
+* */
+
+import static Utils.Helpers.log;
+
+public class L206_ReverseLinkedList {
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+    /*
+    * 解法1：遍历
+    * - 时间复杂度 O(n)，空间复杂度 O(1)
+    * */
+    public static ListNode reverseList(ListNode head) {
+        if (head == null) return null;
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+    
+    public static void printLinkedList(ListNode head) {
+        StringBuilder s = new StringBuilder();
+        while (head != null) {
+            s.append(head.val);
+            s.append("->");
+            head = head.next;
+        }
+        s.append("NULL");
+        log(s.toString());
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        ListNode n2 = new ListNode(2);
+        ListNode n3 = new ListNode(3);
+        ListNode n4 = new ListNode(4);
+        ListNode n5 = new ListNode(5);
+        head.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+        printLinkedList(head);  // 1->2->3->4->5->NULL
+
+        printLinkedList(reverseList(head));  // expects 5->4->3->2->1->NULL
+    }
+}
