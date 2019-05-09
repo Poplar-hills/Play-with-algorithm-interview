c543cc50b7189f5b1ab10e062c3f3452911749f8
Max Jiang <maxjiang23326@gmail.com>
2019-05-09 08:21:10 +0800

    feat: add a solution for L92

--- src/main/java/LinkedList/L92_ReverseLinkedListII.java
+++ src/main/java/LinkedList/L92_ReverseLinkedListII.java
@@ -9,9 +9,42 @@ import static Utils.Helpers.*;
 * */
 
 public class L92_ReverseLinkedListII {
-    public static ListNode reverseBetween(ListNode head, int m, int n) {
-        return head;
+    /*
+    * 解法1：双指针节点交换
+    * */
+    public static class solution1 {
+        private ListNode left;
+        private boolean stop;
+
+        public ListNode reverseBetween(ListNode head, int m, int n) {
+            this.left = head;
+            this.stop = false;
+            recurseAndReverse(head, m, n);
+            return head;
+        }
+
+        private void recurseAndReverse(ListNode right, int m, int n) {
+            if (n == 1) return;
+            if (m > 1) this.left = this.left.next;
+            right = right.next;
+
+            recurseAndReverse(right, m - 1, n - 1);
+
+            if (this.left == right || right.next == this.left)
+                this.stop = true;
+            if (!this.stop) {
+                swapNode(this.left, right);
+                this.left = this.left.next;
+            }
+        }
+
+        private void swapNode(ListNode left, ListNode right) {
+            int temp = left.val;
+            this.left.val = right.val;
+            right.val = temp;
+        }
     }
+
 
     public static void main(String[] args) {
         ListNode head = new ListNode(1);
@@ -25,7 +58,8 @@ public class L92_ReverseLinkedListII {
         n4.next = n5;
         printLinkedList(head);  // 1->2->3->4->5->NULL
 
-        printLinkedList(reverseBetween(head, 2, 4));  // 1->4->3->2->5->NULL
+        ListNode reversed = new solution1().reverseBetween(head, 2, 4);
+        printLinkedList(reversed);  // 1->4->3->2->5->NULL
 
     }
 }
