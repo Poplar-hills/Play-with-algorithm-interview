ee7c7a0f2bfafa85ba94212ef3627aff187a796e
Max Jiang <maxjiang23326@gmail.com>
2019-05-13 12:55:19 +0800

    feat: add a solution for L86

--- src/main/java/LinkedList/L86_PartitionList.java
+++ src/main/java/LinkedList/L86_PartitionList.java
@@ -13,11 +13,46 @@ import static Utils.Helpers.*;
 
 public class L86_PartitionList {
     public static ListNode partition(ListNode head, int x) {
-        return head;
+        ListNode dummyhead = new ListNode();
+        dummyhead.next = head;
+
+        ListNode joint = dummyhead, curr = head;
+        while (curr != null && curr.val < x) {
+            joint = curr;
+            curr = curr.next;
+        }
+
+        ListNode prev = joint;
+        while (curr != null) {
+            if (curr.val < x) {
+                ListNode temp = curr.next;
+                joint = insertNode(curr, joint);
+                prev.next = temp;
+                curr = temp;
+            } else {
+                prev = curr;
+                curr = curr.next;
+            }
+        }
+
+        return dummyhead.next;
+    }
+
+    private static ListNode insertNode(ListNode curr, ListNode p) {
+        ListNode third = p.next;
+        p.next = curr;
+        curr.next = third;
+        return curr;
     }
 
     public static void main(String[] args) {
         ListNode l1 = createLinkedListFromArray(new int[]{1, 4, 3, 2, 5, 2});
         printLinkedList(partition(l1, 3));  // expects 1->2->2->4->3->5
+
+        ListNode l2 = createLinkedListFromArray(new int[]{2, 1});
+        printLinkedList(partition(l2, 2));  // expects 1->2
+
+        ListNode l3 = createLinkedListFromArray(new int[]{2, 4, 1, 0, 3});
+        printLinkedList(partition(l3, 2));  // expects 1->0->2->4->3
     }
 }
