daf651afe71d8da23248434102b9223acc009b4e
Max Jiang <maxjiang23326@gmail.com>
2019-05-15 15:38:05 +0800

    feat: add yet another solution for L203

--- src/main/java/LinkedList/DummyHead/L203_RemoveLinkedListElements.java
+++ src/main/java/LinkedList/DummyHead/L203_RemoveLinkedListElements.java
@@ -13,6 +13,7 @@ import static Utils.Helpers.*;
 public class L203_RemoveLinkedListElements {
     /*
     * 解法1：创建新链表
+    * - 时间复杂度 O(n)，空间复杂度 O(n)。
     * */
     public static ListNode removeElements(ListNode head, int val) {
         ListNode dummyHead = new ListNode();
@@ -28,15 +29,16 @@ public class L203_RemoveLinkedListElements {
 
     /*
      * 解法2：修改原有链表
+     * - 时间复杂度 O(n)，空间复杂度 O(1)。
      * */
     public static ListNode removeElements2(ListNode head, int val) {
         ListNode dummyHead = new ListNode();
         dummyHead.next = head;
-
         ListNode prev = dummyHead, curr = head;
+
         while (curr != null) {
             if (curr.val == val)
-                prev.next = curr.next;
+                prev.next = curr.next;  // 跳过值为 val 的节点
             else
                 prev = prev.next;
             curr = curr.next;
@@ -45,8 +47,27 @@ public class L203_RemoveLinkedListElements {
         return dummyHead.next;
     }
 
+    /*
+    * 解法3：解法2的优化版（只是用 curr 一个变量）
+    * - 时间复杂度 O(n)，空间复杂度 O(1)。
+    * */
+    public static ListNode removeElements3(ListNode head, int val) {
+        ListNode dummyHead = new ListNode();
+        dummyHead.next = head;
+        ListNode curr = dummyHead;
+
+        while (curr.next != null) {
+            if (curr.next.val == val)
+                curr.next = curr.next.next;
+            else
+                curr = curr.next;
+        }
+
+        return dummyHead.next;
+    }
+
     public static void main(String[] args) {
         ListNode l = createLinkedListFromArray(new int[]{1, 2, 6, 3, 4, 5, 6});
-        printLinkedList(removeElements2(l, 6));
+        printLinkedList(removeElements3(l, 6));
     }
 }
