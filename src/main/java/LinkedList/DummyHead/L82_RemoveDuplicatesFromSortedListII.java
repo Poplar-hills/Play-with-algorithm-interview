1bf51fa62d73855a9603dc0795b7e2ea5c0e7ee9
Max Jiang <maxjiang23326@gmail.com>
2019-05-16 12:29:31 +0800

    docs: better explanation

--- src/main/java/LinkedList/DummyHead/L82_RemoveDuplicatesFromSortedListII.java
+++ src/main/java/LinkedList/DummyHead/L82_RemoveDuplicatesFromSortedListII.java
@@ -13,22 +13,19 @@ import static Utils.Helpers.*;
 public class L82_RemoveDuplicatesFromSortedListII {
     /*
     * 解法1：Intuitive solution
-    * - 思路：对于 1->2->3->3->4->4->5 来说，若想将去除 3->3，则需要链接3之前的2节点和第一个4节点，因此这两个节点需要有指针指向才行。
-    *        因此设计两个指针（分别指向重复节点之前和之后的节点）+ 一个标志位（标志当前是否发现了重复节点）：
-    *        D -> 1 -> 2 -> 3 -> 3 -> 4 -> 4 -> 5
-    *        a    b                                 此时 b.val != b.next.val，此时 a, b 向后移动
-    *        D -> 1 -> 2 -> 3 -> 3 -> 4 -> 4 -> 5
-    *             a    b                            此时 b.val != b.next.val，此时 a, b 向后移动
-    *        D -> 1 -> 2 -> 3 -> 3 -> 4 -> 4 -> 5
-    *                  a    b                       此时 b.val != b.next.val，此时 a, b 向后移动
-    *        D -> 1 -> 2 -> 3 -> 3 -> 4 -> 4 -> 5
-    *                  a         b                  此时 b.val == b.next.val，此时 a 不动，b 向后移动，标志位置为 true
-    *        D -> 1 -> 2 -> 3 -> 3 -> 4 -> 4 -> 5
-    *                  a              b             此时 b.val != b.next.val，此时 a 不动，b 向后移动，标志位置为 false，将 a.next 指向 b
-    *        D -> 1 -> 2 -> 4 -> 4 -> 5
-    *                  a    b                       继续遍历直到 b.next == null 结束
-    * - 特殊情况：D -> 1 -> 1，因为此时重复节点后面没有更多节点，无法完成
-    * */
+    * - 思路：对于 2->3->3->4 来说，若想将去除 3->3，则需要链接3之前的2节点和第一个4节点，因此这两个节点需要有指针指向才行。
+    *        因此首先设置虚拟头结点，再创建两个指针（分别指向重复节点之前和之后的节点）+ 一个标志位（标志当前是否发现了重复节点）：
+    *        D -> 2 -> 3 -> 3 -> 4
+    *        a    b                 此时 b.val != b.next.val，则 a, b 向后移动
+    *        D -> 2 -> 3 -> 3 -> 4
+    *             a    b            此时 b.val == b.next.val，则 a 不动，b 向后移动，标志位置为 true
+    *        D -> 2 -> 3 -> 3 -> 4
+    *             a         b       此时 b.val != b.next.val，但因为标志位置为 true，所以 a 不动，b 向后移动，标志位置为 false，让 a.next 指向 b
+    *        D -> 2 -> 4
+    *             a    b            此时 b.next == null，结束遍历
+    * - 注意特殊情况的处理：D -> 2 -> 1 -> 1，此时重复节点后面没有更多节点，因此 a 停在 2 上，b 停在最后一个1上，无法按上面的过
+    *   程完成删除（即循环已结束，但标志位还是 true），因此可直接将 a.next 指向 null 即可。
+    */
     public static ListNode deleteDuplicates(ListNode head) {
         if (head == null) return null;
         ListNode dummyHead = new ListNode();
