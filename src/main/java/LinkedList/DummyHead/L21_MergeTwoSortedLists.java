4ee9079b1728c2ad33ab34aacd4418d92ddc308e
Max Jiang <maxjiang23326@gmail.com>
2019-05-17 13:31:38 +0800

    feat: add two more solution for L21

--- src/main/java/LinkedList/DummyHead/L21_MergeTwoSortedLists.java
+++ src/main/java/LinkedList/DummyHead/L21_MergeTwoSortedLists.java
@@ -16,22 +16,22 @@ public class L21_MergeTwoSortedLists {
     * - 时间复杂度 O(m+n)，空间复杂度 O(1)。
     * */
     public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode dummyHead = new ListNode(), curr = dummyHead, n1 = l1, n2 = l2;
+        ListNode dummyHead = new ListNode(), curr = dummyHead;
 
-        while (n1 != null || n2 != null) {
-            if (n1 == null) {
-                curr.next = n2;
-                n2 = n2.next;
-            } else if (n2 == null) {
-                curr.next = n1;
-                n1 = n1.next;
+        while (l1 != null || l2 != null) {
+            if (l1 == null) {
+                curr.next = l2;
+                l2 = l2.next;
+            } else if (l2 == null) {
+                curr.next = l1;
+                l1 = l1.next;
             } else {
-                if (n1.val < n2.val) {
-                    curr.next = n1;
-                    n1 = n1.next;
+                if (l1.val < l2.val) {
+                    curr.next = l1;
+                    l1 = l1.next;
                 } else {
-                    curr.next = n2;
-                    n2 = n2.next;
+                    curr.next = l2;
+                    l2 = l2.next;
                 }
             }
             curr = curr.next;
@@ -41,8 +41,8 @@ public class L21_MergeTwoSortedLists {
     }
 
     /*
-     * 解法2：
-     * - 时间复杂度 O()，空间复杂度 O()。
+     * 解法2：解法1的递归版
+     * - 时间复杂度 O(m+n)，空间复杂度 O(m+n)。
      * */
     public static ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
         if (l1 == null && l2 == null) return null;
@@ -63,13 +63,52 @@ public class L21_MergeTwoSortedLists {
         }
     }
 
+    /*
+    * 解法3：解法1的改进版
+    * - 时间复杂度 O(m+n)，空间复杂度 O(1)。
+    * */
+    public static ListNode mergeTwoLists3(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(), curr = dummyHead;
+
+        while (l1 != null && l2 != null) {  // 此处是 &&，而非解法1中的 ||
+            if (l1.val < l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            curr = curr.next;
+        }
+
+        if (l1 != null) curr.next = l1;  // while 结束后 l1, l2 中至少有一个被遍历完了，另一个中的节点直接接到新链表上
+        if (l2 != null) curr.next = l2;
+
+        return dummyHead.next;
+    }
+
+    /*
+    * 解法4：解法3的递归版（即解法2的改进版）
+    * */
+    public static ListNode mergeTwoLists4(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists4(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists4(l1, l2.next);
+            return l2;
+        }
+    }
+
     public static void main(String[] args) {
         ListNode l1 = createLinkedListFromArray(new int[]{1, 2, 4});
         ListNode l2 = createLinkedListFromArray(new int[]{1, 3, 4});
-        printLinkedList(mergeTwoLists2(l1, l2));  // expects 1->1->2->3->4->4->NULL
+        printLinkedList(mergeTwoLists4(l1, l2));  // expects 1->1->2->3->4->4->NULL
 
         ListNode l3 = createLinkedListFromArray(new int[]{5});
         ListNode l4 = createLinkedListFromArray(new int[]{1, 2, 4});
-        printLinkedList(mergeTwoLists2(l3, l4));  // expects 1->2->4->5->NULL
+        printLinkedList(mergeTwoLists4(l3, l4));  // expects 1->2->4->5->NULL
     }
 }
