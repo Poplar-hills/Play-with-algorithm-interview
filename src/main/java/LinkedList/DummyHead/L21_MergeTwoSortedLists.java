24be76875805473c96c74d52d501aef946d4a445
Max Jiang <maxjiang23326@gmail.com>
2019-05-17 13:12:51 +0800

    feat: add yet another solution for L21

--- src/main/java/LinkedList/DummyHead/L21_MergeTwoSortedLists.java
+++ src/main/java/LinkedList/DummyHead/L21_MergeTwoSortedLists.java
@@ -40,13 +40,36 @@ public class L21_MergeTwoSortedLists {
         return dummyHead.next;
     }
 
+    /*
+     * 解法2：
+     * - 时间复杂度 O()，空间复杂度 O()。
+     * */
+    public static ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null) return null;
+        if (l1 == null) {
+            l2.next = mergeTwoLists2(null, l2.next);
+            return l2;
+        }
+        if (l2 == null) {
+            l1.next = mergeTwoLists2(l1.next, null);
+            return l1;
+        }
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists2(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists2(l1, l2.next);
+            return l2;
+        }
+    }
+
     public static void main(String[] args) {
         ListNode l1 = createLinkedListFromArray(new int[]{1, 2, 4});
         ListNode l2 = createLinkedListFromArray(new int[]{1, 3, 4});
-        printLinkedList(mergeTwoLists(l1, l2));  // expects 1->1->2->3->4->4->NULL
+        printLinkedList(mergeTwoLists2(l1, l2));  // expects 1->1->2->3->4->4->NULL
 
         ListNode l3 = createLinkedListFromArray(new int[]{5});
         ListNode l4 = createLinkedListFromArray(new int[]{1, 2, 4});
-        printLinkedList(mergeTwoLists(l3, l4));  // expects 1->2->4->5->NULL
+        printLinkedList(mergeTwoLists2(l3, l4));  // expects 1->2->4->5->NULL
     }
 }
