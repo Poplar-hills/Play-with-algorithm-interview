da25941c76dda22c162215ddd17e1f414ee55e71
Max Jiang <maxjiang23326@gmail.com>
2019-05-30 11:42:35 +0800

    fix: incorrect explanation

--- src/main/java/LinkedList/ClassicInterweaving/L206_ReverseLinkedList.java
+++ src/main/java/LinkedList/ClassicInterweaving/L206_ReverseLinkedList.java
@@ -33,10 +33,11 @@ public class L206_ReverseLinkedList {
      *      ← 4<-3<-2<-1
      *   要实现这个过程：
      *   1. 递归终止条件：head == null || head.next == null（这两种情况只需返回 head 即可，不需要改变节点顺序）
-     *   2. 递归单元逻辑：例如当 reserveList(head.next) 返回 4<-3 时，链表为：1->2->4<-3，此时 head 为 2。因此需要让 2<-4
-     *      反向，得到 1->2<-4<-3，再返回 head 到上一层。要反向 2<-4 就需要：
-     *      a. 让 4.next 指向 2 —— head.next.next = head
-     *      b. 移除 2.next 的指向 —— head.next = null
+     *   2. 递归单元逻辑：例如当 reserveList(head.next) 返回 4->3 时，此时链表为：1->2  4->3。此时需要让2链到3后面，
+     *      得到 1  4->3->2，再将4返回给上一层。这就需要：                          |_____↑
+     *           |________↑
+     *      a. 让 3.next 指向 2 —— ∵ 此时 head 是2 ∴ head.next.next = head，此时链表为：1  4->3<->2
+     *      b. 再移除 2->3 的链接（仅保留 3->2）—— head.next = null                    |_________↑
      * - 时间复杂度 O(n)，空间复杂度 O(n)
      * */
     public static ListNode reverseList2(ListNode head) {
