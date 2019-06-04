0048128c9b3f8c6c4be0d33bc149ca8f61b21250
Max Jiang <maxjiang23326@gmail.com>
2019-06-04 12:38:09 +0800

    feat: add another solution for L144

--- src/main/java/StackAndQueue/StackAndRecursion/L144_BinaryTreePreorderTraversal.java
+++ src/main/java/StackAndQueue/StackAndRecursion/L144_BinaryTreePreorderTraversal.java
@@ -1,6 +1,8 @@
 package StackAndQueue.StackAndRecursion;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 
 import static Utils.Helpers.*;
@@ -29,8 +31,31 @@ public class L144_BinaryTreePreorderTraversal {
         preorderTraversal(node.right, list);
     }
 
+    /*
+    * 解法2：遍历
+    * - 时间复杂度 O(n)，空间复杂度 O(n)。
+    * */
+    public static List<Integer> preorderTraversal2(TreeNode root) {
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        List<Integer> list = new ArrayList<>();
+        if (root == null) return list;
+
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            list.add(node.val);
+            if (node.right != null) stack.push(node.right);
+            if (node.left != null) stack.push(node.left);
+        }
+
+        return list;
+    }
+
     public static void main(String[] args) {
-        TreeNode t = createBinaryTreeFromArray(new Integer[]{1, null, 2, 3});
-        log(preorderTraversal(t));  // expects [1, 2, 3]
+        TreeNode t1 = createBinaryTreeFromArray(new Integer[]{1, null, 2, 3});
+        log(preorderTraversal2(t1));  // expects [1, 2, 3]
+
+        TreeNode t2 = createBinaryTreeFromArray(new Integer[]{});
+        log(preorderTraversal2(t2));  // expects []
     }
 }
