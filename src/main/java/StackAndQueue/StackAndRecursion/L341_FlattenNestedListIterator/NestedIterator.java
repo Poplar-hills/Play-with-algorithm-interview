aeaf594692a1ee8802dee0a69b794d6df223cd47
Max Jiang <maxjiang23326@gmail.com>
2019-06-05 17:13:21 +0800

    feat: add question L341

---
+++ src/main/java/StackAndQueue/StackAndRecursion/L341_FlattenNestedListIterator/NestedIterator.java
@@ -0,0 +1,30 @@
+package StackAndQueue.StackAndRecursion.L341_FlattenNestedListIterator;
+
+import java.util.Iterator;
+import java.util.List;
+
+/*
+* Flatten Nested List Iterator
+*
+* - Given a nested list of integers, implement an iterator to flatten it.
+* - Each element is either an integer, or a list -- whose elements may also be integers or other lists.
+* - Your NestedIterator object will be instantiated and called as such:
+*   NestedIterator i = new NestedIterator(nestedList);
+*   while (i.hasNext()) v[f()] = i.next();
+* */
+
+public class NestedIterator implements Iterator<Integer> {
+    public NestedIterator(List<NestedIterator> nestedList) {
+
+    }
+
+    @Override
+    public Integer next() {
+
+    }
+
+    @Override
+    public boolean hasNext() {
+
+    }
+}
