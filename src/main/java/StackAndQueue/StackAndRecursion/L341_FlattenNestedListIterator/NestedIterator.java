02f8f2df0a868304281658313dc8ed748b17f39f
Max Jiang <maxjiang23326@gmail.com>
2019-06-06 13:20:02 +0800

    feat: add another solution for L341

--- src/main/java/StackAndQueue/StackAndRecursion/L341_FlattenNestedListIterator/NestedIterator.java
+++ src/main/java/StackAndQueue/StackAndRecursion/L341_FlattenNestedListIterator/NestedIterator.java
@@ -12,8 +12,8 @@ import java.util.*;
 *     NestedIterator i = new NestedIterator(nestedList);
 *     while (i.hasNext()) v[f()] = i.next();
 *
-* - Example 1: Input: [[1,1],2,[1,1]], Output: [1,1,2,1,1]. By calling next repeatedly until hasNext returns false,
-*   the order of elements returned by next should be: [1,1,2,1,1].
+* - Example 1: Input: [[0,1],2,[3,4]], Output: [0,1,2,3,4]. By calling next repeatedly until hasNext returns false,
+*   the order of elements returned by next should be: [0,1,2,3,4].
 *
 * - Example 2: Input: [1,[4,[6]]], Output: [1,4,6].
 * */
@@ -49,28 +49,32 @@ class NestedIterator implements Iterator<Integer> {
 
 /*
  * 解法2：Lazy approach
- * -
+ * - 实现：因为使用迭代而非递归。
+ * - 注：Stack 是尾进尾出:          Deque 实现的 stack 是头进头出；Deque 实现的 queue 是尾进头出：
+ *                                          poll <--
+ *      [ | | | | ]  <--- push              push -->  [ | | | | ]  <-- offer
+ *                   ---> pop               pop  <--
  * */
 class NestedIterator2 implements Iterator<Integer> {
-    private Deque<NestedInteger> stack = new ArrayDeque<>();  // Deque 接口的实现可以是：ArrayDeque, LinkedList
+    private Deque<NestedInteger> dq = new ArrayDeque<>();  // Deque 接口的实现可以是 ArrayDeque 也可以是 LinkedList
 
     public NestedIterator2(List<NestedInteger> nestedList) {
         for (NestedInteger n : nestedList)
-            stack.push(n);
+            dq.offer(n);    // 从尾部添加第一层的所有 NestedInteger
     }
 
     @Override
     public boolean hasNext() {
-        while (!stack.isEmpty() && !stack.peek().isInteger()) {
-            List<NestedInteger> nestedList = stack.pop().getList();
-            for (int i = nestedList.size() - 1; i >= 0; i--)
-                stack.push(nestedList.get(i));
+        while (!dq.isEmpty() && !dq.peek().isInteger()) {
+            List<NestedInteger> nestedList = dq.pop().getList();
+            for (int i = nestedList.size() - 1; i >= 0; i--)  // 让 nestedList 倒序入栈，因此其第一个元素会先被访问
+                dq.push(nestedList.get(i));
         }
-        return !stack.isEmpty();
+        return !dq.isEmpty();
     }
 
     @Override
     public Integer next() {
-        return hasNext() ? stack.pop().getInteger() : null;
+        return hasNext() ? dq.pop().getInteger() : null;
     }
 }
--- src/main/java/StackAndQueue/StackAndRecursion/L94_BinaryTreeInorderTraversal.java
+++ src/main/java/StackAndQueue/StackAndRecursion/L94_BinaryTreeInorderTraversal.java
@@ -115,7 +115,6 @@ public class L94_BinaryTreeInorderTraversal {
         return list;
     }
 
-
     public static void main(String[] args) {
         TreeNode t1 = createBinaryTreeFromArray(new Integer[]{1, null, 2, 3});
         log(inorderTraversal4(t1));  // expects [1, 3, 2]
