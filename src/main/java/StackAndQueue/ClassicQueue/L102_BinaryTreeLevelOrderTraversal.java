2ff509f2a17e4660fe5d9b8bf7ade874c5535a70
Max Jiang <maxjiang23326@gmail.com>
2019-06-09 10:59:39 +0800

    feat: add level order traversal reference

--- src/main/java/StackAndQueue/ClassicQueue/L102_BinaryTreeLevelOrderTraversal.java
+++ src/main/java/StackAndQueue/ClassicQueue/L102_BinaryTreeLevelOrderTraversal.java
@@ -17,12 +17,29 @@ import static Utils.Helpers.*;
 
 public class L102_BinaryTreeLevelOrderTraversal {
     /*
+    * 该方法是最基本的层序遍历，不是该题的解，只是用于和解法1进行对比。
+    * */
+    public static List<Integer> simpleLevelOrder(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        if (root == null) return res;
+
+        q.offer(root);
+        while (!q.isEmpty()) {
+            TreeNode curr = q.poll();
+            res.add(curr.val);
+            if (curr.left != null) q.offer(curr.left);
+            if (curr.right != null) q.offer(curr.right);
+        }
+        return res;
+    }
+    /*
     * 解法1：
     * */
     public static List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> list = new ArrayList<>();
-        Queue<Pair<TreeNode, Integer>> q = new LinkedList<>();
-        if (root == null) return list;
+        List<List<Integer>> res = new ArrayList<>();
+        Queue<Pair<TreeNode, Integer>> q = new LinkedList<>();  // 因为结果要求同一层的节点放在一个列表中，因此这里队列中除了保存节点之外还需要保存层级信息
+        if (root == null) return res;
 
         q.offer(new Pair<>(root, 1));
         while (!q.isEmpty()) {
@@ -30,22 +47,25 @@ public class L102_BinaryTreeLevelOrderTraversal {
             TreeNode node = pair.getKey();
             Integer level = pair.getValue();
 
-            if (level > list.size()) {
+            if (level > res.size()) {  //
                 List<Integer> l = new ArrayList<>();
                 l.add(node.val);
-                list.add(l);
+                res.add(l);
             } else
-                list.get(level - 1).add(node.val);
+                res.get(level - 1).add(node.val);
 
             if (node.left != null) q.offer(new Pair<>(node.left, level + 1));
             if (node.right != null) q.offer(new Pair<>(node.right, level + 1));
         }
 
-        return list;
+        return res;
     }
 
     public static void main(String[] args) {
-        TreeNode t = createBinaryTreeBreadthFirst(new Integer[]{3, 9, 20, null, null, 15, 7});
-        log(levelOrder(t));  // expects [[3], [9,20], [15,7]]
+        TreeNode t1 = createBinaryTreeBreadthFirst(new Integer[]{3, 9, 20, null, null, 15, 7});
+        log(simpleLevelOrder(t1));  // expects [3, 9, 20, 15, 7]
+
+        TreeNode t2 = createBinaryTreeBreadthFirst(new Integer[]{3, 9, 20, null, null, 15, 7});
+        log(levelOrder(t2));  // expects [[3], [9,20], [15,7]]
     }
 }
