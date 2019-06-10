ef6e7d1d1ce8c0887e6d82e09c83f50925413ec4
Max Jiang <maxjiang23326@gmail.com>
2019-06-10 08:26:17 +0800

    feat: add simple bottom-up level order traversal

--- src/main/java/StackAndQueue/ClassicQueue/L107_BinaryTreeLevelOrderTraversalII.java
+++ src/main/java/StackAndQueue/ClassicQueue/L107_BinaryTreeLevelOrderTraversalII.java
@@ -1,6 +1,6 @@
 package StackAndQueue.ClassicQueue;
 
-import java.util.List;
+import java.util.*;
 
 import static Utils.Helpers.*;
 
@@ -12,12 +12,33 @@ import static Utils.Helpers.*;
 * */
 
 public class L107_BinaryTreeLevelOrderTraversalII {
-    public static List<List<Integer>> levelOrderBottom(TreeNode root) {
+    public static List<Integer> simpleLevelOrderBottom(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        if (root == null) return res;
 
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node.right != null) queue.offer(node.right);
+            if (node.left != null) queue.offer(node.left);
+            stack.push(node);
+        }
+
+        while (!stack.isEmpty())
+            res.add(stack.pop().val);
+
+        return res;
     }
+
+//    public static List<List<Integer>> levelOrderBottom(TreeNode root) {
+//
+//    }
 
     public static void main(String[] args) {
         TreeNode t = createBinaryTreeBreadthFirst(new Integer[]{3, 9, 20, null, 8, 15, 7});
-        log(levelOrderBottom(t));  // expects [[8,15,7], [9,20], [3]]
+        log(simpleLevelOrderBottom(t));  // expects [8, 15, 7, 9, 20, 3]
+//        log(levelOrderBottom(t));        // expects [[8,15,7], [9,20], [3]]
     }
 }
