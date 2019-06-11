9c7c36102e6a52936d69795ccabb2f1f3697fc93
Max Jiang <maxjiang23326@gmail.com>
2019-06-11 18:33:48 +0800

    refactor: performance boost

--- src/main/java/StackAndQueue/BFSAndGraphShortestPath/L279_PerfectSquares.java
+++ src/main/java/StackAndQueue/BFSAndGraphShortestPath/L279_PerfectSquares.java
@@ -3,6 +3,7 @@ package StackAndQueue.BFSAndGraphShortestPath;
 import javafx.util.Pair;
 
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 import static Utils.Helpers.log;
@@ -15,7 +16,7 @@ import static Utils.Helpers.log;
 
 public class L279_PerfectSquares {
     /*
-    * 超时解：BFS
+    * 解法1：BFS
     * - 思路：将该问题建模成一个图论问题：从 n 到 0，每个数字表示一个顶点；若两顶点之间相差一个完全平方数，则链接两顶点，由此得到一个
     *   无权图，并且该问题转化为了求该无权图中从 n 到 0 的最短路径的步数。例如：
     *                                    0 -- 1 -- 2                 0 -- 1 ---- 2
@@ -29,8 +30,12 @@ public class L279_PerfectSquares {
     * */
     public static int numSquares(int n) {
         assert n > 0;
-        Queue<Pair<Integer, Integer>> q = new LinkedList<>();  // Pair<顶点, 从起点开始到达该顶点所走过的步数>
+
+        Queue<Pair<Integer, Integer>> q = new LinkedList<>();  // Pair<顶点, 从起点到该顶点所走过的步数>
         q.offer(new Pair<>(n, 0));  // 顶点 n 作为 BFS 的起点，因此经过0步路径到达
+
+        boolean[] visited = new boolean[n + 1];  // 从 n 到 0 需要开 n+1 的空间
+        visited[n] = true;
 
         while (!q.isEmpty()) {
             Pair<Integer, Integer> pair = q.poll();
@@ -38,8 +43,13 @@ public class L279_PerfectSquares {
             int step = pair.getValue();
 
             if (num == 0) return step;  // 若到达终点则返回路径步数
-            for (int i = 1; num - i * i >= 0; i++)  // 若还未到达终点则将所有与当前顶点相差一个完全平方数的顶点入队（这里相当于 BFS 中将所有相邻顶点入队）
-                q.offer(new Pair<>(num - i * i, step + 1));
+            for (int i = 1; num - i * i >= 0; i++) {  // 若还未到达终点则将所有与当前顶点相差一个完全平方数的顶点入队（这里相当于 BFS 中将所有相邻顶点入队）
+                int next = num - i * i;
+                if (!visited[next]) {   // 已访问过的节点不入队
+                    q.offer(new Pair<>(next, step + 1));
+                    visited[next] = true;
+                }
+            }
         }
         throw new IllegalStateException("No Solution.");  // 只要输入参数正确则不会到达这行 ∵ 所有正整数最终都可以用1相加得到
     }
--- src/main/java/Utils/Helpers.java
+++ src/main/java/Utils/Helpers.java
@@ -8,9 +8,9 @@ import java.util.function.Consumer;
 public class Helpers {
     public static void log(Object content) { System.out.println(content); }
 
-    public static void log(int[] arr) { System.out.println(Arrays.toString(arr)); }
-
     public static void log(char[] arr) { System.out.println(Arrays.toString(arr)); }
+
+    public static void log(boolean[] arr) { System.out.println(Arrays.toString(arr)); }
 
     public static void log(Object[] arr) {
         System.out.println(Arrays.toString(arr));
