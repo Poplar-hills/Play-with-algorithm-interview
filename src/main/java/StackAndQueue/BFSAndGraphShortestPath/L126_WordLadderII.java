c00d97061dce9716d360211168f3484852eaf69e
Max Jiang <maxjiang23326@gmail.com>
2019-06-16 21:04:54 +0800

    feat: add test cases for L126

--- src/main/java/StackAndQueue/BFSAndGraphShortestPath/L126_WordLadderII.java
+++ src/main/java/StackAndQueue/BFSAndGraphShortestPath/L126_WordLadderII.java
@@ -22,8 +22,28 @@ public class L126_WordLadderII {
         log(findLadders("hit", "cog", wordList));
         // expects [["hit","hot","dot","dog","cog"], ["hit","hot","lot","log","cog"]]
 
-        List<String> wordList2 = Arrays.asList("hot", "dot", "dog", "lot", "log");
-        log(findLadders("hit", "cog", wordList2));
+        List<String> wordList2 = Arrays.asList("a", "b", "c");
+        log(findLadders("a", "c", wordList2));
+        // expects [["a","c"]]
+
+        List<String> wordList3 = Arrays.asList("ted", "tex", "red", "tax", "tad", "den", "rex", "pee");
+        log(findLadders("red", "tax", wordList3));
+        // expects [["red","ted","tad","tax"]]
+
+        List<String> wordList4 = Arrays.asList("hot", "dot", "dog", "lot", "log");
+        log(findLadders("hit", "cog", wordList4));
         // expects []
+
+        List<String> wordList5 = Arrays.asList("hot", "dog");
+        log(findLadders("hot", "dog", wordList5));
+        // expects []
+
+        List<String> wordList6 = Arrays.asList("lest", "leet", "lose", "code", "lode", "robe", "lost");
+        log(findLadders("leet", "code", wordList6));
+        // expects [["leet","lest","lost","lose","lode","code"]]
+
+        List<String> wordList7 = Arrays.asList("miss", "dusk", "kiss", "musk", "tusk", "diss", "disk", "sang", "ties", "muss");
+        log(findLadders("kiss", "tusk", wordList7));
+        // expects [["kiss","miss","muss","musk","tusk"]]
     }
 }
