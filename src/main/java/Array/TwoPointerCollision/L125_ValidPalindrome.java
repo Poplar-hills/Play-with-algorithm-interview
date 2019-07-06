10e597bb569cb6fbb7da3dfdb5e8fbfa81120033
Max Jiang <maxjiang23326@gmail.com>
2019-07-06 17:27:40 +0800

    docs: update comments

--- src/main/java/Array/TwoPointerCollision/L125_ValidPalindrome.java
+++ src/main/java/Array/TwoPointerCollision/L125_ValidPalindrome.java
@@ -5,18 +5,19 @@ import static Utils.Helpers.log;
 /*
 * Valid Palindrome
 *
-* - Given a string, determine if it is a palindrome, considering only alphanumeric characters (即只考虑字符串和数字，不考
+* - Given a string, determine if it is a palindrome, considering only alphanumeric characters (即只考虑字母和数字，不考
 *   虑空格、标点符号) and ignoring cases. Also, empty string is a valid palindrome.
 * */
 
 public class L125_ValidPalindrome {
     /*
     * 解法1：指针对撞
+    * - 时间复杂度 O()，空间复杂度 O()。
     * */
     private static boolean validPalindrome(String s) {
         s = s.toLowerCase();
-        for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
-            while (i < j && !Character.isLetterOrDigit(s.charAt(i))) i++;
+        for (int i = 0, j = s.length() - 1; i < j; i++, j--) {             // 指针对撞
+            while (i < j && !Character.isLetterOrDigit(s.charAt(i))) i++;  // 跳过无效字符
             while (j > i && !Character.isLetterOrDigit(s.charAt(j))) j--;
             if (s.charAt(i) != s.charAt(j)) return false;
         }
