3ae7e4c041f484b6ac3db463fd17d4967dee15a8
Max Jiang <maxjiang23326@gmail.com>
2019-07-09 12:59:46 +0800

    docs: update comments

--- src/main/java/HashTable/SetAndMapBasics/L242_ValidAnagram.java
+++ src/main/java/HashTable/SetAndMapBasics/L242_ValidAnagram.java
@@ -9,7 +9,7 @@ import static Utils.Helpers.log;
 /*
 * Valid Anagram
 *
-* - Determine if string t is an anagram of string s.
+* - Determine if string t is an anagram (由颠倒字母顺序而构成的词) of string s.
 * - 思路：找到 anagram 的特点 —— Two strings are anagrams if and only if their character counts are the same.
 * */
 
@@ -33,18 +33,16 @@ public class L242_ValidAnagram {
     * - 时间复杂度 O(n)，空间复杂度 O(n)。
     * */
     public static boolean isAnagram2(String s, String t) {
-        if (t.length() != s.length())
-            return false;
+        if (t.length() != s.length()) return false;
 
         Map<Character, Integer> map = new HashMap<>();
-        for (char c : s.toCharArray())  // O(n)
-            map.put(c, map.getOrDefault(c, 0) + 1);
+        for (char c : s.toCharArray())    // O(n)
+            map.put(c, map.getOrDefault(c, 0) + 1);  // 记录所有字符的频率
 
         for (char c : t.toCharArray()) {  // O(n)
-            if (!map.containsKey(c))
-                return false;
+            if (!map.containsKey(c)) return false;
             if (map.get(c) > 0) {
-                map.put(c, map.get(c) - 1);
+                map.put(c, map.get(c) - 1);  // 让频率-1
                 if (map.get(c) == 0)
                     map.remove(c);
             }
@@ -57,14 +55,13 @@ public class L242_ValidAnagram {
     * - 时间复杂度 O(n)，空间复杂度 O(n)。
     * */
     public static boolean isAnagram3(String s, String t) {
-        if (t.length() != s.length())
-            return false;
+        if (t.length() != s.length()) return false;
 
         Map<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < s.length(); i++) {  // O(n)，因为之前已经处理过长度不等的情况，因此这里可以一次遍历处理两个字符串
+        for (int i = 0; i < s.length(); i++) {  // O(n)，第一行已经处理过长度不等的情况，因此这里可以一次遍历处理两个字符串
             char sc = s.charAt(i);
-            map.put(sc, map.getOrDefault(sc, 0) + 1);  // 一个加
             char tc = t.charAt(i);
+            map.put(sc, map.getOrDefault(sc, 0) + 1);  // 一个加
             map.put(tc, map.getOrDefault(tc, 0) - 1);  // 一个减
         }
 
@@ -95,13 +92,9 @@ public class L242_ValidAnagram {
 
     public static void main(String[] args) {
         log(isAnagram4("anagram", "nagaram"));  // expects true
+        log(isAnagram4("weq", "qwe"));          // expects true
         log(isAnagram4("rat", "car"));          // expects false
         log(isAnagram4("abcd", "abc"));         // expects false
         log(isAnagram4("aacc", "ccac"));        // expects false
-
-        log(isAnagram2("anagram", "nagaram"));  // expects true
-        log(isAnagram2("rat", "car"));          // expects false
-        log(isAnagram2("abcd", "abc"));         // expects false
-        log(isAnagram2("aacc", "ccac"));        // expects false
     }
 }
