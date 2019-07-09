a62e50326e9b010e5ea4b60acfbc8a39c6ca5294
Max Jiang <maxjiang23326@gmail.com>
2019-07-09 12:33:35 +0800

    docs: update comments

--- src/main/java/HashTable/SetAndMapBasics/L202_HappyNumber.java
+++ src/main/java/HashTable/SetAndMapBasics/L202_HappyNumber.java
@@ -10,7 +10,7 @@ import static Utils.Helpers.log;
 /*
 * Happy Number
 *
-* - 将一个数字替换为其各位数字的平方和，重复该过程，如果最终能得到1，则是 happer number，若陷入循环则不是。
+* - 将一个数字替换为其各位数字的平方和，重复该过程，如果最终能得到1，则是 happy number，若陷入循环则不是。
 *   例如：19 -> 1^2 + 9^2 = 82
 *           -> 8^2 + 2^2 = 68
 *           -> 6^2 + 8^2 = 100
@@ -19,7 +19,8 @@ import static Utils.Helpers.log;
 
 public class L202_HappyNumber {
     /*
-    * 解法1 - 使用 map，递归
+    * 解法1 - Map + 递归
+    * - 思路：本题的关键是如何检测循环 -- 若没有循环则所有数字应该只出现一次，不会重复，因此可以使用 Map 或 Set。
     * */
     public static boolean isHappy(int n) {
         Map<Integer, Integer> map = new HashMap<>();
@@ -43,12 +44,12 @@ public class L202_HappyNumber {
     }
 
     /*
-     * 解法2 - 使用 set，非递归
+     * 解法2 - Set + 迭代
      * */
     public static boolean isHappy2(int n) {
         Set<Integer> set = new HashSet<>();
         set.add(n);
-        while (n != 1) {
+        while (n != 1) {  // 循环终止条件是抵达1
             int sum = sumOfDigitSquare(n);
             if (set.contains(sum)) return false;
             set.add(sum);
@@ -59,20 +60,18 @@ public class L202_HappyNumber {
 
     /*
      * 解法3 - Floyd Cycle detection
-     * - 复杂度 O(1)
      * */
     public static boolean isHappy3(int n) {
         int slow = n, fast = n;
         do {
             slow = sumOfDigitSquare(slow);
-            fast = sumOfDigitSquare(fast);
-            fast = sumOfDigitSquare(fast);
-        } while (slow != fast);
+            fast = sumOfDigitSquare(sumOfDigitSquare(fast));  // fast 每次执行两遍，slow 执行一遍
+        } while (slow != fast);                               // 若中途 fast == slow 则说明有环
         return slow == 1;
     }
 
     public static void main(String[] args) {
-        log(isHappy(19));  // true
+        log(isHappy(19));   // true
         log(isHappy2(19));  // true
         log(isHappy3(19));  // true
     }
