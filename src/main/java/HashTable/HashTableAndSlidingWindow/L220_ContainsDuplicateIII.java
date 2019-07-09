711fcf820007f55c8b26366bd81d7c58e1dd3f73
Max Jiang <maxjiang23326@gmail.com>
2019-07-09 11:59:02 +0800

    docs: update comments

--- src/main/java/HashTable/HashTableAndSlidingWindow/L220_ContainsDuplicateIII.java
+++ src/main/java/HashTable/HashTableAndSlidingWindow/L220_ContainsDuplicateIII.java
@@ -7,35 +7,40 @@ import static Utils.Helpers.log;
 /*
 * Contains Duplicate III
 *
-* - 对于整型数组 nums 和整数 k，是否存在索引 i 和 j，使得 nums[i] 与 nums[j] 的差距不超过给定的整数 t，
-*   且 i 和 j 之间的差不超过给定的整数 k。
+* - 对于整型数组 nums 和整数 k，是否存在索引 i 和 j，使得：
+*   1. nums[i] 与 nums[j] 的差不超过整数 t；
+*   2. i 和 j 之间的差不超过整数 k。
 *
 * - 分析可知本题中的两个限制条件：
 *   1. |i - j| <= k
 *   2. |nums[i] - nums[j]| <= t
 *
-* - 思路：仍然可以采用 L219_ContainsDuplicateII 中解法2的实现方式，即用 Set 作查找表 + 滑动窗口。具体来说：
-*   - 对于限制1来说，与 L219_ContainsDuplicateII 的解法2一样（用 Set 作为 window，将差距超过 k 的元素从 Set 中移除）；
-*   - 对于限制2来说，需要判断窗口中是否存在 nums[j] 使得 nums[i]-t <= nums[j] <= nums[i]+t。若 nums 有序，则进行这样的判断会
-*     非常容易 —— 只需判断元素 nums[i] 在 nums 中的 ceil 是否小于 nums[i] + t，或 floor 是否大于 nums[i] - t。
 * */
 
 public class L220_ContainsDuplicateIII {
     /*
     * 解法1：
-    * - 时间复杂度 O(nlogk)，空间复杂度 O(k)，因为 TreeSet 的操作复杂度为 O(logn)，而 set 中只存 k 个元素。
-    * - 注意：因为代码中存在 next + t，对于第四个 test case 的大整数来说会造成整型溢出。因此需要将 next 和 t 转成 long
+    * - 分析：本题中的两个需求：1. |i - j| <= k； 2. |nums[i] - nums[j]| <= t。
+    * - 思路：仍然可以采用 L219_ContainsDuplicateII 中解法2的实现方式，即 Set 作查找表 + 滑动窗口：
+    *   - 对于需求1：用 Set 作为 window，只保存 k 个元素，将差距超过 k 的元素从 Set 中移除；
+    *   - 对于需求2：需要判断窗口中是否有元素 nums[j] 在 [nums[i]-t, nums[i]+t] 范围内。
+    *     - 对于该需求，若 nums 有序，则做这样的判断非常容易 —— 只需判断在 nums 中 nums[i] 的 ceil 是否 <= nums[i]+t，
+    *       或 nums[i] 的 floor 是否 >= nums[i]-t。而 nums 的有序性可以通过 TreeSet 实现。
+    * - 时间复杂度 O(nlogk)，因为 TreeSet 的操作复杂度为 O(logn)，而 set 中只存 k 个元素；空间复杂度 O(k)。
+    * - 注意：因为代码中存在 curr + t，对于第四个 test case 的大整数来说会造成整型溢出。因此需要将 curr 和 t 转成 long
     *   （不过这种问题在面试中很少见）。
     * */
     public static boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-        TreeSet<Long> set = new TreeSet<>();  // set 的类型声明必须是 TreeSet 而不能是 Set，否则后面 ceiling 方法取不到（Set 接口上没有 ceiling 方法）
+        TreeSet<Long> set = new TreeSet<>();  // set 的类型声明必须是 TreeSet 而不能是 Set，否则后面无法调用 ceiling 方法（Set 接口上没有 ceiling 方法）
         for (int i = 0; i < nums.length; i++) {
-            long next = (long) nums[i];
-            Long ceiling = set.ceiling(next), floor = set.floor(next);
-            boolean containsAlmostDuplicate = (ceiling != null && ceiling <= next + (long) t) ||
-                    (floor != null && floor >= next - (long) t);
-            if (containsAlmostDuplicate) return true;
-            set.add(next);
+            long curr = (long) nums[i];
+            Long ceiling = set.ceiling(curr), floor = set.floor(curr);  // 获取 curr 在有序的 nums 中的前后元素
+
+            boolean containsDuplicate = (ceiling != null && ceiling <= curr + (long) t) ||
+                    (floor != null && floor >= curr - (long) t);
+            if (containsDuplicate) return true;
+
+            set.add(curr);
             if (set.size() == k + 1)
                 set.remove((long) nums[i - k]);  // 注意这里也要转换成 long
         }
