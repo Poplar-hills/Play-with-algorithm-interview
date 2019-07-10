c951550a4239c56a91713818a5cc627948ef2dd9
Max Jiang <maxjiang23326@gmail.com>
2019-07-10 23:27:45 +0800

    docs: update comments

--- src/main/java/HashTable/SetAndMapBasics/L205_IsomorphicStrings.java
+++ src/main/java/HashTable/SetAndMapBasics/L205_IsomorphicStrings.java
@@ -11,24 +11,24 @@ import static Utils.Helpers.log;
 public class L205_IsomorphicStrings {
     /*
     * 解法1：双查找表
-    * - 思路：判断两个字符串是否是 isomorphic 的，只需判断两者中各字符的相对位置是否相同，因此需要建立 s 中的字符 -> t 中的字符的映射。
-    * - 实现：两个 map 分别记录 s[i] -> t[i] 和 t[i] -> s[i] 的映射关系，之后在遍历字符串过程中检查每个字符的映射关系是否与之前记录的映射关系一致。
+    * - 思路：判断两个字符串是否是 isomorphic，只需判断两个字符串中的相同索引上的字符是否具有相同的频率。
+    * - 实现：两个查找表分别记录两个字符串中所有字符的频率，并在之后遍历过程中检查相同索引上的两个字符的频率是否与之前记录的一致。
     * - 时间复杂度 O(n)，空间复杂度 O(2len(charset))。
     * */
     public static boolean isIsomorphic(String s, String t) {
         if (s.length() != t.length())
             return false;
 
-        char[] sMap = new char[256];  // 也可以使用 Map 实现
-        char[] tMap = new char[256];
+        char[] sMap = new char[128];  // 也可以使用 Map 实现
+        char[] tMap = new char[128];
 
-        for (int i = 0; i < s.length(); i++) {     // 遍历一个字符串
-            char sc = s.charAt(i), tc = t.charAt(i);
-            if (sMap[sc] == 0 && tMap[tc] == 0) {  // 若 sc 和 tc 没有被记录过则记录下来（char[] 的默认值是 '\u0000'，其十进制值为0）
+        for (int i = 0; i < s.length(); i++) {        // 只需遍历一遍即可
+            char sc = s.charAt(i), tc = t.charAt(i);  // 取两个字符串相同索引上的字符
+            if (sMap[sc] == 0 && tMap[tc] == 0) {     // 若两字符的频率还未被记录过则记录下来（char[] 的默认值是'\u0000'，即十进制的0）
                 sMap[sc] = tc;
                 tMap[tc] = sc;
             }
-            else if (sMap[sc] != tc || tMap[tc] != sc)  // 若已有记录，但映射关系与之前记录的不一致，则说明不是 isomorphic 的
+            else if (sMap[sc] != tc || tMap[tc] != sc)  // 若已记录过，但记录中的频率不一样，则说明不是 isomorphic
                 return false;
         }
 
@@ -37,16 +37,16 @@ public class L205_IsomorphicStrings {
 
     /*
      * 解法2：单查找表
-     * - 思路：对于 s 和 t 中的每个字符 s[i]、t[i] 来说，只需要知道他们上次出现时的索引相同即可判断 s 和 t 是 isomorphic 的。
+     * - 思路：对于 s 和 t 中的每个字符 s[i]、t[i] 来说，若他们上次在字符串中出现的索引相同，即说明 s 和 t 是 isomorphic 的。
      * - 实现：一个查找表分成两部分使用，两部分分别记录 s[i] -> i 和 t[i] -> i 的映射。
      * - 时间复杂度 O(n)，空间复杂度 O(2len(charset))。
      * */
     public static boolean isIsomorphic2(String s, String t) {
-        int[] map = new int[256];                               // 无法用 Map 实现
+        int[] map = new int[256];      // ∵ 要分成两部分使用 ∴ 无法用 Map 实现
         for (int i = 0; i < s.length(); i++) {
             if (map[s.charAt(i)] != map[t.charAt(i) + 128])
                 return false;
-            map[s.charAt(i)] = map[t.charAt(i) + 128] = i + 1;  // i+1 是因为要避免0，因为 int[] 的默认值是0
+            map[s.charAt(i)] = map[t.charAt(i) + 128] = i + 1;  // 记录索引（+1 是因为要避免0，因为 int[] 的默认值是0）
         }
         return true;
     }
@@ -56,10 +56,5 @@ public class L205_IsomorphicStrings {
         log(isIsomorphic("paper", "title"));  // expects true
         log(isIsomorphic("foo", "bar"));      // expects false
         log(isIsomorphic("ab", "aa"));        // expects false
-
-        log(isIsomorphic2("egg", "add"));      // expects true
-        log(isIsomorphic2("paper", "title"));  // expects true
-        log(isIsomorphic2("foo", "bar"));      // expects false
-        log(isIsomorphic2("ab", "aa"));        // expects false
     }
 }
