4cf3c63bd14cc324425a72ed243b4d6bc3928c1d
Max Jiang <maxjiang23326@gmail.com>
2019-07-11 17:22:32 +0800

    docs: update comments

--- src/main/java/HashTable/WiselyChooseKeyAndValue/L149_MaxPointsOnLine.java
+++ src/main/java/HashTable/WiselyChooseKeyAndValue/L149_MaxPointsOnLine.java
@@ -10,16 +10,17 @@ import static Utils.Helpers.log;
 * Max Points on a Line
 *
 * - Given n points on a 2D plane, find the maximum number of points that lie on the same straight line.
-*
-* - 思路：该问题的关键在于如何判断多个点在一条直线上。首先想到可以通过两点一线的斜率（slope）来判断。具体来说，遍历所
-*   有点的两两组合，并用 Map 记录斜率 -> 点个数的映射。但这种方式还有问题，即存在两条直线斜率 k 相等，但偏移量 b 不等
-*   的情况（y = kx + b），因此不能用一个 Map 统计所有斜率，需要为每个点单独统计，即为每个点创建一个 Map，记录该点与
-*   所有其他点连成的直线的斜率。
-*
-* - 时间复杂度 O(n^2)，空间复杂度 O(n)。
 * */
 
 public class L149_MaxPointsOnLine {
+    /*
+    * 解法1：
+    * - 思路：该问题的关键在于如何判断多个点在一条直线上。首先想到可以通过两点一线的斜率（slope）来判断。具体来说，遍历所
+    *   有点的两两组合，并用 Map 记录斜率 -> 点个数的映射。但这种方式还有问题，即存在两条直线斜率 k 相等，但偏移量 b 不等
+    *   的情况（y = kx + b），因此不能用一个 Map 统计所有斜率，需要为每个点单独统计，即为每个点创建一个 Map，记录该点与
+    *   所有其他点连成的直线的斜率。
+    * - 时间复杂度 O(n^2)，空间复杂度 O(n)。
+    * */
     public static int maxPoints(int[][] points) {
         if (points == null || points.length == 0) return 0;
         if (points.length <= 2) return points.length;
@@ -56,21 +57,19 @@ public class L149_MaxPointsOnLine {
     public static void main(String[] args) {
         /*
          *   ^
-         *   |
          *   |        o
          *   |     o
          *   |  o
          *   +------------->
          *   0  1  2  3  4
          * */
-        int[] p1 = new int[] {1, 1};
-        int[] p2 = new int[] {2, 2};
-        int[] p3 = new int[] {3, 3};
-//        log(maxPoints(new int[][] {p1, p2, p3}));  // expects 3
+        int[] p1 = new int[]{1, 1};
+        int[] p2 = new int[]{2, 2};
+        int[] p3 = new int[]{3, 3};
+        log(maxPoints(new int[][]{p1, p2, p3}));  // expects 3
 
         /*
          *   ^
-         *   |
          *   |  o
          *   |     o        o
          *   |        o
@@ -78,28 +77,28 @@ public class L149_MaxPointsOnLine {
          *   +------------------->
          *   0  1  2  3  4  5  6
          * */
-        int[] p4 = new int[] {1, 1};
-        int[] p5 = new int[] {3, 2};
-        int[] p6 = new int[] {5, 3};
-        int[] p7 = new int[] {4, 1};
-        int[] p8 = new int[] {2, 3};
-        int[] p9 = new int[] {1, 4};
-//        log(maxPoints(new int[][] {p4, p5, p6, p7, p8, p9}));  // expects 4
+        int[] p4 = new int[]{1, 1};
+        int[] p5 = new int[]{3, 2};
+        int[] p6 = new int[]{5, 3};
+        int[] p7 = new int[]{4, 1};
+        int[] p8 = new int[]{2, 3};
+        int[] p9 = new int[]{1, 4};
+        log(maxPoints(new int[][]{p4, p5, p6, p7, p8, p9}));  // expects 4
 
-        int[] q1 = new int[] {1, 1};
-        int[] q2 = new int[] {1, 1};
-        int[] q3 = new int[] {2, 2};
-        int[] q4 = new int[] {2, 2};
-        log(maxPoints(new int[][] {q1, q2, q3, q4}));  // expects 4.（重复点的情况）
+        int[] q1 = new int[]{1, 1};
+        int[] q2 = new int[]{1, 1};
+        int[] q3 = new int[]{2, 2};
+        int[] q4 = new int[]{2, 2};
+        log(maxPoints(new int[][]{q1, q2, q3, q4}));  // expects 4.（重复点的情况）
 
-        int[] q5 = new int[] {1, 1};
-        int[] q6 = new int[] {1, 1};
-        int[] q7 = new int[] {1, 1};
-        log(maxPoints(new int[][] {q5, q6, q7}));  // expects 3.（所有都是重复点的情况）
+        int[] q5 = new int[]{1, 1};
+        int[] q6 = new int[]{1, 1};
+        int[] q7 = new int[]{1, 1};
+        log(maxPoints(new int[][]{q5, q6, q7}));  // expects 3.（所有都是重复点的情况）
 
-        int[] r1 = new int[] {0, 0};
-        int[] r2 = new int[] {94911151, 94911150};
-        int[] r3 = new int[] {94911152, 94911151};
-        log(maxPoints(new int[][] {r1, r2, r3}));  // expects 2.（若使用 double 计算斜率则 r1->r2 的直线与 r1->r3 的直线斜率会相等，导致认为这三点在一条线上）
+        int[] r1 = new int[]{0, 0};
+        int[] r2 = new int[]{94911151, 94911150};
+        int[] r3 = new int[]{94911152, 94911151};
+        log(maxPoints(new int[][]{r1, r2, r3}));  // expects 2.（若使用 double 计算斜率则 r1->r2 的直线与 r1->r3 的直线斜率会相等，导致认为这三点在一条线上）
     }
 }
