c07f4d9438cd3c27adfbeb8878d02db3e2794401
Max Jiang <maxjiang23326@gmail.com>
2019-07-12 14:48:04 +0800

    docs: update comments

--- src/main/java/LinkedList/ClassicInterweaving/L83_RemoveDuplicatesFromSortedList.java
+++ src/main/java/LinkedList/ClassicInterweaving/L83_RemoveDuplicatesFromSortedList.java
@@ -8,15 +8,18 @@ import static Utils.Helpers.*;
 * Remove Duplicates from Sorted List
 *
 * - 为有序链表去重。
-* - 时间复杂度 O(n)，空间复杂度 O(1)。
 * */
 
 public class L83_RemoveDuplicatesFromSortedList {
+    /*
+     * 解法1：
+     * - 时间复杂度 O(n)，空间复杂度 O(1)。
+     * */
     public static ListNode deleteDuplicates(ListNode head) {
         ListNode curr = head;
         while (curr != null && curr.next != null) {
             if (curr.val == curr.next.val) {
-                curr.next = curr.next.next;
+                curr.next = curr.next.next;  // 跳过 curr.next 节点，将 curr 和 curr.next.next 相连
             } else {
                 curr = curr.next;
             }
