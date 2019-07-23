ae9b1752633dc8ee1fcbac48a608ff6ea857bbe9
Max Jiang <maxjiang23326@gmail.com>
2019-07-23 13:58:32 +0800

    docs: improve comments

--- src/main/java/StackAndQueue/BasicsOfStack/L150_EvaluateReversePolishNotation.java
+++ src/main/java/StackAndQueue/BasicsOfStack/L150_EvaluateReversePolishNotation.java
@@ -1,7 +1,6 @@
 package StackAndQueue.BasicsOfStack;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.*;
 import java.util.function.BiFunction;
 
 import static Utils.Helpers.*;
@@ -9,7 +8,7 @@ import static Utils.Helpers.*;
 /*
 * Evaluate Reverse Polish Notation
 *
-* - Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+* - Evaluate the value of an arithmetic expression in Reverse Polish Notation (RPN, 逆波兰表示法).
 *   1. Valid operators are +, -, *, /.
 *   2. Each operand may be an integer or another expression.
 *   3. The given RPN expression is always valid. That means the expression would always evaluate to a result
@@ -23,27 +22,24 @@ public class L150_EvaluateReversePolishNotation {
     * */
     public static int evalRPN(String[] tokens) {
         Deque<String> stack = new ArrayDeque<>();
+        Set<String> operators = new HashSet<>(Arrays.asList("+", "-", "*", "/"));
 
         for (String s : tokens) {
-            if (isOperator(s)) {
+            if (operators.contains(s)) {
               int operand2 = Integer.parseInt(stack.pop());
-              int operand1 = Integer.parseInt(stack.pop());
+              int operand1 = Integer.parseInt(stack.pop());  // String -> Integer
               stack.push(calculate(operand1, operand2, s));
             } else {
                 stack.push(s);
             }
         }
 
-        return Integer.parseInt(stack.pop());
-    }
-
-    private static boolean isOperator(String s) {
-        return s.equals("+") || s.equals("-") || s.equals("*") || s.equals("/");
+        return Integer.parseInt(stack.pop());  // String -> Integer
     }
 
     private static String calculate(int operand1, int operand2, String operator) {
         switch (operator) {
-            case "+": return String.valueOf(operand1 + operand2);
+            case "+": return String.valueOf(operand1 + operand2);  // Integer -> String
             case "-": return String.valueOf(operand1 - operand2);
             case "*": return String.valueOf(operand1 * operand2);
             case "/": return String.valueOf(operand1 / operand2);
@@ -52,14 +48,17 @@ public class L150_EvaluateReversePolishNotation {
     }
 
     /*
-    * 解法2：Stack（lambda 版）
+    * 解法2：Stack（FP 版）
+    * - 改进：
+    *   1. stack 中存的是 Integer 而非解法1中的 String，这样避免了需要进行3次类型转化的麻烦；
+    *   2. 先根据 operator 得到 function，再 apply 参数；
     * - 时间复杂度 O(n)，空间复杂度 O(n)。
     * */
     public static int evalRPN2(String[] tokens) {
         Deque<Integer> stack = new ArrayDeque<>();
 
         for (String s : tokens) {
-            BiFunction<Integer, Integer, Integer> f = getFunction(s);
+            BiFunction<Integer, Integer, Integer> f = getFunction(s);  // 接受2个参数的 function，更多函数式接口 SEE: http://ocpj8.javastudyguide.com/ch10.html
             if (f != null) {
                 int operand2 = stack.pop();
                 int operand1 = stack.pop();
@@ -83,11 +82,11 @@ public class L150_EvaluateReversePolishNotation {
     }
 
     public static void main(String[] args) {
-        log(evalRPN2(new String[]{"2", "1", "+", "3", "*"}));   // expects 9. ((2 + 1) * 3) = 9
+        log(evalRPN(new String[]{"2", "1", "+", "3", "*"}));   // expects 9. ((2 + 1) * 3) = 9
 
-        log(evalRPN2(new String[]{"4", "13", "5", "/", "+"}));  // expects 6. (4 + (13 / 5)) = 6
+        log(evalRPN(new String[]{"4", "13", "5", "/", "+"}));  // expects 6. (4 + (13 / 5)) = 6
 
-        log(evalRPN2(new String[]{"10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"}));
+        log(evalRPN(new String[]{"10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"}));
         // expects 22.
         // ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
         //= ((10 * (6 / (12 * -11))) + 17) + 5
