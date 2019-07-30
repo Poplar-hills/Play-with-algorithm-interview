c1cf48c845fcf5d246634f06f5dbbe1dcc03ca56
Max Jiang <maxjiang23326@gmail.com>
2019-07-30 21:55:29 +0800

    docs: update comments

--- src/main/java/StackAndQueue/StackAndRecursion/L144_BinaryTreePreorderTraversal.java
+++ src/main/java/StackAndQueue/StackAndRecursion/L144_BinaryTreePreorderTraversal.java
@@ -59,7 +59,6 @@ public class L144_BinaryTreePreorderTraversal {
      * */
     public static List<Integer> preorderTraversal3(TreeNode root) {
         List<Integer> res = new ArrayList<>();
-        if (root == null) return res;
         Deque<TreeNode> stack = new ArrayDeque<>();
         TreeNode curr = root;
 
@@ -85,11 +84,11 @@ public class L144_BinaryTreePreorderTraversal {
         TreeNode curr = root;
 
         while (curr != null || !stack.isEmpty()) {
-            if (curr != null) {
+            if (curr != null) {  // 同样也是先往左走到底
                 res.add(curr.val);
                 stack.push(curr);
                 curr = curr.left;
-            } else {
+            } else {             // 再转向右一步
                 curr = stack.pop();
                 curr = curr.right;
             }
@@ -99,20 +98,20 @@ public class L144_BinaryTreePreorderTraversal {
     }
 
     /*
-    * 解法5：迭代
-    * - 思路：模拟系统栈的指令 —— 解法1的递归过程可以抽象为：1.访问节点值 2.遍历左子节点 3.遍历右子节点。这个过程若用栈来模拟：
+    * 解法5：迭代（模拟指令）
+    * - 思路：使用栈来模拟树的遍历能写出解法2，但解法2是在栈中存储节点。若在栈中存入节点的同时还存入对该节点的操作指令（遍历或访问），
+    *   则可以得到另一种解法。例如：
     *               5       |      |       |      |
     *            /    \     |      |       |______|
-    *          3       8    |      |  -->  |_v__5_| 访问5
-    *        /  \     /     |______|       |_i__3_| 遍历左子节点3
-    *       1    4   6      |_i__5_|       |_i__8_| 遍历右子节点8
-    *   根据这种模拟能写出解法2，但解法2的实现是只在栈中存储节点。若我们在栈中存节点的同时还存入对该节点的操作指令（遍历或访问），
-    *   即将节点和操作 pair 起来存储，从而可以得到另一种解法。
+    *          3       8    |      |  -->  |_v__5_| 访问节点5
+    *        /  \     /     |______|       |_i__3_| 遍历节点3
+    *       1    4   6      |_i__5_|       |_i__8_| 遍历节点8
+    *   首先入栈1条指令"遍历节点5"，执行这条指令时相应要入栈"遍历节点8"、"遍历节点3"、"访问节点5"这3条指令。
     * - 优势：这种解法虽然繁琐一点，但是更加灵活，只需极少的改动即可变为中序或后续遍历（SEE: L94 的解法4、L145 的解法4）。
     * - 时间复杂度 O(n)，空间复杂度 O(h)，其中 h 是树高。
     * */
-    static class Command {  // 将节点和指令的 pair 抽象成一个 Command
-        String type;        // type 的取值只能为 "iterate" or "visit"（遍历或访问）（更好的方式是用枚举）
+    static class Command {  // 将节点和指令的组合抽象成 Command（对什么节点做什么事）
+        String type;        // type 取值只能为 "iterate" or "visit"（更好的方式是用枚举）
         TreeNode node;
         Command(String type, TreeNode node) {
             this.type = type;
@@ -122,16 +121,16 @@ public class L144_BinaryTreePreorderTraversal {
 
     public static List<Integer> preorderTraversal5(TreeNode root) {
         List<Integer> res = new ArrayList<>();
-        Deque<Command> stack = new ArrayDeque<>();   // 栈中存的是 Command（将节点和指令的 pair）
         if (root == null) return res;
-
+        Deque<Command> stack = new ArrayDeque<>();   // 栈中存的是 Command
         stack.push(new Command("iterate", root));
+
         while (!stack.isEmpty()) {
             Command cmd = stack.pop();
             TreeNode curr = cmd.node;
             if (cmd.type.equals("visit"))
                 res.add(curr.val);
-            else {                          // 若是 "iterate"
+            else {                  // 若 type 是 "iterate"
                 if (curr.right != null)
                     stack.push(new Command("iterate", curr.right));
                 if (curr.left != null)
@@ -145,12 +144,12 @@ public class L144_BinaryTreePreorderTraversal {
 
     public static void main(String[] args) {
         TreeNode t1 = createBinaryTreeDepthFirst(new Integer[]{1, null, 2, 3});
-        log(preorderTraversal5(t1));  // expects [1, 2, 3]
+        log(preorderTraversal3(t1));  // expects [1, 2, 3]
 
         TreeNode t2 = createBinaryTreeDepthFirst(new Integer[]{});
-        log(preorderTraversal5(t2));  // expects []
+        log(preorderTraversal3(t2));  // expects []
 
         TreeNode t3 = createBinaryTreeDepthFirst(new Integer[]{5, 3, 1, null, null, 4, null, null, 7, 6});
-        log(preorderTraversal5(t3));  // expects [5, 3, 1, 4, 7, 6]
+        log(preorderTraversal3(t3));  // expects [5, 3, 1, 4, 7, 6]
     }
 }
