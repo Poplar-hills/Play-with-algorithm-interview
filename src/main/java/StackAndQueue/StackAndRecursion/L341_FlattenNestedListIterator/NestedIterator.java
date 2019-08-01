97a2d8505e0d871fe0f96e0262f625af787d6aba
Max Jiang <maxjiang23326@gmail.com>
2019-08-01 11:28:52 +0800

    feat: add a solution for L341

--- src/main/java/StackAndQueue/StackAndRecursion/L341_FlattenNestedListIterator/NestedIterator.java
+++ src/main/java/StackAndQueue/StackAndRecursion/L341_FlattenNestedListIterator/NestedIterator.java
@@ -9,18 +9,25 @@ import java.util.*;
 * - Each element is either an integer, or a list -- whose elements may also be integers or other lists.
 *
 * - Your NestedIterator object will be instantiated and called as such:
-*     NestedIterator i = new NestedIterator(nestedList);
-*     while (i.hasNext())
-*         doSomethingWith(i.next());
+*       NestedIterator i = new NestedIterator(nestedList);
+*       while (i.hasNext())
+*           doSomethingWith(i.next());
 *
-* - Example 1: Input: [[0,1],2,[3,4]], Output: [0,1,2,3,4].
-*   Example 2: Input: [1,[4,[6]]],     Output: [1,4,6].
+* - Example 1: Input: [0,[[1,2],3],[4,[5,6]]]  Output: [0,1,2,3,4,5,6]
+*   Example 2: Input: [1,[4,[6]]]              Output: [1,4,6]
 * */
 
 
 /*
- * 解法1：Eager approach
- * - 缺点：对于大数据集存在性能问题 —— we are pre-computing and pre-loading everything into the queue, which is a big waste of resource.
+ * 解法1：Eager + recursive
+ * - 思路：
+ *   1. Eager 的 iterator 会在实例化时一次性将整个 nestedList 解析成可用的 integer list。这样每次调 hasNext/next
+ *     方法时就可以直接检查/获得 integer list 中的下一个 integer 即可；
+ *   2. ∵ iterator 的输出顺序应该与输入数据一致，只是将数据中的层级拍平而已 ∴ iterator 的基本数据结构应该是 queue；
+ *   3. ∵ 输入数据中可能有无限层级的嵌套，这是个典型的递归结构 ∴ iterator 解析 nestedList 的逻辑应该是递归直到找到下
+ *      一个 integer 为止。
+ * - 缺点：对于大数据集存在性能问题 —— we are pre-computing and pre-loading everything into the queue, which
+ *   is a big waste of resource.
  * */
 class NestedIterator implements Iterator<Integer> {
     private Queue<Integer> queue = new LinkedList<>();
@@ -30,7 +37,7 @@ class NestedIterator implements Iterator<Integer> {
     }
 
     private void addToQueue(List<NestedInteger> nestedList) {
-        for (NestedInteger n : nestedList) {
+        for (NestedInteger n : nestedList) {  // 递归遍历（其实也是 DFT）
             if (n.isInteger())
                 queue.offer(n.getInteger());
             else
@@ -45,10 +52,42 @@ class NestedIterator implements Iterator<Integer> {
     public boolean hasNext() { return !queue.isEmpty(); }
 }
 
+/*
+ * 解法2：Eager + iterative
+ * - 思路：解法1的迭代版
+ * */
+class NestedIterator2 implements Iterator<Integer> {
+    private Queue<Integer> queue = new LinkedList<>();
+
+    public NestedIterator2(List<NestedInteger> nestedList) {
+        Stack<NestedInteger> stack = new Stack<>();  // 模拟解法1中的递归栈
+        pushInReverseOrder(nestedList, stack);
+
+        while (!stack.isEmpty()) {
+            NestedInteger n = stack.pop();
+            if (n.isInteger())
+                queue.offer(n.getInteger());
+            else
+                pushInReverseOrder(n.getList(), stack);
+        }
+    }
+
+    private void pushInReverseOrder(List<NestedInteger> list, Stack<NestedInteger> stack) {
+        for (int i = list.size() - 1; i >= 0; i--)
+            stack.push(list.get(i));
+    }
+
+    @Override
+    public Integer next() { return queue.poll(); }
+
+    @Override
+    public boolean hasNext() { return !queue.isEmpty(); }
+}
 
 /*
- * 解法2：Lazy approach
- * - 实现：使用迭代而非递归。
+ * 解法3：Lazy approach
+ * - 思路：Lazy 的 iterator 会弱化实例化时的处理逻辑，只拿到 nestedList 的引用即可。主要逻辑放在 hasNext/next 方法中，
+ *   在调用时再开始从 nestedList 中解析出下一个 integer。
  *
  * - 总结：Queue 是尾进头出： poll <-- [1|2|3|4|5] <-- offer
  *
@@ -59,10 +98,10 @@ class NestedIterator implements Iterator<Integer> {
  *                         poll <-- [1|2|3|4|5] <-- offer         push --> [5|4|3|2|1]
  *                                                                 pop <--
  * */
-class NestedIterator2 implements Iterator<Integer> {
+class NestedIterator3 implements Iterator<Integer> {
     private Deque<NestedInteger> dq = new ArrayDeque<>();  // Deque 接口的实现可以是 ArrayDeque 也可以是 LinkedList
 
-    public NestedIterator2(List<NestedInteger> nestedList) {
+    public NestedIterator3(List<NestedInteger> nestedList) {
         for (NestedInteger n : nestedList)
             dq.offer(n);    // 从尾部添加第一层的所有 NestedInteger
     }
