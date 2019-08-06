6ddefaeeeb695e10950b4696880bbe96d19674dc
Max Jiang <maxjiang23326@gmail.com>
2019-08-07 07:17:43 +0800

    test: add a test case for L64

--- src/main/java/DP/L64_MinimumPathSum.java
+++ src/main/java/DP/L64_MinimumPathSum.java
@@ -6,20 +6,25 @@ import static Utils.Helpers.log;
 * Minimum Path Sum
 *
 * - Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right which minimizes
-*   the sum of all numbers along its path.
+*   the sum of all numbers along its path. Returns the sum at the end.
 * - Note: You can only move either down or right at any point in time.
 * */
 
 public class L64_MinimumPathSum {
     public static int minPathSum(int[][] grid) {
+
         return 0;
     }
 
     public static void main(String[] args) {
-        log(minPathSum((new int[][]{
+        log(minPathSum(new int[][]{
             new int[]{1, 3, 1},
             new int[]{1, 5, 1},
             new int[]{4, 2, 1}
-        })));  // expects 7. (1 -> 3 -> 1 -> 1 -> 1)
+        }));  // expects 7. (1->3->1->1->1)
+
+        log(minPathSum(new int[][]{
+            new int[]{0}
+        }));  // expects 0.
     }
 }
