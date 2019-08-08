79bced333c5bc95c8eb897c68502e2a73342c426
Max Jiang <maxjiang23326@gmail.com>
2019-08-08 20:17:08 +0800

    feat: add a solution for L343

--- src/main/java/DP/L343_IntegerBreak.java
+++ src/main/java/DP/L343_IntegerBreak.java
@@ -2,6 +2,8 @@ package DP;
 
 import static Utils.Helpers.log;
 
+import java.util.Arrays;
+
 /*
 * Integer Break
 *
@@ -10,11 +12,49 @@ import static Utils.Helpers.log;
 * */
 
 public class L343_IntegerBreak {
+    /*
+    * 解法1：
+    * - 思路：
+    *   1. 因为需要将 n 分割成几份是未知的，因此很难使用循环解决（不知道需要几重循环），需要使用递归解决（只要设置好终止条件，
+    *      其余的就交给递归即可）。
+    *   2. 因此可以对 n 进行递归分割（动画讲解 SEE: https://coding.imooc.com/lesson/82.html#mid=2953，2'38''）：
+    *                            分割4
+    *                    1+?/    2+?|    3+?\
+    *                  分割3      分割2      分割1
+    *              1+?/  2+?\   1+?|
+    *             分割2    分割1   分割1
+    *            1+?|
+    *             分割1
+    *      - 要分割4有三种方案：1+?、2+?、3+?（其中?代表分割出来的一个或多个整数），最后只需从其中选出各整数乘积最大的即可；
+    *      - 对于方案 1+? 来说，?中的整数之和为3，而3又有两种分割方案：1+?、2+?；
+    *      - 这样下去直到每部分都分割成最小整数1时递归结束。
+    *   3. 在不断分割过程中出现了很多重叠子问题（如分割2），这就可以使用 memoization 或 tabulation 进行优化：
+    *      - 递归分解问题 + memoization 其实就是 top-down DP；
+    *      - 从小到大解决问题（即 tabulation）其实就是 bottom-up DP。
+    *
+    * - 原理：之所以可以使用这样的递归结构解决问题，是因为该题是一个求最优解的问题，而求最优解的问题只要可以分解，它就符合
+    *   “最优子结构”性质，即“通过求子问题的最优解，可以获得原问题的最优解”。
+    * */
     public static int integerBreak(int n) {
-        return 0;
+        assert n >= 2;               // ∵ 题中要求 n 至少分要被割成两部分 ∴ 要 >= 2
+        int[] cache = new int[n + 1];
+        Arrays.fill(cache, -1);
+        return integerBreak(n, cache);
+    }
+
+    private static int integerBreak(int n, int[] cache) {
+        if (n == 1) return 1;
+        if (cache[n] != -1) return cache[n];
+
+        int res = -1;
+        for (int i = 1; i < n; i++)  // 每次将 n 分割成 i 和 n-i 两部分
+            res = Math.max(res, Math.max(i * (n - i), i * integerBreak(n - i)));  // 求三者中的最大值
+
+        return cache[n] = res;
     }
     public static void main(String[] args) {
-        log(integerBreak(2));   // expects 1. (2 = 1 + 1, 1 × 1 = 1)
+        log(integerBreak(4));   // expects 4.  (4 = 2 + 2, 2 × 2 = 4)
+        log(integerBreak(2));   // expects 1.  (2 = 1 + 1, 1 × 1 = 1)
         log(integerBreak(10));  // expects 36. (10 = 3 + 3 + 4, 3 × 3 × 4 = 36)
     }
 }
