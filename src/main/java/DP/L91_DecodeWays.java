c017f943b5c7415887c3d569ee6eb261c5b5f9f3
Max Jiang <maxjiang23326@gmail.com>
2019-08-11 00:10:36 +0800

    docs: update comments

--- src/main/java/DP/L91_DecodeWays.java
+++ src/main/java/DP/L91_DecodeWays.java
@@ -28,12 +28,12 @@ public class L91_DecodeWays {
     *                     num("3")      num("")                  1       1
     *                        ↓                                   ↑
     *                     num("")                                1
-    *   4. ∵ 分解过程中有多个重叠子问题 ∴ 可以采用 memoization 优化。需要记忆的内容是 {索引: num}。
+    *   4. ∵ 分解过程中有多个重叠子问题 ∴ 可以采用 memoization 优化，需要记忆的是 { i: num(从 i 开始的字符串) }。
     * - 时间复杂度 O(n)，空间复杂度 O(n)。
     * */
     public static int numDecodings(String s) {
         if (s == null || s.length() == 0) return 0;
-        return dfs(s, 0, new HashMap<>());  // 第2个参数是指向待解码的字符，从第0个字符开始
+        return dfs(s, 0, new HashMap<>());  // 第2个参数是指向待解码字符串第0个字母的指针（cache 采用 Map，使用数组也可以）
     }
 
     public static int dfs(String s, int i, HashMap<Integer, Integer> map) {
@@ -51,20 +51,22 @@ public class L91_DecodeWays {
 
     /*
     * 解法2：DP
-    *
-    *
+    * - 思路：DP 与解法1中 recursion 的思路一致 —— 每个以指针 i 为起点的字符串的解码方法数 num(s, i) = num(s, i+1) + num(s, i+2)；
+    *   只是实现方式相反 —— recursion 是从前往后递归，而 DP 从后往前遍历，前一个的解是建立在后两个的解的基础上。
+    * - 时间复杂度 O(n)，空间复杂度 O(n)。
     * */
     public static int numDecodings2(String s) {
         if (s == null || s.length() == 0) return 0;
         int n = s.length();
-        int[] cache = new int[n + 1];
-        cache[n] = 1;
+        int[] cache = new int[n + 1];   // ∵ cache 大小是已知的 ∴ 可以采用数组（否则只能采用 Map）
+        cache[n] = 1;                   // 先解答最小问题，即 num("") 的情况
 
         for (int i = n - 1; i >= 0; i--) {
-            if (s.charAt(i) == '0') cache[i] = 0;
+            if (s.charAt(i) == '0')
+                cache[i] = 0;
             else {
                 cache[i] = cache[i + 1];
-                if (i + 1 < n && (s.charAt(i) == '1' || (s.charAt(i) == '2' && s.charAt(i + 1) < '7')))
+                if (i + 1 < n && Integer.parseInt(s.substring(i, i + 2)) < 27)
                     cache[i] += cache[i + 2];
             }
         }
@@ -73,9 +75,9 @@ public class L91_DecodeWays {
     }
 
     public static void main(String[] args) {
-        log(numDecodings("12"));     // expects 2. "AB" (1 2) or "L" (12)
-        log(numDecodings("227"));    // expects 2. "VG" (22 7), or "BBG" (2 2 7)
-        log(numDecodings("226"));    // expects 3. "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6)
-        log(numDecodings("102213")); // expects 5. ...
+        log(numDecodings2("12"));     // expects 2. "AB" (1 2) or "L" (12)
+        log(numDecodings2("227"));    // expects 2. "VG" (22 7), or "BBG" (2 2 7)
+        log(numDecodings2("226"));    // expects 3. "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6)
+        log(numDecodings2("102213")); // expects 5. ...
     }
 }
