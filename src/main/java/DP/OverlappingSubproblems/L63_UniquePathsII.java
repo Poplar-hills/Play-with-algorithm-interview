cdd76883c89d3dd665cae40e0544d05af4d0c6ae
Max Jiang <maxjiang23326@gmail.com>
2019-08-12 07:27:27 +0800

    feat: add a solution for L63

--- src/main/java/DP/OverlappingSubproblems/L63_UniquePathsII.java
+++ src/main/java/DP/OverlappingSubproblems/L63_UniquePathsII.java
@@ -2,10 +2,6 @@ package DP.OverlappingSubproblems;
 
 import static Utils.Helpers.log;
 
-import java.util.LinkedList;
-import java.util.Queue;
-import javafx.util.Pair;
-
 /*
 * Unique Paths II
 *
@@ -16,10 +12,35 @@ public class L63_UniquePathsII {
     /*
     * 解法1：
     * - 思路：
+    *       ■ → ■ → ■         2 ← 1 ← 1
+    *       ↓       ↓         ↑       ↑
+    *       ■   □   ■   -->   1   0   1
+    *       ↓       ↓         ↑       ↑
+    *       ■ → ■ → ■         1 ← 1 ← 1
     * - 时间复杂度 O()，空间复杂度 O()。
     * */
     public static int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        return 0;
+        if (obstacleGrid == null || obstacleGrid[0] == null)
+            return 0;
+        int[][] cache = new int[obstacleGrid.length][obstacleGrid[0].length];
+        return uniquePathsWithObstacles(obstacleGrid, 0, 0, cache);
+    }
+
+    private static int uniquePathsWithObstacles(int[][] grid, int i, int j, int[][] cache) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        if (grid[i][j] == 1) return 0;
+        if (i == m - 1 && j == n - 1) return 1;
+        if (cache[i][j] != 0) return cache[i][j];
+
+        int res = 0;
+        if (i + 1 < m)
+            res += uniquePathsWithObstacles(grid, i + 1, j, cache);
+        if (j + 1 < n)
+            res += uniquePathsWithObstacles(grid, i, j + 1, cache);
+
+        return cache[i][j] = res;
     }
 
     public static void main(String[] args) {
@@ -28,5 +49,9 @@ public class L63_UniquePathsII {
             new int[]{0, 1, 0},
             new int[]{0, 0, 0},
         }));  // expects 2. (R->R->D->D, D->D->R->R)
+
+        log(uniquePathsWithObstacles(new int[][]{
+            new int[]{1},
+        }));  // expects 0.
     }
 }
