a5388a059c0bd1020582dfd66168202c62c95530
Max Jiang <maxjiang23326@gmail.com>
2019-08-20 18:54:53 +0800

    refactor: rename

--- src/main/java/DP/KnapsackProblem/ZeroOneKnapsack.java
+++ src/main/java/DP/KnapsackProblem/ZeroOneKnapsack.java
@@ -18,8 +18,8 @@ import java.util.Arrays;
 *     - 总时间复杂度为 O((2^n)*n)。
 *
 *   - Greedy Algorithm：
-*     采用贪心算法（优先放入价值最高的物品）对于解决这类问题存在缺陷：∵ 本问题是要求的是一个“全局最优解”，而贪心算法是只顾眼前的选
-*     择策略 ∴ 很容易举出反例：
+*     采用贪心算法（优先放入性价比最高的物品）对于解决这类问题存在缺陷：∵ 本问题是要求的是一个“全局最优解”，而贪心算法是只顾眼前的
+*     选择策略 ∴ 很容易举出反例：
 *                           Item       0     1     2
 *                           Weight     1     2     3
 *                           Value      6     10    12
@@ -28,7 +28,7 @@ import java.util.Arrays;
 *     量就被浪费掉了。而事实上，全局最优解应该是放入1、2号物品（价值22，占用5的容量），因此贪心算法对本问题不可行。
 *
 *   - DP：
-*     若采用 DP 则需要认清该问题中的状态有哪些，以及它们之间是如何转移的，即写出状态转移方程：
+*     若采用 DP 则需要认清该问题中的状态有哪些，以及它们之间是如何转移的，即写出状态转移方程（递推表达式）：
 *     1. 首先状态可以通过题中的约束条件来识别：
 *        1. 每次从 i 个物品中找出最佳价值组合（i 是变量，每次放/不放一个物品后 i 都会-1）；
 *        2. 背包的剩余容量 j（每次放入一个物品后 j 都会减小）；
@@ -57,18 +57,18 @@ public class ZeroOneKnapsack {
         int[][] cache = new int[n][c + 1];  // ∵ 状态转移方程有两个输入变量 ∴ 缓存也是二维的（n 行 c+1 列；c 的取值范围是 [0,c]）
         for (int[] row : cache)
             Arrays.fill(row, -1);           // ∵ 要缓存的值可能有0 ∴ 都初始化为-1
-        return largestValue(n - 1, w, v, c, cache);
+        return largestValue(n - 1, c, w, v, cache);
     }
 
-    private static int largestValue(int i, int[] w, int[] v, int c, int[][] cache) {
-        if (i < 0 || c == 0) return 0;
-        if (cache[i][c] != -1) return cache[i][c];
+    private static int largestValue(int i, int j, int[] w, int[] v, int[][] cache) {
+        if (i < 0 || j == 0) return 0;
+        if (cache[i][j] != -1) return cache[i][j];
 
-        int res = largestValue(i - 1, w, v, c, cache);  // 不放入第 i 个的最大价值
-        if (c >= w[i])                                  // 若剩余容量充足则可以尝试放入第 i 个
-            res = Math.max(res, v[i] + largestValue(i - 1, w, v, c - w[i], cache));
+        int res = largestValue(i - 1, j, w, v, cache);  // 不放入第 i 个的最大价值
+        if (j >= w[i])                                  // 若剩余容量充足则可以尝试放入第 i 个
+            res = Math.max(res, v[i] + largestValue(i - 1, j - w[i], w, v, cache));
 
-        return cache[i][c] = res;
+        return cache[i][j] = res;
     }
 
     /*
