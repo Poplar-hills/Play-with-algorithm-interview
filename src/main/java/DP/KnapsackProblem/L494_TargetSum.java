366def1ee7da7d55e22e1cd9c01bb4157718ac87
Max Jiang <maxjiang23326@gmail.com>
2019-08-30 18:23:26 +0800

    feat: add a solution for L494

--- src/main/java/DP/KnapsackProblem/L494_TargetSum.java
+++ src/main/java/DP/KnapsackProblem/L494_TargetSum.java
@@ -92,7 +92,7 @@ public class L494_TargetSum {
     * - 思路：容量为 S，nums 中的每个元素都有 + 或 - 两种选择
     *   - 定义子问题：f(i, s) 表示"用前 i 个元素填充剩余容量 s 共有几种方式"；
     *   - 状态转移方程：f(i, s) = f(i-1, s-nums[i]) + f(i-1, s+nums[i])。
-    *   - 填表：对于 nums=[1,1,1,1], S=2 有：
+    *   - 填表验证：对于 nums=[1,1,1,1], S=2 有：
     *          v | i\s -3 -2 -1  0  1  2  3   (其中 s ∈ [-sum, sum])
     *          1 |  0   0  0  1  0  1  0  0
     *          1 |  1   0  1  0  2  0  1  0
@@ -124,8 +124,36 @@ public class L494_TargetSum {
         return dp[n - 1][S + sum];  // 注意最终要返回的是经过加/减 i 个元素得到 S 的结果 ∴ 应取第 S + sum 个元素，而非最后一个元素
     }
 
+    /*
+    * 解法4：DP + 一维数组
+    * - 思路：通过一点数学推导转化为0/1背包问题：设 nums 中加 + 的元素之和为 plusSum，加 - 的元素之和为 minusSum，则有：
+    *       plusSum + minusSum = sum
+    *       plusSum - minusSum = S
+    *   两边相加得到：2 * plusSum = S + sum，最终得到：plusSum = (S + sum) / 2，于是问题转化为：
+    *   “用 nums 中的元素填充 (S+sum)/2，共有几种选择方式能填满？”，这就是个典型的0/1背包问题了 —— 每个元素有放/不放两种选择。
+    * - 时间复杂度 O(n*sum)，空间复杂度 O(n*sum)。
+    * */
+    public static int findTargetSumWays5(int[] nums, int S) {
+        if (nums == null || nums.length == 0) return 0;
+
+        int sum = Arrays.stream(nums).reduce(0, Integer::sum);
+        if (S > sum || S < -sum) return 0;
+
+        int[] dp = new int[(S + sum) / 2 + 1];  // 从这往下就是标准的 _ZeroOneKnapsack 解法4的实现
+        for (int s = 0; s < dp.length; s++) {
+            if (s == 0) dp[s] += 1;             // 若容量为0，则结果至少为1（nums[0]=0 时为2，nums[0]!=0 时再看下面的条件是否满足）
+            if (nums[0] == s) dp[s] += 1;
+        }
+
+        for (int i = 1; i < nums.length; i++)
+            for (int s = dp.length - 1; s >= nums[i]; s--)
+                dp[s] += dp[s - nums[i]];
+
+        return dp[dp.length - 1];
+    }
+
     public static void main(String[] args) {
-        log(findTargetSumWays4(new int[]{1, 1, 1, 1}, 2));
+        log(findTargetSumWays5(new int[]{1, 1, 1, 1}, 2));
         /*
         * expects 4:
         *   -1+1+1+1 = 2
@@ -134,7 +162,7 @@ public class L494_TargetSum {
         *   +1+1+1-1 = 2
         * */
 
-        log(findTargetSumWays4(new int[]{2, 1, 1, 2}, 0));
+        log(findTargetSumWays5(new int[]{2, 1, 1, 2}, 0));
         /*
         *  expects 4:
         *    +2-1+1-2 = 0
@@ -143,7 +171,7 @@ public class L494_TargetSum {
         *    -2-1+1+2 = 0
         * */
 
-        log(findTargetSumWays4(new int[]{0, 0, 1}, 1));
+        log(findTargetSumWays5(new int[]{0, 0, 1}, 1));
         /*
         *  expects 4:
         *    +0+0+1 = 1
