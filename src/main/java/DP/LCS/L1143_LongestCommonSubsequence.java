537a1edd1b24b04b7e609b0e7db9f2ce51e5d7b0
Max Jiang <maxjiang23326@gmail.com>
2019-09-04 13:25:33 +0800

    refactor: simplify

--- src/main/java/DP/LCS/L1143_LongestCommonSubsequence.java
+++ src/main/java/DP/LCS/L1143_LongestCommonSubsequence.java
@@ -14,35 +14,43 @@ public class L1143_LongestCommonSubsequence {
     /*
     * 超时解：DFS
     * - 分析：与 L376 类似，该问题也有两个维度需要进行动态规划。因此：
-    *   - 子问题定义：f(m, n) 表示“s1[0..m] 和 s2[0..n] 两段字符串的 LCS 的长度”。
+    *   - 子问题定义：f(i, j) 表示“s1[i..] 和 s2[j..] 两段字符串的 LCS 的长度”。
     *   - 状态转移方程：∵ 每个字符都有两种状态 —— 在/不在 LCS 中：
-    *     - 若 s1[m] == s2[n]，即该字符在 LCS 中时：f(m, n) = 1 + f(m-1, n-1)，其中的1就是该字符；
-    *     - 若 s1[m] != s2[n]，即该字符不在 LCS 中时：f(m, n) = max(f(m-1, n), f(m, n-1))；即让两个字符串各自
-    *       收缩一个字符：s1[0..m-1]、s2[0..n] 与 s1[0..m]、s2[0..n-1]，看这两种情况里哪种情况的的 LCS 更长。
-    *                       ABCD|AEBD              - 此时 D == D，LCS 长度+1，同时收缩
+    *     - 若 s1[i] == s2[j]，即该字符在 LCS 中时：f(i, j) = 1 + f(i+1, j+1)，其中的1就是该字符；
+    *     - 若 s1[i] != s2[j]，即该字符不在 LCS 中时：f(i, j) = max(f(i+1, j), f(i, j+1))；即让两个字符串窗口各自
+    *       向右收缩一个字符，即 s1[i+1..]、s2[j..] 与 s1[i..]、s2[j+1..]，看这两种情况里哪种情况的的 LCS 更长。
+    *                       ABCD|AEBD              - 此时 A == A，LCS 长度+1，同时向右收缩
     *                           ↓
-    *                        ABC|AEB               - 此时 C != B，不在 LCS 中，各自收缩
+    *                        BCD|EBD               - 此时 B != E，不在 LCS 中，各自收缩
     *                       ↙       ↘
-    *                 AB|AEB        ABC|AE         - 左分支中 B == B，LCS 长度+1，同时收缩
-    *                   ↓          ↙       ↘
-    *                  A|AE     AB|AE     ABC|A    - 左分支中 A != E，不在 LCS 中，各自收缩
-    *                ↙     ↘     ...       ...
-    *             -|AE     A|A                     - 左左分支已收缩到底，左右分支中 A == A，LCS 长度+1，同时收缩
-    *                       ↓
-    *                      -|-                     - 左右分支已收缩到底
-    * - 时间复杂度 O(2^(m+n))，空间复杂度 O(m+n)。
+    *                 CD|EBD        BCD|BD         - 右分支中 B == B，LCS 长度+1，同时收缩
+    *               ↙       ↘          ↓
+    *            D|EBD     CD|BD     CD|D          - 右分支中 C != D，不在 LCS 中，各自收缩
+    *             ...       ...     ↙    ↘
+    *                             D|D    CD|-      - 右右分支已到底；右左分支 D == D，LCS 长度+1，同时收缩
+    *                              ↓
+    *                             -|-              - 右左分支已到底
+    *
+    * - 时间复杂度 O(2^(len1+len2))，空间复杂度 O(len1+len2)。
     * */
     public static int longestCommonSubsequence(String s1, String s2) {
-        if (s1 == null || s2 == null || s1.length() == 0 || s2.length() == 0)
-            return 0;
-        return helper(s1, s2, s1.length() - 1, s2.length() - 1);
+        if (s1 == null || s2 == null) return 0;
+        return helper(s1, s2, 0, 0);
     }
 
-    private static int helper(String s1, String s2, int m, int n) {
-        if (m < 0 || n < 0) return 0;
-        return s1.charAt(m) == s2.charAt(n)
-            ? 1 + helper(s1, s2, m - 1, n - 1)
-            : Math.max(helper(s1, s2, m - 1, n), helper(s1, s2, m, n - 1));
+    private static int helper(String s1, String s2, int i, int j) {
+        if (i == s1.length() || j == s2.length()) return 0;
+        return s1.charAt(i) == s2.charAt(j)
+            ? 1 + helper(s1, s2, i + 1, j + 1)
+            : Math.max(helper(s1, s2, i + 1, j), helper(s1, s2, i, j + 1));
+    }
+
+    /*
+    * 解法1：Recursion + Memoization
+    * -
+    * */
+    public static int longestCommonSubsequence1(String s1, String s2) {
+        return 0;
     }
 
     public static void main(String[] args) {
