01bd3535c5ed6d166c0d519c60be66b8bf806b52
Max Jiang <maxjiang23326@gmail.com>
2019-09-10 21:44:43 +0800

    docs: update comments

--- src/main/java/DP/StateTransition/L337_HouseRobberIII.java
+++ src/main/java/DP/StateTransition/L337_HouseRobberIII.java
@@ -30,15 +30,18 @@ public class L337_HouseRobberIII {
     /*
     * 解法1：
     * - 思路：该问题在二叉树上同样具有最优子结构性质 ∴ 可以以 bottom-up 的方式分析：先思考一个节点，对于每一个节点都可以偷或
-    *   不偷，因此有2种所得。而对其父节点来说：1.若偷父节点，则不能偷子节点；2.若不偷父节点，则最大所得为2个子节点各自的最大所得
+    *   不偷，因此有2种收获。而对其父节点来说：1.若偷父节点，则不能偷子节点；2.若不偷父节点，则最大收获为2个子节点各自的最大收获
     *   之和。以这样的方式一直推导到根节点记得到最终解。
     *           1                             12/15
-    *          / \    推导每个节点的2种所得      /    \      取根节点的最大所得
+    *          / \    推导每个节点的2种收获      /    \      取根节点的最大收获
     *         1   5    ---------------->    1/10   5/1    -------------->   15
     *        / \   \                       /   \     \
     *       5   5   1                    5/0   5/0   1/0
-    *   对左下角的5来说：若偷则所得为5，否则为0；对其父节点1来说：若偷则最大所得1，否则为10；对根节点1来说：若偷则最大所得为
-    *   1+10+1 = 12，若不偷则最大所得为 max(1,10) + max(5,1) = 15，因此最终解为15。
+    *   对左下角的5来说：若偷则收获为5，否则为0；对其父节点1来说：若偷则最大收获 0+0+1=1，否则为 5+5=10；对根节点1来说：若偷则
+    *   最大收获为 1+10+1=12，若不偷则最大收获为 max(1,10) + max(5,1) = 15，因此最终解为15。由此可见：
+    *     f(i) = max(y(i), n(i))，其中：
+    *       - y(i) = i.val + n(i.l) + n(i.r)；
+    *       - n(i) = max(y(i.l), n(i.l)) + max(y(i.r), n(i.r))。
     * - 时间复杂度 O(n)，空间复杂度 O(logn)，n 为节点个数。
     * */
     public static int rob(TreeNode root) {
@@ -53,8 +56,8 @@ public class L337_HouseRobberIII {
         int[] left = tryToRob(node.left);
         int[] right = tryToRob(node.right);
 
-        res[0] = node.val + left[1] + right[1];  // res[0] 记录若偷该节点得到的最大收获
-        res[1] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);  // res[1] 记录若不偷该节点得到的最大收获
+        res[0] = node.val + left[1] + right[1];  // res[0] 记录若偷该节点时的最大收获
+        res[1] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);  // res[1] 记录若不偷该节点时的最大收获
 
         return res;
     }
@@ -83,42 +86,13 @@ public class L337_HouseRobberIII {
         return res;
     }
 
-    /*
-    * 解法3：DP + Memoization
-    * - 思路：不同于解法2，本解法更 straight-forward，但也更冗长。加入的 Memoization 是以 TreeNode 为 key，因此需要在
-    *   TreeNode 类上 @override hashCode 和 equals 方法。
+	/*
+    * 解法3：DP
+    * - 思路：与解法2一致，但更精巧。
+    * - 实现：根据解法2中的 boolean 参数值将 helper2 方法分成了两个方法 robInclude 和 robExclude。
     * - 时间复杂度 O(n)，空间复杂度 O(logn)。
     * */
     public static int rob3(TreeNode root) {
-        if (root == null) return 0;
-        return helper3(root, new HashMap<>());  // ∵ 二叉树节点个数未知 ∴ 使用 map 实现 memoization
-    }
-
-    private static int helper3(TreeNode node, Map<TreeNode, Integer> memo) {
-        if (node == null) return 0;
-        if (node.left == null && node.right == null) return node.val;
-        if (memo.containsKey(node)) return memo.get(node);
-
-        int sum1 = node.val;    // 偷该节点时的最大收获
-        if (node.left != null)
-            sum1 += helper3(node.left.left, memo) + helper3(node.left.right, memo);
-        if (node.right != null)
-            sum1 += helper3(node.right.left, memo) + helper3(node.right.right, memo);
-
-        int sum2 = helper3(node.left, memo) + helper3(node.right, memo);  // 不偷该节点时的最大收获
-
-        int sum = Math.max(sum1, sum2);  // 得到子问题解
-        memo.put(node, sum);
-
-        return sum;
-	}
-
-	/*
-    * 解法4：非常聪明的解法
-    * - 思路：同样是基于每个节点可以偷或不偷进行递归。
-    * - 时间复杂度 O(n)，空间复杂度 O(logn)。
-    * */
-    public static int rob4(TreeNode root) {
         if (root == null) return 0;
         return Math.max(robInclude(root), robExclude(root));
     }
@@ -133,6 +107,36 @@ public class L337_HouseRobberIII {
         return rob2(node.left) + rob2(node.right);
     }
 
+    /*
+    * 解法4：DP + Memoization
+    * - 思路：不同于解法2，本解法更 straight-forward，但也更冗长。加入的 Memoization 是以 TreeNode 为 key，因此需要在
+    *   TreeNode 类上 @override hashCode 和 equals 方法。
+    * - 时间复杂度 O(n)，空间复杂度 O(logn)。
+    * */
+    public static int rob4(TreeNode root) {
+        if (root == null) return 0;
+        return helper4(root, new HashMap<>());  // ∵ 二叉树节点个数未知 ∴ 使用 map 实现 memoization
+    }
+
+    private static int helper4(TreeNode node, Map<TreeNode, Integer> memo) {
+        if (node == null) return 0;
+        if (node.left == null && node.right == null) return node.val;
+        if (memo.containsKey(node)) return memo.get(node);
+
+        int sum1 = node.val;    // 偷该节点时的最大收获
+        if (node.left != null)
+            sum1 += helper4(node.left.left, memo) + helper4(node.left.right, memo);
+        if (node.right != null)
+            sum1 += helper4(node.right.left, memo) + helper4(node.right.right, memo);
+
+        int sum2 = helper4(node.left, memo) + helper4(node.right, memo);  // 不偷该节点时的最大收获
+
+        int sum = Math.max(sum1, sum2);  // 得到子问题解
+        memo.put(node, sum);
+
+        return sum;
+	}
+
     public static void main(String[] args) {
         /*
         *      3
@@ -142,7 +146,7 @@ public class L337_HouseRobberIII {
         *      3   1
         * */
         TreeNode t1 = createBinaryTreeBreadthFirst(new Integer[]{3, 2, 2, null, 3, null, 1});
-        log(rob3(t1));  // expects 7. (3 + 3 + 1)
+        log(rob4(t1));  // expects 7. (3 + 3 + 1)
 
         /*
         *        1
@@ -152,7 +156,7 @@ public class L337_HouseRobberIII {
         *    1   1   1
         * */
         TreeNode t2 = createBinaryTreeBreadthFirst(new Integer[]{1, 5, 5, 1, 1, null, 1});
-        log(rob3(t2));  // expects 10. (5 + 5)
+        log(rob4(t2));  // expects 10. (5 + 5)
 
         /*
         *          5
@@ -164,7 +168,7 @@ public class L337_HouseRobberIII {
         *    5
         * */
         TreeNode t4 = createBinaryTreeBreadthFirst(new Integer[]{5, 1, null, 1, null, 5});
-        log(rob3(t4));  // expects 10. (5 + 5)
+        log(rob4(t4));  // expects 10. (5 + 5)
 
         /*
         *        1
@@ -174,6 +178,6 @@ public class L337_HouseRobberIII {
         *    5   5   1
         * */
         TreeNode t3 = createBinaryTreeBreadthFirst(new Integer[]{1, 1, 5, 5, 5, null, 1});
-        log(rob3(t3));  // expects 15. (5 + 5 + 5)
+        log(rob4(t3));  // expects 15. (5 + 5 + 5)
     }
 }
