14a9680bade17d3f03f947022756f0426a50dad0
Max Jiang <maxjiang23326@gmail.com>
2019-09-10 18:39:03 +0800

    refactor: rename

--- src/main/java/DP/StateTransition/L198_HouseRobber.java
+++ src/main/java/DP/StateTransition/L198_HouseRobber.java
@@ -16,7 +16,7 @@ import java.util.Arrays;
 * - ⭐ 总结：函数定义、状态、状态转移：
 *   1. 解法1采用了递归，其 tryToRob 方法用于“计算从某个范围内的房子中能偷得的最大收获”，这就是递归中的“函数定义”。明确合理的函数
 *      定义对于写出正确的递归逻辑至关重要。
-*   2. 解法2、3采用了 DP，而“函数定义”在 DP 中的对应概念是“状态”，例如“[0..n-1]内的最大收获”就是该问题的顶层状态，由于在该状态
+*   2. 解法2、3采用了 DP，而“函数定义”在 DP 中的对应概念是“状态”，例如“[0..n)内的最大收获”就是该问题的顶层状态，由于在该状态
 *      下采取了不同的行动（偷0号、偷1号……），该问题的状态发生了转移，产生了其他3个可能的状态。而描述清楚这些状态之间的转移方式（即
 *      明确的“状态转移方程”）对于写出正确的 DP 逻辑至关重要。例如，该问题的状态转移方程：
 *      f(0..n-1) = max(v(0)+f(1..n-1), v(1)+f(3..n-1), v(3)+f(5..n-1), ..., v(n-1))，其中 f 为“某区间内的最大收获”，
@@ -26,59 +26,66 @@ import java.util.Arrays;
 public class L198_HouseRobber {
     /*
     * 超时解：暴力破解
-    * - 思路：该题的本质是一个组合优化问题 —— 在所有房子中，哪几个房子的组合能满足条件：1.房子之间都不相邻 2.收益最大化。因此可以
-    *   遍历所有组合，从中筛出所有符合条件的组合，并从中找到最大的收益。
-    * - 时间复杂度 O((2^n)*n)，空间复杂度 O()。每个房子有2种可能（偷或不偷），n 个房子共有 2^n 种组合，因此遍历所有组合是
-    *   O(2^n) 操作；在所有组合中进行筛选，检查每种组合其是否符合"房子之间不相邻"的条件，这是 O(n) 操作，因此整体是 O((2^n)*n)。
+    * - 思路：该题的本质是一个组合优化问题 —— 在所有房子中，哪几个房子的组合能满足条件：1.房子之间各不相邻 2.收益最大化。
+    *   因此可以遍历所有房子的组合，从符合条件的组合中找到最大的收益。
+    * - 时间复杂度 O((2^n)*n)。∵ 每个房子有偷/不偷2种可能，n 个房子共有 2^n 种组合 ∴ 遍历所有组合就是 O(2^n) 操作；从所有
+    *   组合中筛出符合条件1的组合是 O(n) 操作 ∴ 整体是 O((2^n)*n)。
     * */
 
     /*
     * 解法1：Recursion + Memoization (DFS with cache)
-    * - 思路：∵ 该题的本质是一个组合优化问题 ∴ 并不需要求出所有的组合，只需要像 L91_DecodeWays 解法1那样对问题进行分解：
-    *                                            [0..n-1]内的最大收获
+    * - 思路：类似 L343_IntegerBreak 解法1那样对问题进行分解：设 max[0..n) 表示“从第 0~n-1 所房子中所能得到的最大收获”，则：
+    *                                           max[0..n)
     *                       偷0号/                    偷1号|            ...   偷n-1号\
-    *                  [2..n-1]内的最大收获        [3..n-1]内的最大收获            []内的最大收获
+    *                  max[2..n)        max[3..n)            max[]
     *             偷2号/   偷3号|     \           偷3号/         \
-    *     [4..n-1]内的最大收获   ...    ...  [5..n-1]内的最大收获   ...
-    *   这样的分解的含义：[0..n-1]内的最多收获 = Max((偷0号收获 + [2..n-1]内的最多收获), (偷1号收获 + [3..n-1]内的最多收获), ..., 偷n-1号收获)
+    *     [4..n)内的最大收获   ...    ...  [5..n)内的最大收获   ...
+    *
+    *   说明：要求 [0..n) 内的最大收获，第一次偷共有 n 种方案：偷0号、偷1号、……、偷n-1号。而这 n 种方案分别对应了 n 个子问题：
+    *
+    *
+    *
+    *
+    *这样的分解的含义：[0..n)内的最多收获 = Max((偷0号收获 + [2..n)内的最多收获), (偷1号收获 + [3..n)内的最多收获), ..., 偷n-1号收获)
     *   这样的分解可以很自然的使用递归实现，而又因为分解过程中存在重叠子问题，可以使用 memoization 进行优化。
     * - 时间复杂度 O(n)，空间复杂度 O(n)。
     * */
     public static int rob1(int[] nums) {
         if (nums == null || nums.length == 0) return 0;
         int[] cache = new int[nums.length + 1];
-        Arrays.fill(cache, -1);
+        Arrays.fill(cache, -1);           // ∵ 收获可能为0 ∴ 初始化为-1
         return tryToRob(nums, 0, cache);
     }
 
-    private static int tryToRob(int[] nums, int start, int[] cache) {     // 计算 [start..n-1] 内的最大收获
-        if (start >= nums.length) return 0;
-        if (cache[start] != -1) return cache[start];
+    private static int tryToRob(int[] nums, int i, int[] cache) {     // 计算 [start..n) 内的最大收获
+        if (i >= nums.length) return 0;
+        if (cache[i] != -1) return cache[i];
 
         int res = 0;
-        for (int i = start; i < nums.length; i++)
-            res = Math.max(res, nums[i] + tryToRob(nums, i + 2, cache));  // 例：res = 1号收获 + [2..n-1]内的最大收获
+        for (int j = i; j < nums.length; j++)
+            res = Math.max(res, nums[j] + tryToRob(nums, j + 2, cache));  // 例：res = nums[1] + [2..n)内的最大收获
                                                                           // 这里不用管 i+2 越界问题 ∵ 上面 start >= nums.length 已经覆盖过了
-        return cache[start] = res;
+        return cache[i] = res;
     }
 
     /*
-     * 解法2：DP - 思路：类似 L91_DecodeWays 解法2。recursion 是从前往后递归，DP
-     * 是从后往前递推，前一个问题的解是建立在后面问题的解的基础上。 - 时间复杂度 O(n)，空间复杂度 O(n)。
-     */
+    * 解法2：DP
+    * - 思路：类似 L91_DecodeWays 解法2。
+    * - 时间复杂度 O(n)，空间复杂度 O(n)。
+    * */
     public static int rob2(int[] nums) {
         if (nums == null || nums.length == 0) return 0;
 
         int n = nums.length;
-        int[] cache = new int[n];
-        Arrays.fill(cache, -1);
-        cache[n - 1] = nums[n - 1];  // 先解答最后一个问题，即偷 n-1 号的收获
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
+        dp[n - 1] = nums[n - 1];  // 先解答最后一个问题，即偷 n-1 号的收获
 
-        for (int start = n - 2; start >= 0; start--)  // 计算 [start..n-1] 内的最大收获
-            for (int i = start; i < n; i++)           // 范围固定的情况下，看哪种方案收获最大，例：求[2..5]内的最大收获，是偷2、4收获多还是偷3、5收获多
-                cache[start] = Math.max(cache[start], nums[i] + (i + 2 < n ? cache[i + 2] : 0));
+        for (int i = n - 2; i >= 0; i--)  // 计算 [i..n) 内的最大收获
+            for (int j = i; j < n; j++)   // 范围固定的情况下，看哪种方案收获最大，例：求[2..5]内的最大收获，是偷2、4收获多还是偷3、5收获多
+                dp[i] = Math.max(dp[i], nums[j] + (j + 2 < n ? dp[j + 2] : 0));
 
-        return cache[0];
+        return dp[0];
     }
 
     /*
@@ -107,7 +114,7 @@ public class L198_HouseRobber {
     }
 
     /*
-    * 解法4：更简洁的 DP
+    * 解法4：DP（另一种子问题定义方式）
     * - 思路：解法3的思路是将 f(i) 定义为“从前 i 所房子中所能得到的最大收获”，这里同时包含了偷以及不偷 i 这2种情况。另一种思路
     *   是将偷/不偷这2种情况分开进行递推：
     *   - y(i) 表示“若偷第 i 所房子，则从前 i 所房子中能获得的最大收获”； y(i) = n(i-1) + nums[i]；
