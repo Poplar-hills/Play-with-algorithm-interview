aa9ad3c4aabfb5da1b38f4ea77dd6d0e049a3745
Max Jiang <maxjiang23326@gmail.com>
2019-09-12 15:08:52 +0800

    feat: add a solution for L416

--- src/main/java/DP/KnapsackProblem/L416_PartitionEqualSubsetSum.java
+++ src/main/java/DP/KnapsackProblem/L416_PartitionEqualSubsetSum.java
@@ -16,37 +16,48 @@ import java.util.Arrays;
 *     2. 背包需要完全填满，不允许有剩余空间。
 *   由此可定义：
 *     1. 子问题：f(i, s) 表示“前 i 个整数的组合是否能填满 s”；
-*     2. 状态转移方程：f(i, s) = f(i-1, s) || f(i-1, s-v[i])。解释：(前 i-1 个整数的组合就能填满 s) || (前 i-1 个整数的
-*        组合能填满 s-v[i]，再加上整数 i 的值刚好填满)。
+*     2. 状态转移方程：f(i, s) = f(i-1, s) || f(i-1, s-nums[i])。
+*        解释：(前 i-1 个整数能填满 s) || (前 i-1 个整数能填满 s-v[i]，再加上整数 i 的值刚好填满)。
 * */
 
 public class L416_PartitionEqualSubsetSum {
     /*
-    * 解法1：Recursion + Memoization
+    * 超时解：
     * - 思路：top-down。
     * - 时间复杂度 O(n*sum)，空间复杂度 O(n*sum)。
     * */
     public static boolean canPartition(int[] nums) {
         int sum = Arrays.stream(nums).reduce(0, Integer::sum);  // 相当于背包容量
-        if (sum % 2 == 1) return false;   // 若总和为奇数则一定无解
-
-        int n = nums.length;
-        int halfSum = sum / 2;
-        int[][] cache = new int[n][halfSum + 1];
-        for (int[] row : cache)
-            Arrays.fill(row, -1);  // cache 中 -1表示未计算；0表示 false；1表示 true
-
-        return canPartition(n - 1, halfSum, nums, cache);
+        if (sum % 2 != 0) return false;                         // 若总和为奇数则一定无解
+        return helper(nums, nums.length - 1, sum / 2);
     }
 
-    private static boolean canPartition(int i, int s, int[] nums, int[][] cache) {
-        if (s == 0) return true;   // 只有完全填满时才是有效解（见分析中的第2条）
-        if (s < 0 || i < 0) return false;
-        if (cache[i][s] != -1) return cache[i][s] == 1;
+    private static boolean helper(int[] nums, int i, int s) {
+        if (i < 0 || s < 0) return false;
+        if (s == 0) return true;           // 只有完全填满时才是有效解（见分析中的第2条）
+        return helper(nums, i - 1, s) || helper(nums, i - 1, s - nums[i]);
+    }
 
-        boolean res = canPartition(i - 1, s, nums, cache) || canPartition(i - 1, s - nums[i], nums, cache);
-        cache[i][s] = res ? 1 : 0;
-        return res;
+    /*
+    * 解法1：Recursion + Memoization
+    * - 思路：top-down。
+    * - 时间复杂度 O(n*sum)，空间复杂度 O(n*sum)。
+    * */
+    public static boolean canPartition1(int[] nums) {
+        int sum = Arrays.stream(nums).reduce(0, Integer::sum);
+        if (sum % 2 == 1) return false;
+
+        int n = nums.length;
+        Boolean[][] cache = new Boolean[n][sum / 2 + 1];  // Boolean 数组默认值都为 null
+
+        return helper1(nums, n - 1, sum / 2, cache);
+    }
+
+    private static boolean helper1(int[] nums, int i, int s, Boolean[][] cache) {
+        if (s == 0) return true;
+        if (s < 0 || i < 0) return false;
+        if (cache[i][s] != null) return cache[i][s];
+        return cache[i][s] = helper1(nums, i - 1, s, cache) || helper1(nums, i - 1, s - nums[i], cache);
     }
 
     /*
@@ -105,11 +116,11 @@ public class L416_PartitionEqualSubsetSum {
     }
 
     public static void main(String[] args) {
-        log(canPartition3(new int[]{1, 5, 11, 5}));    // expects true. ([1, 5, 5] and [11])
-        log(canPartition3(new int[]{1, 2, 3, 4}));     // expects true. ([1, 4] and [2, 3])
-        log(canPartition3(new int[]{1, 2, 3, 5}));     // expects false
-        log(canPartition3(new int[]{1, 5, 3}));        // expects false
-        log(canPartition3(new int[]{1, 100, 5, 3}));   // expects false
-        log(canPartition3(new int[]{1}));              // expects false
+        log(canPartition1(new int[]{1, 5, 11, 5}));    // expects true. ([1, 5, 5] and [11])
+        log(canPartition1(new int[]{1, 2, 3, 4}));     // expects true. ([1, 4] and [2, 3])
+        log(canPartition1(new int[]{1, 2, 3, 5}));     // expects false
+        log(canPartition1(new int[]{1, 5, 3}));        // expects false
+        log(canPartition1(new int[]{1, 100, 5, 3}));   // expects false
+        log(canPartition1(new int[]{1}));              // expects false
     }
 }
