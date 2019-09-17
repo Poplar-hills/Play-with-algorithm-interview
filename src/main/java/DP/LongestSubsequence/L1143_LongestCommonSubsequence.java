ee61f96caf9e4987fdf7ffc91a5ae55fd9c648dd
Max Jiang <maxjiang23326@gmail.com>
2019-09-17 17:29:50 +0800

    refactor: rename

--- src/main/java/DP/LongestSubsequence/L1143_LongestCommonSubsequence.java
+++ src/main/java/DP/LongestSubsequence/L1143_LongestCommonSubsequence.java
@@ -9,32 +9,32 @@ import java.util.Arrays;
 *
 * - Given two strings s1 and s2, return the length of their longest common subsequence.
 *
-* - LCS 的应用非常广泛，最经典的就是文本处理，除此之外还用于基因工程中判断两段基因的相似性，LCS 约长则两段基因越相似。
+* - LCS 的应用非常广泛，最经典的就是文本处理，除此之外还用于基因工程中两段基因的相似性判断，LCS 越长则说明两段基因越相似。
 * */
 
 public class L1143_LongestCommonSubsequence {
     /*
-    * 超时解：DFS
-    * - 分析：与 L376 类似，该问题也有两个维度需要进行动态规划。因此：
-    *   - 子问题定义：f(i, j) 表示“s1[i..] 和 s2[j..] 两段字符串的 LCS 的长度”。
-    *   - 状态转移方程：∵ 每个字符都有两种状态 —— 在/不在 LCS 中：
-    *     - 若 s1[i] == s2[j]，即该字符在 LCS 中时：f(i, j) = 1 + f(i+1, j+1)，其中的1就是该字符；
-    *     - 若 s1[i] != s2[j]，即该字符不在 LCS 中时：f(i, j) = max(f(i+1, j), f(i, j+1))；即让两个字符串窗口各自
-    *       向右收缩一个字符，即 s1[i+1..]、s2[j..] 与 s1[i..]、s2[j+1..]，看这两种情况里哪种情况的的 LCS 更长。
-    *                       ABCD|AEBD              - 此时 A == A，LCS 长度+1，同时向右收缩
-    *                           ↓
-    *                        BCD|EBD               - 此时 B != E，不在 LCS 中，各自收缩
-    *                       ↙       ↘
-    *                 CD|EBD        BCD|BD         - 右分支中 B == B，LCS 长度+1，同时收缩
-    *               ↙       ↘          ↓
-    *            D|EBD     CD|BD     CD|D          - 右分支中 C != D，不在 LCS 中，各自收缩
-    *             ...       ...     ↙    ↘
-    *                             D|D    CD|-      - 右右分支已到底；右左分支 D == D，LCS 长度+1，同时收缩
-    *                              ↓
-    *                             -|-              - 右左分支已到底
-    *
-    * - 时间复杂度 O(2^(l1+l2))，空间复杂度 O(l1+l2)。
-    * */
+     * 超时解：DFS
+     * - 分析：与 L376 类似，该问题也有两个维度需要进行动态规划。因此：
+     *   - 子问题定义：f(i, j) 表示“s1[i..] 和 s2[j..] 两段字符串的 LCS 的长度”。
+     *   - 状态转移方程：∵ 每个字符都有在/不在 LCS 中这2种状态：
+     *     1. 若 s1[i] == s2[j]，即该字符在 LCS 中时：f(i, j) = 1 + f(i+1, j+1)，其中的1就是该字符；
+     *     2. 若 s1[i] != s2[j]，即该字符不在 LCS 中时：f(i, j) = max(f(i+1, j), f(i, j+1))；即让两个字符串窗口各自向
+     *        右收缩一个字符，即 s1[i+1..]、s2[j..] 与 s1[i..]、s2[j+1..]，看这两种情况里哪种情况的的 LCS 更长。
+     *                       ABCD|AEBD              - 此时 A == A，LCS 长度+1，同时向右收缩
+     *                           ↓
+     *                        BCD|EBD               - 此时 B != E，不在 LCS 中，各自收缩
+     *                       ↙       ↘
+     *                 CD|EBD        BCD|BD         - 右分支中 B == B，LCS 长度+1，同时收缩
+     *               ↙       ↘          ↓
+     *            D|EBD     CD|BD     CD|D          - 右分支中 C != D，不在 LCS 中，各自收缩
+     *             ...       ...     ↙    ↘
+     *                             D|D    CD|-      - 右右分支已到底；右左分支 D == D，LCS 长度+1，同时收缩
+     *                              ↓
+     *                             -|-              - 右左分支已到底
+     *
+     * - 时间复杂度 O(2^(l1+l2))，空间复杂度 O(l1+l2)。
+     * */
     public static int longestCommonSubsequence(String s1, String s2) {
         if (s1 == null || s2 == null) return 0;
         return helper(s1, s2, 0, 0);
@@ -48,9 +48,9 @@ public class L1143_LongestCommonSubsequence {
     }
 
     /*
-    * 解法1：Recursion + Memoization
-    * - 时间复杂度 O(l1*l2))，空间复杂度 O(l1*l2)，其中 l1、l2 分别为两字符串的长度。
-    * */
+     * 解法1：Recursion + Memoization
+     * - 时间复杂度 O(l1*l2))，空间复杂度 O(l1*l2)，其中 l1、l2 分别为两字符串的长度。
+     * */
     public static int longestCommonSubsequence1(String s1, String s2) {
         if (s1 == null || s2 == null) return 0;
 
@@ -58,24 +58,22 @@ public class L1143_LongestCommonSubsequence {
         for (int[] row : cache)
             Arrays.fill(row, -1);
 
-        return helper(s1, s2, 0, 0, cache);
+        return helper1(s1, s2, 0, 0, cache);
     }
 
-    private static int helper(String s1, String s2, int i, int j, int[][] cache) {
+    private static int helper1(String s1, String s2, int i, int j, int[][] cache) {
         if (i == s1.length() || j == s2.length()) return 0;
         if (cache[i][j] != -1) return cache[i][j];
 
-        int len = s1.charAt(i) == s2.charAt(j)
-            ? 1 + helper(s1, s2, i + 1, j + 1, cache)
-            : Math.max(helper(s1, s2, i + 1, j, cache), helper(s1, s2, i, j + 1, cache));
-
-        return cache[i][j] = len;
+        return cache[i][j] = s1.charAt(i) == s2.charAt(j)
+            ? 1 + helper1(s1, s2, i + 1, j + 1, cache)
+            : Math.max(helper1(s1, s2, i + 1, j, cache), helper1(s1, s2, i, j + 1, cache));
     }
 
     /*
-    * 解法2：DP
-    * - 时间复杂度 O(l1*l2))，空间复杂度 O(l1*l2)。
-    * */
+     * 解法2：DP
+     * - 时间复杂度 O(l1*l2))，空间复杂度 O(l1*l2)。
+     * */
     public static int longestCommonSubsequence2(String s1, String s2) {
         if (s1 == null || s2 == null) return 0;
 
@@ -149,10 +147,10 @@ public class L1143_LongestCommonSubsequence {
     }
 
     public static void main(String[] args) {
-        log(longestCommonSubsequence4("abcd", "aebd"));  // expects 3. "ace"
-        log(longestCommonSubsequence4("abcde", "ace"));  // expects 3. "ace"
-        log(longestCommonSubsequence4("abc", "abc"));    // expects 3. "abc"
-        log(longestCommonSubsequence4("bl", "yby"));     // expects 1. "b"
-        log(longestCommonSubsequence4("abc", "def"));    // expects 0.
+        log(longestCommonSubsequence1("abcd", "aebd"));  // expects 3. "ace"
+        log(longestCommonSubsequence1("abcde", "ace"));  // expects 3. "ace"
+        log(longestCommonSubsequence1("abc", "abc"));    // expects 3. "abc"
+        log(longestCommonSubsequence1("bl", "yby"));     // expects 1. "b"
+        log(longestCommonSubsequence1("abc", "def"));    // expects 0.
     }
 }
