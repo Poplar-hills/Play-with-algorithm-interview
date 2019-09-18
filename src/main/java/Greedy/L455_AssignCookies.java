ad98ba05bac9c5538ed88e00a94c59b24cca998c
Max Jiang <maxjiang23326@gmail.com>
2019-09-18 19:41:52 +0800

    feat: add a solution for L455

--- src/main/java/Greedy/L455_AssignCookies.java
+++ src/main/java/Greedy/L455_AssignCookies.java
@@ -4,11 +4,13 @@ import static Utils.Helpers.log;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Map;
+import java.util.TreeMap;
 
 /*
 * Assign Cookies
 *
-* - 给小朋友分饼干，每个小朋友最多分到一块。每个小朋友想要的饼干大小用”贪心指数“ g(i) 表示，每块饼干的大小用 s(i) 表示。
+* - 给小朋友分饼干，每个小朋友最多分到一块。每个小朋友想要的饼干大小用“贪心指数” g(i) 表示，每块饼干的大小用 s(i) 表示。
 *   若小朋友 i 分到了饼干 j，且 s(j) >= g(i) 则小朋友会很高兴。给定数组 s 和 g，问如何分饼干能让最多的小朋友高兴。
 *
 * - 贪心算法的题一般比较简单，因为代码会比较短。但难点在于判断一个问题是否可以使用贪心算法。
@@ -71,16 +73,42 @@ public class L455_AssignCookies {
         return count;
     }
 
+    /*
+     * 解法3：TreeMap
+     * - 思路：要让最多的小朋友开心，只需为每个小朋友从 s 中找到 ≥ 且最接近其贪心指数的饼干即可。要实现这个逻辑可以使用 TreeMap
+     *   中的 ceilingKey() 方法。
+     * - 时间复杂度 O(nlogn)，空间复杂度 O(1)。
+     * */
+    public static int findContentChildren3(int[] g, int[] s) {
+        TreeMap<Integer, Integer> tree = new TreeMap<>();  // ∵ 要用到 ceilingKey() 方法 ∴ 接口和实现都得是 TreeMap
+        int count = 0;
+
+        for (int size : s)                           // 将所有饼干添加进 tree 并累计个数，O(m)
+            tree.put(size, tree.getOrDefault(size, 0) + 1);
+
+        for (int greed : g) {                        // O(nlogn)
+            Integer gSize = tree.ceilingKey(greed);  // 从 tree 上找 ≥ greed 的所有 size 中最小的那个，O(logn)
+            if (gSize != null) {
+                count++;
+                tree.put(gSize, tree.get(gSize) - 1);
+                if (tree.get(gSize) == 0)
+                    tree.remove(gSize);
+            }
+        }
+
+        return count;
+    }
+
     public static void main(String[] args) {
-        log(findContentChildren(
+        log(findContentChildren3(
             new int[]{1, 2, 3},
             new int[]{1, 1}));        // expects 1. 最多让一个小朋友开心
 
-        log(findContentChildren(
+        log(findContentChildren3(
             new int[]{1, 2},
             new int[]{1, 2, 3}));     // expects 2. 两个小朋友都能开心
 
-        log(findContentChildren(
+        log(findContentChildren3(
             new int[]{10, 9, 8, 7},
             new int[]{5, 6, 7, 8}));  // expects 2. 小朋友7、8能开心
     }
