a80f6727f89e7da72c3dbc6c3c36f27a8b8ca6b4
Max Jiang <maxjiang23326@gmail.com>
2019-10-09 19:02:17 +0800

    feat: add a solution to L104

---
+++ src/main/java/BinaryTreeAndRecursion/L104_MaximumDepthOfBinaryTree.java
@@ -0,0 +1,29 @@
+package BinaryTreeAndRecursion;
+
+import static Utils.Helpers.*;
+
+/*
+ * Maximum Depth of Binary Tree
+ *
+ * Given a binary tree, find its maximum depth.
+ * */
+
+public class L104_MaximumDepthOfBinaryTree {
+    public static int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+
+    public static void main(String[] args) {
+        TreeNode t = createBinaryTreeBreadthFirst(new Integer[]{3, 9, 20, null, null, 15, 7});
+        log(maxDepth(t));
+        /*
+         *  expects 3
+         *      3
+         *     / \
+         *    9  20
+         *      /  \
+         *     15   7
+         * */
+    }
+}
