1ecc703080d601453273ae727691544ea421fd58
Max Jiang <maxjiang23326@gmail.com>
2019-10-11 16:52:35 +0800

    feat: add a solution to L226

--- src/main/java/BinaryTreeAndRecursion/Basics/L226_InvertBinaryTree.java
+++ src/main/java/BinaryTreeAndRecursion/Basics/L226_InvertBinaryTree.java
@@ -2,6 +2,9 @@ package BinaryTreeAndRecursion.Basics;
 
 import static Utils.Helpers.*;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /*
  * Invert Binary Tree
  * */
@@ -20,9 +23,30 @@ public class L226_InvertBinaryTree {
         return root;
     }
 
+    /*
+     * 解法2：Iterative (BFS)
+     * - 时间复杂度 O(n)，空间复杂度 O(h)，其中 n 为节点数，h 为树高。
+     * */
+    public static TreeNode invertTree2(TreeNode root) {
+        if (root == null) return null;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+
+        while (!q.isEmpty()) {
+            TreeNode node = q.poll();
+            TreeNode temp = node.left;
+            node.left = node.right;
+            node.right = temp;
+            if (node.right != null) q.offer(node.right);
+            if (node.left != null) q.offer(node.left);
+        }
+
+        return root;
+    }
+
     public static void main(String[] args) {
         TreeNode t1 = createBinaryTreeBreadthFirst(new Integer[]{4, 2, 7, 1, 3, 6, 9});
-        printBinaryTreeBreadthFirst(invertTree(t1));
+        printBinaryTreeBreadthFirst(invertTree2(t1));
         /*
          * expects [4, 7, 2, 9, 6, 3, 1].
          *         4                  4
