1e22259f308d7de89d6c0320a76cd1a9769c26bc
Max Jiang <maxjiang23326@gmail.com>
2019-10-16 14:09:04 +0800

    test: add test cases

--- src/main/java/BinaryTreeAndRecursion/Basics/L110_BalancedBinaryTree.java
+++ src/main/java/BinaryTreeAndRecursion/Basics/L110_BalancedBinaryTree.java
@@ -18,6 +18,7 @@ public class L110_BalancedBinaryTree {
      * - 时间复杂度 O(n)，空间复杂度 O(h)，其中 h 为树高（平衡树时 h=logn；退化为链表时 h=n）。
      * */
     public static boolean isBalanced(TreeNode root) {
+
         return false;
     }
 
@@ -33,8 +34,32 @@ public class L110_BalancedBinaryTree {
          *        15  7
          * */
 
-        TreeNode t2 = createBinaryTreeBreadthFirst(new Integer[]{1, 2, 2, 3, 3, null, null, 4, 4});
+        TreeNode t2 = createBinaryTreeBreadthFirst(new Integer[]{3, 9, 20, 4, null, 15, 7});
         log(isBalanced(t2));
+        /*
+         * expects true.
+         *        3
+         *       / \
+         *      9  20
+         *     /   / \
+         *    4   15  7
+         * */
+
+        TreeNode t3 = createBinaryTreeBreadthFirst(new Integer[]{1, 2, 2, 3, 3, null, 7, 4, 4});
+        log(isBalanced(t3));
+        /*
+         * expects false.
+         *           1
+         *          / \
+         *         2   2
+         *        / \   \
+         *       3   3   7
+         *      / \
+         *     4   4
+         * */
+
+        TreeNode t4 = createBinaryTreeBreadthFirst(new Integer[]{1, 2, 2, 3, 3, null, null, 4, 4});
+        log(isBalanced(t4));
         /*
          * expects false.
          *           1
