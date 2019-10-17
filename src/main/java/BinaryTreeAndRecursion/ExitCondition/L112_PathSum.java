6094222d37484a1788333db705fccfb9e9aa613b
Max Jiang <maxjiang23326@gmail.com>
2019-10-17 13:09:24 +0800

    refactor: simplify

--- src/main/java/BinaryTreeAndRecursion/ExitCondition/L112_PathSum.java
+++ src/main/java/BinaryTreeAndRecursion/ExitCondition/L112_PathSum.java
@@ -15,18 +15,14 @@ import Utils.Helpers.TreeNode;
 public class L112_PathSum {
     /*
      * 解法1：Recursion
+     * - 思路：递归的终止条件很容易写成 if (root == null) return sum == 0; 但这样 test case 3 过不去。因此递归终止条件
+     *   需要用叶子节点来判断
      * - 时间复杂度 O(n)，空间复杂度 O(h)，其中 h 为树高（平衡树时 h=logn；退化为链表时 h=n）。
      * */
     public static boolean hasPathSum(TreeNode root, int sum) {
         if (root == null) return false;
-
-        boolean isLeafNode = root.left == null && root.right == null;
-        if (isLeafNode && sum == root.val) return true;
-        if ((isLeafNode && sum != root.val) && (!isLeafNode && sum == 0)) return false;
-
-        boolean foundInLeft = root.left != null && hasPathSum(root.left, sum - root.val);
-        boolean foundInRight = root.right != null && hasPathSum(root.right, sum - root.val);
-        return foundInLeft || foundInRight;
+        if (root.left == null && root.right == null) return sum == root.val;
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
     }
 
     public static void main(String[] args) {
@@ -43,10 +39,23 @@ public class L112_PathSum {
          *   7    2      1
          * */
 
-        TreeNode t2 = createBinaryTreeBreadthFirst(new Integer[]{5, 4, 8, 11, null, 13, 4});
-        log(hasPathSum(t2, 9));
+        TreeNode t2 = createBinaryTreeBreadthFirst(new Integer[]{1, -2, -3, 1, 3, -2, null, -1});
+        log(hasPathSum(t2, -1));
         /*
-         * expects false. (5 -> 4 虽然和为9，但不是一条 root-to-leaf path)
+         * expects true. (1 -> -2 -> 1 -> -1)
+         *          1
+         *        /   \
+         *      -2    -3
+         *      / \   /
+         *     1  3  -2
+         *    /
+         *   -1
+         * */
+
+        TreeNode t3 = createBinaryTreeBreadthFirst(new Integer[]{5, 4, 8, 11, null, 13, 4});
+        log(hasPathSum(t3, 9));
+        /*
+         * expects false. (注意：5 -> 4 虽然和为9，但不是一条 root-to-leaf path)
          *         5
          *        / \
          *       4   8
@@ -54,8 +63,8 @@ public class L112_PathSum {
          *     11  13  4
          * */
 
-        TreeNode t3 = createBinaryTreeBreadthFirst(new Integer[]{});
-        log(hasPathSum(t3, 1));
+        TreeNode t4 = createBinaryTreeBreadthFirst(new Integer[]{});
+        log(hasPathSum(t4, 1));
         /*
          * expects false.
          * */
