b9b8fb50e0cf3328678bc2eaebdbc24145ea8754
Max Jiang <maxjiang23326@gmail.com>
2019-10-21 20:36:06 +0800

    feat: add question L113

--- src/main/java/BinaryTreeAndRecursion/DefineRecursiveProblem/L113_PathSumII.java
+++ src/main/java/BinaryTreeAndRecursion/DefineRecursiveProblem/L113_PathSumII.java
@@ -24,17 +24,17 @@ public class L113_PathSumII {
     }
 
     public static void main(String[] args) {
-        TreeNode t1 = createBinaryTreeBreadthFirst(new Integer[]{5, 4, 8, 11, null, 13, 4, 7, 2, null, null, 5, 1});
-        log(pathSum(t1, 22));
+        TreeNode t1 = createBinaryTreeBreadthFirst(new Integer[]{1, 2, 3, 4, null, 5, 6, 7, 8, null, null, 9, 10});
+        log(pathSum(t1, 9));
         /*
-         * expects [[5,4,11,2], [5,8,4,5]].
-         *        5
+         * expects [[1,3,-2,7], [1,3,5]].（注意 [1,2,6] 不是）
+         *        1
          *       / \
-         *      4   8
+         *      2   3
          *     /   / \
-         *    11  13  4
-         *   /  \    / \
-         *  7    2  5   1
+         *    6   5  -2
+         *   / \     / \
+         *  2   8   7   9
          * */
     }
 }
