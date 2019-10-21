5248a66c456abee15366e69dca9457554327f215
Max Jiang <maxjiang23326@gmail.com>
2019-10-21 19:34:08 +0800

    feat: add a solution for L257

--- src/main/java/BinaryTreeAndRecursion/DefineRecursiveProblem/L257_BinaryTreePaths.java
+++ src/main/java/BinaryTreeAndRecursion/DefineRecursiveProblem/L257_BinaryTreePaths.java
@@ -4,7 +4,10 @@ import static Utils.Helpers.createBinaryTreeBreadthFirst;
 import static Utils.Helpers.log;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 import Utils.Helpers.TreeNode;
@@ -37,16 +40,75 @@ public class L257_BinaryTreePaths {
             .collect(Collectors.toList());
     }
 
+    /*
+     * 解法2：Iteration (BFS)
+     * - 思路：类似 L70_ClimbingStairs 解法4。
+     * - 时间复杂度 O(n)，空间复杂度 O(n)。
+     * */
+    public static List<String> binaryTreePaths2(TreeNode root) {
+        List<String> res = new ArrayList<>();
+        if (root == null) return res;
+
+        Queue<List<TreeNode>> q = new LinkedList<>();
+        List<TreeNode> initialPath = new ArrayList<>();
+        initialPath.add(root);
+        q.offer(initialPath);
+
+        while (!q.isEmpty()) {
+            List<TreeNode> path = q.poll();
+            TreeNode lastNode = path.get(path.size() - 1);
+
+            if (lastNode.left == null && lastNode.right == null) {  // 若是叶子节点则说明该 path 是完整的
+                res.add(convertToString(path));
+                continue;
+            }
+
+            Consumer<TreeNode> fn = node -> {
+                List<TreeNode> newPath = new ArrayList<>(path);
+                newPath.add(node);
+                q.offer(newPath);
+            };
+
+            if (lastNode.left != null) fn.accept(lastNode.left);
+            if (lastNode.right != null) fn.accept(lastNode.right);
+        }
+
+        return res;
+    }
+
+    private static String convertToString(List<TreeNode> path) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < path.size(); i++) {
+            builder.append(Integer.toString(path.get(i).val));
+            if (i != path.size() - 1)
+                builder.append("->");
+        }
+        return builder.toString();
+    }
+
     public static void main(String[] args) {
-        TreeNode t1 = createBinaryTreeBreadthFirst(new Integer[]{1, 2, 3, null, 5});
-        log(binaryTreePaths(t1));
+        TreeNode t1 = createBinaryTreeBreadthFirst(new Integer[]{1, 2, 3, null, 4});
+        log(binaryTreePaths2(t1));
         /*
-         * expects ["1->2->5", "1->3"].
+         * expects ["1->2->4", "1->3"].
          *       1
          *     /   \
          *    2     3
          *     \
-         *      5
+         *      4
+         * */
+
+        TreeNode t2 = createBinaryTreeBreadthFirst(new Integer[]{1, 2, 3, null, 4, 5, 6, null, null, 7});
+        log(binaryTreePaths2(t2));
+        /*
+         * expects ["1->2->4", "1->3->5->7", "1->3->6"].
+         *        1
+         *     /     \
+         *    2       3
+         *     \     / \
+         *      4   5   6
+         *         /
+         *        7
          * */
     }
 }
