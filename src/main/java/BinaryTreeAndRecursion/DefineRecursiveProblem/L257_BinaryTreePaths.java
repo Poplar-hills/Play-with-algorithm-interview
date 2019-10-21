6def6f944ab29270ae277e7b0747f963dcfd8b4b
Max Jiang <maxjiang23326@gmail.com>
2019-10-21 15:45:46 +0800

    feat: add a solution for L257

--- src/main/java/BinaryTreeAndRecursion/DefineRecursiveProblem/L257_BinaryTreePaths.java
+++ src/main/java/BinaryTreeAndRecursion/DefineRecursiveProblem/L257_BinaryTreePaths.java
@@ -1,9 +1,11 @@
-package BinaryTreeAndRecursion.ExitCondition;
+package BinaryTreeAndRecursion.DefineRecursiveProblem;
 
 import static Utils.Helpers.createBinaryTreeBreadthFirst;
 import static Utils.Helpers.log;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import Utils.Helpers.TreeNode;
 
@@ -16,11 +18,23 @@ import Utils.Helpers.TreeNode;
 public class L257_BinaryTreePaths {
     /*
      * 解法1：Recursion
-     * -
+     * - 思路：递归函数 f(n) 定义：返回以 n 为根的二叉树的全部 root-to-leaf paths。
      * - 时间复杂度 O(n)，空间复杂度 O(h)，其中 h 为树高（平衡树时 h=logn；退化为链表时 h=n）。
      * */
     public static List<String> binaryTreePaths(TreeNode root) {
+        if (root == null) return new ArrayList<>();
 
+        List<String> paths = binaryTreePaths(root.left);
+        paths.addAll(binaryTreePaths(root.right));
+
+        if (paths.size() == 0) {
+            paths.add(Integer.toString(root.val));
+            return paths;
+        }
+
+        return paths.stream()
+            .map(p -> root.val + "->" + p)
+            .collect(Collectors.toList());
     }
 
     public static void main(String[] args) {
