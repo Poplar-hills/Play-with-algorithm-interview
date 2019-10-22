2e4324d207edec44bfa258af6d9d4c379d738983
Max Jiang <maxjiang23326@gmail.com>
2019-10-22 12:08:13 +0800

    feat: add a solution for L113

--- src/main/java/BinaryTreeAndRecursion/DefineRecursiveProblem/L113_PathSumII.java
+++ src/main/java/BinaryTreeAndRecursion/DefineRecursiveProblem/L113_PathSumII.java
@@ -3,9 +3,14 @@ package BinaryTreeAndRecursion.DefineRecursiveProblem;
 import static Utils.Helpers.createBinaryTreeBreadthFirst;
 import static Utils.Helpers.log;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 import Utils.Helpers.TreeNode;
+import javafx.util.Pair;
 
 /*
  * Path Sum II
@@ -15,7 +20,7 @@ import Utils.Helpers.TreeNode;
 
 public class L113_PathSumII {
     /*
-     * 解法1：Recursion
+     * 解法1：Recursion (DFS)
      * - 思路：递归函数 f(n) 定义：
      * - 时间复杂度 O(n)，空间复杂度 O(h)，其中 h 为树高（平衡树时 h=logn；退化为链表时 h=n）。
      * */
@@ -23,8 +28,46 @@ public class L113_PathSumII {
 
     }
 
+    /*
+     * 解法2：Iteration (DFS)
+     * - 时间复杂度 O(n)，空间复杂度 O(n)。
+     * */
+    public static List<List<Integer>> pathSum2(TreeNode root, int sum) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+
+        Stack<Pair<List<TreeNode>, Integer>> stack = new Stack<>();  // stack 中的数据格式：<path 节点列表, path 节点之和>
+        List<TreeNode> initialList = new ArrayList<>();
+        initialList.add(root);
+        stack.push(new Pair<>(initialList, root.val));
+
+        while (!stack.isEmpty()) {
+            Pair<List<TreeNode>, Integer> pair = stack.pop();
+            List<TreeNode> list = pair.getKey();
+            int currSum = pair.getValue();
+            TreeNode lastNode = list.get(list.size() - 1);
+
+            if (currSum == sum && lastNode.left == null && lastNode.right == null) {
+                List<Integer> valList = list.stream().map(node -> node.val).collect(Collectors.toList());
+                res.add(valList);
+                continue;
+            }
+
+            Consumer<TreeNode> fn = node -> {
+                List<TreeNode> newList = new ArrayList<>(list);
+                newList.add(node);
+                stack.push(new Pair<>(newList, currSum + node.val));
+            };
+
+            if (lastNode.left != null) fn.accept(lastNode.left);
+            if (lastNode.right != null) fn.accept(lastNode.right);
+        }
+
+        return res;
+    }
+
     public static void main(String[] args) {
-        TreeNode t1 = createBinaryTreeBreadthFirst(new Integer[]{1, 2, 3, 4, null, 5, 6, 7, 8, null, null, 9, 10});
+        TreeNode t1 = createBinaryTreeBreadthFirst(new Integer[]{1, 2, 3, 6, null, 5, -2, 2, 8, null, null, 7, 9});
         log(pathSum(t1, 9));
         /*
          * expects [[1,3,-2,7], [1,3,5]].（注意 [1,2,6] 不是）
