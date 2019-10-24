b850bc1cdd0775c061df16025d7b6bc557e08345
Max Jiang <maxjiang23326@gmail.com>
2019-10-24 17:02:55 +0800

    feat: add a solution for L560

--- src/main/java/Array/TwoPointerSlidingWindow/L560_SubarraySumEqualsK.java
+++ src/main/java/Array/TwoPointerSlidingWindow/L560_SubarraySumEqualsK.java
@@ -9,13 +9,13 @@ import static Utils.Helpers.log;
  *   whose sum equals to k.
  *
  * - 分析：若该题中的数组元素都是正数，则可以通过滑动窗口方式简单解决。但该题中数组元素既可以是正也可以是负（如 test case 5），
- *   当 window sum > k 时无法判断是应该右移左边界还是右移右边界，因此需要进行动态规划。
+ *   当 window sum > k 时无法判断是应该右移左边界还是右移右边界，因此无法单纯使用滑动窗口。
  * */
 
 public class L560_SubarraySumEqualsK {
     /*
-     * 解法1：DP (Saving cummulative sums)
-     * - 思路：DP 可以从暴力解法开始思考，即先看看一共有多少种 sum 组合：
+     * 解法1：双指针累加计数
+     * - 思路：通过双指针遍历 nums 中的所有 subarray，同时数出和为 k 的 subarray 个数即可。找到所有 subarry 的方法：
      *   [4, 2, -1, 5]
      *    -
      *    ----
@@ -27,11 +27,28 @@ public class L560_SubarraySumEqualsK {
      *          --
      *          -----
      *              -
-     *   像这样 n 个元素，从每个元素开始往后累加一遍，是 O(n^n) 的复杂度。∵ 累加的过程存在大量重复计算 ∴ 可以通过缓存
-     *   cummulative sums 的方式进行优化，即对数组的每个位置 i 缓存 nums[0..i] 之和。
-     * - 时间复杂度 O(n^2)，空间复杂度 O(n)。
+     * - 时间复杂度 O(n^2)，空间复杂度 O(1)。
      * */
     public static int subarraySum(int[] nums, int k) {
+        int count = 0, len = nums.length;
+
+        for (int i = 0; i < len; i++) {
+            int sum = 0;
+            for (int j = i; j < len; j++) {
+                sum += nums[j];
+                if (sum == k) count++;
+            }
+        }
+
+        return count;
+    }
+/*
+     * 解法2：Saving cummulative sums
+     * - 思路：解法1中，每次累加的过程存在大量重复计算 ∴ 可以通过缓存 cummulative sums 的方式进行优化，即在数组的每个位置 i 处
+     *   缓存 nums[0..i] 之和。
+     * - 时间复杂度 O(n^2)，空间复杂度 O(n)。
+     * */
+    public static int subarraySum2(int[] nums, int k) {
         int count = 0, len = nums.length;
         int[] sums = new int[len + 1];  // sums[i] 上存储 nums[0..i] 之和
         sums[0] = 0;                    // 前0个元素的和为0（之所以要多出 sums[0]=0 这个元素是因为在后面遍历过程中每次要
@@ -48,19 +65,19 @@ public class L560_SubarraySumEqualsK {
     }
 
     /*
-     * 解法2：DP (Saving cummulative sums)
+     * 解法3：
      *
      * - 时间复杂度 O(n^2)，空间复杂度 O(n)。
      * */
-    public static int subarraySum2(int[] nums, int k) {
+    public static int subarraySum3(int[] nums, int k) {
         return 0;
     }
 
     public static void main(String[] args) {
-        log(subarraySum2(new int[]{1, 1, 1}, 2));                 // expects 2. (1+1, 1+1)
-        log(subarraySum2(new int[]{1, 2, 3}, 3));                 // expects 2. (1+2, 3)
-        log(subarraySum2(new int[]{4, 2, 1, 5, 2, 6, 8, 7}, 8));  // expects 4. (2+1+5, 1+5+2, 2+6, 8)
-        log(subarraySum2(new int[]{-1, -1, 1}, 0));               // expects 1. (-1+1)
-        log(subarraySum2(new int[]{4, 2, -1, 5, -5}, 5));         // expects 3. (4+2-1, 4+2-1+5-5, 5)
+        log(subarraySum(new int[]{1, 1, 1}, 2));                 // expects 2. (1+1, 1+1)
+        log(subarraySum(new int[]{1, 2, 3}, 3));                 // expects 2. (1+2, 3)
+        log(subarraySum(new int[]{4, 2, 1, 5, 2, 6, 8, 7}, 8));  // expects 4. (2+1+5, 1+5+2, 2+6, 8)
+        log(subarraySum(new int[]{-1, -1, 1}, 0));               // expects 1. (-1+1)
+        log(subarraySum(new int[]{4, 2, -1, 5, -5}, 5));         // expects 3. (4+2-1, 4+2-1+5-5, 5)
     }
 }
--- src/main/java/BinaryTreeAndRecursion/ComplexRecursiveProblem/L437_PathSumIII.java
+++ src/main/java/BinaryTreeAndRecursion/ComplexRecursiveProblem/L437_PathSumIII.java
@@ -1,13 +1,11 @@
-Find the number of paths that sum to a given value.
-
-The path does not need to start or end at the root or a leaf, but it must go downwards (traveling only from parent nodes to child nodes).
-
-The tree has no more than 1,000 nodes and the values are in the range -1,000,000 to 1,000,000.
-
 package BinaryTreeAndRecursion.ComplexRecursiveProblem;
 
 import static Utils.Helpers.createBinaryTreeBreadthFirst;
 import static Utils.Helpers.log;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 
 import Utils.Helpers.TreeNode;
 
@@ -22,16 +20,22 @@ import Utils.Helpers.TreeNode;
 
 public class L437_PathSumIII {
     /*
-     * 解法1：Recursion (DFS)
-     * - 思路：
+     * 解法1：Iteration
+     * - 思路：找到所有
      * - 时间复杂度 O(n)，空间复杂度 O(h)，其中 h 为树高（平衡树时 h=logn；退化为链表时 h=n）。
      * */
     public static int pathSum(TreeNode root, int sum) {
+        if (root == null) return 0;
+        Stack<List<TreeNode>> stack = new Stack<>();
+        List<TreeNode> initialList = new ArrayList<>();
+        initialList.add(root);
+        stack.push(initialList);
+
         return 0;
     }
 
     public static void main(String[] args) {
-        TreeNode t1 = createBinaryTreeBreadthFirst(new Integer[]{10, 5, -3, 3, 2, null, 11, 3, -2, null, 1});
+        TreeNode t1 = createBinaryTreeBreadthFirst(new Integer[]{10, 5, -3, 3, 2, null, 11, 3, -10, null, 1});
         log(pathSum(t1, 8));
         /*
          * expects 4. (5->3, 5->2->1, -3->11, 10->5->3->-10)
@@ -42,6 +46,21 @@ public class L437_PathSumIII {
          *     3   2   11
          *    / \   \
          *   3 -10   1
+         * */
+
+        TreeNode t2 = createBinaryTreeBreadthFirst(new Integer[]{10, 8, -2});
+        log(pathSum(t2, 8));
+        /*
+         * expects 2. (8, 10->-2)
+         *         10
+         *        /  \
+         *       8   -2
+         * */
+
+        TreeNode t3 = createBinaryTreeBreadthFirst(new Integer[]{});
+        log(pathSum(t3, 0));
+        /*
+         * expects 0.
          * */
     }
 }
