1bca372be4f5122c781219c30404cc7696de1469
Max Jiang <maxjiang23326@gmail.com>
2019-10-28 20:27:46 +0800

    feat: refactor: simplify

--- src/main/java/BinaryTreeAndRecursion/LowestCommonAncestor/L235_LowestCommonAncestor.java
+++ src/main/java/BinaryTreeAndRecursion/LowestCommonAncestor/L235_LowestCommonAncestor.java
@@ -9,16 +9,18 @@ import Utils.Helpers.TreeNode;
  * Lowest Common Ancestor of a Binary Search Tree
  *
  * - Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
+ * - Note:
+ *   1. All of the nodes' values will be unique.
+ *   2. p and q are different and both values will exist in the BST.
  * */
 
 public class L235_LowestCommonAncestor {
     /*
      * 解法1：Recursion (DFS)
-     * - 思路：递归函数定义：判断节点 p 和 q 是否同时在以 root 为根的 BST 上，若是返回 root 节点，否则返回 null。
      * - 时间复杂度 O(n)，空间复杂度 O(h)，其中 h 为树高（平衡树时 h=logn；退化为链表时 h=n）。
      * */
     public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null || contains(root, p) || contains(root, q))
+        if (root == null)
             return null;
         if (p.val < root.val && q.val < root.val)
             return lowestCommonAncestor(root.left, p, q);
@@ -27,11 +29,13 @@ public class L235_LowestCommonAncestor {
         return root;
     }
 
-    private static boolean contains(TreeNode root, TreeNode node) {
-        if (root == null) return false;
-        if (node.val < root.val) return contains(root.left, node);
-        if (node.val > root.val) return contains(root.right, node);
-        return true;
+    /*
+     * 解法2：Iteration (DFS)
+     * - 思路：
+     * - 时间复杂度 O(n)，空间复杂度 O(h)，其中 h 为树高（平衡树时 h=logn；退化为链表时 h=n）。
+     * */
+    public static TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
+        return null;
     }
 
     public static void main(String[] args) {
@@ -50,6 +54,5 @@ public class L235_LowestCommonAncestor {
         log(lowestCommonAncestor(t1, new TreeNode(3), new TreeNode(7)));   // expects 6.
         log(lowestCommonAncestor(t1, new TreeNode(2), new TreeNode(4)));   // expects 2.
         log(lowestCommonAncestor(t1, new TreeNode(0), new TreeNode(5)));   // expects 2.
-        log(lowestCommonAncestor(t1, new TreeNode(-1), new TreeNode(5)));  // expects null.
     }
 }
