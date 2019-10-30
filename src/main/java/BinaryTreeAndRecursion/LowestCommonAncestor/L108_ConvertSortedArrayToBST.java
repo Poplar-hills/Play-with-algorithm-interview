2ff468d254e3ec8e59e429fa77f465628b8c8fc4
Max Jiang <maxjiang23326@gmail.com>
2019-10-30 16:24:48 +0800

    feat: add a solution for L108

--- src/main/java/BinaryTreeAndRecursion/LowestCommonAncestor/L108_ConvertSortedArrayToBST.java
+++ src/main/java/BinaryTreeAndRecursion/LowestCommonAncestor/L108_ConvertSortedArrayToBST.java
@@ -2,6 +2,8 @@ package BinaryTreeAndRecursion.LowestCommonAncestor;
 
 import static Utils.Helpers.printBinaryTreeBreadthFirst;
 
+import java.util.Stack;
+
 import Utils.Helpers.TreeNode;
 
 /*
@@ -18,7 +20,7 @@ public class L108_ConvertSortedArrayToBST {
      * 解法1：Recursion + 二分查找
      * - 思路：∵ BST 中的元素天生就是以二分的形式排列的 ∴ 从有序数组建立 BST 的过程其实就是对数组不断进行二分查找，然后顺序
      *   添加到 BST 上。
-     * - 时间复杂度 O()，空间复杂度 O()。
+     * - 时间复杂度 O(n)，空间复杂度 O(logn)。
      * */
     public static TreeNode sortedArrayToBST(int[] nums) {
         if (nums == null || nums.length == 0) return null;
@@ -34,16 +36,56 @@ public class L108_ConvertSortedArrayToBST {
         return node;
     }
 
+    /*
+     * 解法2：Iteration
+     * - 思路：解法1的迭代版，用 NodeCell 类来封装 <节点, 节点值下界, 节点值上界> 信息。
+     * - 时间复杂度 O(n)，空间复杂度 O(n)。
+     * */
+    private static class NodeCell {
+        TreeNode node;
+        int lower, upper;
+        public NodeCell(TreeNode node, int lower, int upper) {
+            this.node = node;
+            this.lower = lower;
+            this.upper = upper;
+        }
+    }
+
+    public static TreeNode sortedArrayToBST2(int[] nums) {
+        if (nums == null || nums.length == 0) return null;
+        TreeNode root = new TreeNode(0);           // 先生成一个占位节点
+        NodeCell rootCell = new NodeCell(root, 0, nums.length - 1);
+        Stack<NodeCell> stack = new Stack<>();
+        stack.push(rootCell);
+
+        while (!stack.isEmpty()) {
+            NodeCell cell = stack.pop();           // 得到栈顶 NodeCell，为其确定节点值
+            int mid = (cell.upper - cell.lower) / 2 + cell.lower;
+            cell.node.val = nums[mid];             // nums[mid] 即是该节点的真正节点值，替换原来的占位节点值
+
+            if (cell.lower != mid) {               // 若 nums[mid] 左侧还有元素
+                cell.node.left = new TreeNode(0);  // 创建左子占位节点（主要是先创建两节点之间的连接关系，节点值后面再覆盖）
+                stack.push(new NodeCell(cell.node.left, cell.lower, mid - 1));
+            }
+            if (cell.upper != mid) {               // 若 nums[mid] 右侧还有元素
+                cell.node.right = new TreeNode(0);
+                stack.push(new NodeCell(cell.node.right, mid + 1, cell.upper));
+            }
+        }
+
+        return root;
+    }
+
     public static void main(String[] args) {
       int[] arr1 = new int[]{-6, -4, -2, 0, 1, 3, 5};
-      printBinaryTreeBreadthFirst(sortedArrayToBST(arr1));
+      printBinaryTreeBreadthFirst(sortedArrayToBST2(arr1));
       /*
        * expects [0,-4,3,-6,-2,1,5] or [-2,-4,3,-6,null,1,5,null,null,0] or others.
-       *                   0                          -2
-       *                 /   \                       /  \
-       *               -4     3                    -4    3
-       *               / \   / \                   /    / \
-       *             -6  -2 1   5                -6    1   5
+       *                  0                           -2
+       *                /   \                        /  \
+       *              -4     3                     -4    3
+       *              / \   / \                    /    / \
+       *            -6  -2 1   5                 -6    1   5
        *                                              /
        *                                             0
        * */
