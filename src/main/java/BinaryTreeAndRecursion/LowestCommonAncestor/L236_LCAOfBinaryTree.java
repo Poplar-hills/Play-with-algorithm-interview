613aa969d96218479e50dfc74095bc2a85d87e80
Max Jiang <maxjiang23326@gmail.com>
2019-11-01 17:05:14 +0800

    feat: add a solution for L236

--- src/main/java/BinaryTreeAndRecursion/LowestCommonAncestor/L236_LCAOfBinaryTree.java
+++ src/main/java/BinaryTreeAndRecursion/LowestCommonAncestor/L236_LCAOfBinaryTree.java
@@ -17,12 +17,24 @@ import Utils.Helpers.TreeNode;
 
 public class L236_LCAOfBinaryTree {
     /*
-     * 解法1：
+     * 解法1：Recursion (DFS)
      * - 思路：
      * - 时间复杂度 O()，空间复杂度 O()。
      * */
     public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        return null;
+        if (root == null)
+            return null;
+        if (contains(root.left, p) && contains(root.left, q))
+            return lowestCommonAncestor(root.left, p, q);
+        if (contains(root.right, p) && contains(root.right, q))
+            return lowestCommonAncestor(root.right, p, q);
+        return root;
+    }
+
+    private static boolean contains(TreeNode root, TreeNode node) {  // O(2^logn)
+        if (root == null) return false;
+        if (root.val == node.val) return true;
+        return contains(root.left, node) || contains(root.right, node);
     }
 
     public static void main(String[] args) {
@@ -38,6 +50,8 @@ public class L236_LCAOfBinaryTree {
          * */
 
         log(lowestCommonAncestor(t1, new TreeNode(5), new TreeNode(1)));  // expects 3. (The LCA of nodes 5 and 1 is 3.)
+        log(lowestCommonAncestor(t1, new TreeNode(7), new TreeNode(0)));  // expects 3.
         log(lowestCommonAncestor(t1, new TreeNode(5), new TreeNode(4)));  // expects 5.
+        log(lowestCommonAncestor(t1, new TreeNode(4), new TreeNode(6)));  // expects 5.
     }
 }
