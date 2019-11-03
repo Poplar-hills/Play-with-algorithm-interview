9a142ea36aafae2ad41f6e7fe0a72148f455bf1b
Max Jiang <maxjiang23326@gmail.com>
2019-11-03 16:49:56 +0800

    feat: add a solution to L450

--- src/main/java/BinaryTreeAndRecursion/LowestCommonAncestor/L450_DeleteNodeInBST.java
+++ src/main/java/BinaryTreeAndRecursion/LowestCommonAncestor/L450_DeleteNodeInBST.java
@@ -1,7 +1,7 @@
 package BinaryTreeAndRecursion.LowestCommonAncestor;
 
 import static Utils.Helpers.createBinaryTreeBreadthFirst;
-import static Utils.Helpers.log;
+import static Utils.Helpers.printBinaryTreeBreadthFirst;
 
 import Utils.Helpers.TreeNode;
 
@@ -19,23 +19,68 @@ import Utils.Helpers.TreeNode;
 public class L450_DeleteNodeInBST {
     /*
      * 解法1：
-     * - 思路：
+     * - 思路：先分析删除操作涉及哪些节点：1.待删除节点 2.待删除的两个子节点  3.前驱/后继节点
      * - 时间复杂度 O()，空间复杂度 O()。
      * */
     public static TreeNode deleteNode(TreeNode root, int key) {
-        return null;
+        if (root == null) return null;
+        if (root.val != key) {
+            root.left = deleteNode(root.left, key);
+            root.right = deleteNode(root.right, key);
+            return root;
+        }
+        if (root.left == null && root.right == null) return null;
+        if (root.left == null) return root.right;
+        if (root.right == null) return root.left;
+
+        TreeNode successor = getMin(root.right);
+        successor.right = removeMin(root.right);
+        successor.left = root.left;
+        return successor;
+    }
+
+    private static TreeNode getMin(TreeNode node) {
+        return (node.left == null) ? node : getMin(node.left);
+    }
+
+    private static TreeNode removeMin(TreeNode node) {
+        if (node.left == null) return node.right;
+        node.left = removeMin(node.left);
+        return node;
     }
 
     public static void main(String[] args) {
-        TreeNode t1 = createBinaryTreeBreadthFirst(new Integer[]{5, 3, 6, 2, 4, null, 7});
-        log(deleteNode(t1, 7));
+        TreeNode t1 = createBinaryTreeBreadthFirst(new Integer[]{5, 3, 6, 2, null, null, 7});
+        printBinaryTreeBreadthFirst(deleteNode(t1, 3));
+        /*
+         * expects [5,2,6,null,null,null,7]
+         *       5                              5
+         *      / \       After deleting       / \
+         *     3   6     --------------->     2   6
+         *    /     \       the node 3             \
+         *   2       7                              7
+         * */
+
+        TreeNode t2 = createBinaryTreeBreadthFirst(new Integer[]{5, 3, 6, null, 4, null, 7});
+        printBinaryTreeBreadthFirst(deleteNode(t2, 3));
+        /*
+         * expects [5,4,6,null,null,null,7]
+         *       5                              5
+         *      / \       After deleting       / \
+         *     3   6     --------------->     4   6
+         *      \   \       the node 3             \
+         *       4   7                              7
+         * */
+
+        TreeNode t3 = createBinaryTreeBreadthFirst(new Integer[]{5, 3, 6, 2, 4, null, 7});
+        printBinaryTreeBreadthFirst(deleteNode(t3, 3));
         /*
          * expects [5,4,6,2,null,null,7] or [5,2,6,null,4,null,7]
-         *       5                                5                  5
-         *      / \       After deleting         / \                / \
-         *     3   6     --------------->       4   6      or      2   6
-         *    / \   \       the node 3         /     \              \   \
-         *   2   4   7                        2       7              4   7
+         *       5                              5                  5
+         *      / \       After deleting       / \                / \
+         *     3   6     --------------->     4   6      or      2   6
+         *    / \   \       the node 3       /     \              \   \
+         *   2   4   7                      2       7              4   7
          * */
     }
 }
