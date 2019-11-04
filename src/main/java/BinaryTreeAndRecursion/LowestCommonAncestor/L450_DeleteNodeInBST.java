2d469f4123137f96719d467286baeffd25ee7a6e
Max Jiang <maxjiang23326@gmail.com>
2019-11-04 15:46:34 +0800

    feat: add a solution for L450

--- src/main/java/BinaryTreeAndRecursion/LowestCommonAncestor/L450_DeleteNodeInBST.java
+++ src/main/java/BinaryTreeAndRecursion/LowestCommonAncestor/L450_DeleteNodeInBST.java
@@ -31,8 +31,7 @@ public class L450_DeleteNodeInBST {
         else {
             if (root.left == null) return root.right;
             if (root.right == null) return root.left;
-
-            TreeNode successor = getMin(root.right);   // 若左右子树都有，则使用 Hibbard Deletion 方法
+            TreeNode successor = getMin(root.right);  // 若左右子树都有，则使用 Hibbard Deletion 方法
             successor.right = removeMin(root.right);
             successor.left = root.left;
             root = successor;
@@ -50,9 +49,47 @@ public class L450_DeleteNodeInBST {
         return node;
     }
 
+    /*
+     * 解法2：Iteration + Hibbard Deletion 方法
+     * - 思路：解法1的非递归版，总体思路是：1. 先找到以待删除节点为根的子树；2. 删除其父节点。具体移动过程比较复杂，要画图来辅助思考。
+     * - 👉 总结：二叉树操作的非递归实现通常都需要拿到：1. 待操作节点；2. 待操作节点的父节点。
+     * - 时间复杂度 O(logn)，空间复杂度 O(h)，其中 h 为树高（平衡树时 h=logn；退化为链表时 h=n）。
+     * */
+    public static TreeNode deleteNode2(TreeNode root, int key) {
+        TreeNode prev = null, curr = root;
+
+        while (curr != null && curr.val != key) {  // 找到待删除节点，及其父节点
+            prev = curr;
+            if (key < curr.val) curr = curr.left;
+            else if (key > curr.val) curr = curr.right;
+        }
+
+        if (prev == null) return deleteRootNode(curr);            // 待删除节点就是二叉树的根节点的情况
+        if (prev.left == curr) prev.left = deleteRootNode(curr);  // 待删除节点是 prev.left 的情况
+        else prev.right = deleteRootNode(curr);                   // 待删除节点是 prev.right 的情况
+        return root;
+    }
+
+    private static TreeNode deleteRootNode(TreeNode root) {  // 从以 root 为根的二叉树中删除根节点，并返回以 succssor 为根的二叉树
+        if (root == null) return null;
+        if (root.left == null) return root.right;  // 没有子树或只有右子树
+        if (root.right == null) return root.left;  // 没有子树或只有左子树
+
+        TreeNode prev = null, succ = root.right;  // 若左右子树都有，则去右子树中找到最小节点 successor 节点，及其父节点
+        while (succ.left != null) {
+            prev = succ;
+            succ = succ.left;
+        }
+
+        if (root.right == succ) return succ;  // 若 successor 就是右子树的根节点的情况（再没有左子树）
+        prev.left = succ.right;               // 在移动 successor 之前需要先保留其右子树（移动到父节点上，接替 successor 的位置）
+        succ.right = root.right;              // 再让 successor 接替根节点
+        return succ;                          // 返回新的根节点
+    }
+
     public static void main(String[] args) {
         TreeNode t1 = createBinaryTreeBreadthFirst(new Integer[]{5, 3, 6, 2, null, null, 7});
-        printBinaryTreeBreadthFirst(deleteNode(t1, 3));
+        printBinaryTreeBreadthFirst(deleteNode2(t1, 3));
         /*
          * expects [5,2,6,null,null,null,7]
          *       5                              5
@@ -63,7 +100,7 @@ public class L450_DeleteNodeInBST {
          * */
 
         TreeNode t2 = createBinaryTreeBreadthFirst(new Integer[]{5, 3, 6, null, 4, null, 7});
-        printBinaryTreeBreadthFirst(deleteNode(t2, 3));
+        printBinaryTreeBreadthFirst(deleteNode2(t2, 3));
         /*
          * expects [5,4,6,null,null,null,7]
          *       5                              5
@@ -74,7 +111,7 @@ public class L450_DeleteNodeInBST {
          * */
 
         TreeNode t3 = createBinaryTreeBreadthFirst(new Integer[]{5, 3, 6, 2, 4, null, 7});
-        printBinaryTreeBreadthFirst(deleteNode(t3, 3));
+        printBinaryTreeBreadthFirst(deleteNode2(t3, 3));
         /*
          * expects [5,4,6,2,null,null,7] or [5,2,6,null,4,null,7]
          *       5                              5                  5
