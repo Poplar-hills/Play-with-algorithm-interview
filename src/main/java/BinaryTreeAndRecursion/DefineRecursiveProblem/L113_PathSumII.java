aa694a3e1359975c99be33af9ab1a42ac5e2915c
Max Jiang <maxjiang23326@gmail.com>
2019-11-07 18:50:22 +0800

    docs: update comments

--- src/main/java/BinaryTreeAndRecursion/DefineRecursiveProblem/L113_PathSumII.java
+++ src/main/java/BinaryTreeAndRecursion/DefineRecursiveProblem/L113_PathSumII.java
@@ -4,7 +4,9 @@ import static Utils.Helpers.createBinaryTreeBreadthFirst;
 import static Utils.Helpers.log;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.Stack;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
@@ -16,6 +18,11 @@ import javafx.util.Pair;
  * Path Sum II
  *
  * - Given a binary tree and a sum, find all root-to-leaf paths where each path's sum equals the given sum.
+ *
+ * - Path Sum 系列：
+ *   - Path Sum I 要求是否存在节点和为 sum 的 root-to-leaf 路径；
+ *   - Path Sum II 要求打印出所有节点和为 sum 的 root-to-leaf 路径；
+ *   - Path SUm III 要求节点和为 sum 的路径个数（不管是否是 root-to-leaf 都算）。
  * */
 
 public class L113_PathSumII {
@@ -29,17 +36,17 @@ public class L113_PathSumII {
         List<List<Integer>> res = new ArrayList<>();
         if (root == null) return res;
 
-        if (root.left == null && root.right == null && sum == root.val) {  // 叶子节点
+        if (root.left == null && root.right == null && sum == root.val) {  // 若该路径符合要求，则在 res 中创建包含该节点的新列表
             List<Integer> path = new ArrayList<>();
             path.add(root.val);
             res.add(path);
             return res;
         }
 
-        List<List<Integer>> paths = pathSum(root.left, sum - root.val);  // 将左右子树返回的结果 concat 起来
+        List<List<Integer>> paths = pathSum(root.left, sum - root.val);  // 将左右子树返回的 res 连接起来
         paths.addAll(pathSum(root.right, sum - root.val));
 
-        return paths.stream().map(path -> {  // 向 concat 结果中的每个 path 头部添加当前节点值
+        return paths.stream().map(path -> {  // 向连接后的 res 中的每个 path 头部添加当前节点值
             path.add(0, root.val);
             return path;
         }).collect(Collectors.toList());
@@ -47,7 +54,7 @@ public class L113_PathSumII {
 
     /*
      * 解法2：Recursion (DFS)
-     * - 思路：从根节点开始递归生成路径（path），若到达叶子节点且剩余 sum 为0，则说明是一条符合要求的路径，添加到结果集 res 中。
+     * - 思路：从根节点开始递归生成路径 path，若到达叶子节点且剩余 sum 为0，则说明该 path 是符合要求的路径，添加到结果集 res 中。
      * - 时间复杂度 O(n)，空间复杂度 O(h)，其中 h 为树高（平衡树时 h=logn；退化为链表时 h=n）。
      * */
     public static List<List<Integer>> pathSum2(TreeNode root, int sum) {
@@ -89,11 +96,11 @@ public class L113_PathSumII {
         if (root.left == null && root.right == null && root.val == sum)
             res.add(new ArrayList<>(path));  // 若是符合要求的 path，则复制进 res 里
         else {
-            helper3(root.left, sum - root.val, path, res);  // 复用 path
+            helper3(root.left, sum - root.val, path, res);  // 若不符合要求，则继续递归，并复用 path
             helper3(root.right, sum - root.val, path, res);
         }
-        path.remove(path.size() - 1);  // 在本层递归结束前需要将 path 恢复原状，把添加的节点值移除，这样回到调用栈上一层后才能继续正常对右子树进行递归
-	}
+        path.remove(path.size() - 1);  // 在本层递归结束前（返回上层之前）需要将 path 恢复原状，把添加的节点值移除掉，这样
+	}                                  // 回到调用栈上一层后才能继续正常对右子树进行递归
 
 	/*
      * 解法4：Iteration (DFS) (解法2的迭代版)
@@ -105,7 +112,7 @@ public class L113_PathSumII {
         List<List<Integer>> res = new ArrayList<>();
         if (root == null) return res;
 
-        Stack<Pair<List<TreeNode>, Integer>> stack = new Stack<>();  // stack 中的数据格式：<path 节点列表, path 节点之和>
+        Stack<Pair<List<TreeNode>, Integer>> stack = new Stack<>();  // stack 中保存 <path 节点列表, path 节点之和>
         List<TreeNode> initialList = new ArrayList<>();
         initialList.add(root);
         stack.push(new Pair<>(initialList, root.val));
@@ -172,9 +179,27 @@ public class L113_PathSumII {
         return res;
     }
 
+    public static List<List<Integer>> pathSum0(TreeNode root, int sum) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+        helper0(root, sum, new ArrayList<>(), res);
+        return res;
+    }
+
+    private static void helper0(TreeNode root, int sum, List<Integer> path, List<List<Integer>> res) {
+        if (root == null) return;
+        path.add(root.val);
+        if (sum == root.val && root.left == null && root.right == null) {
+            res.add(path);
+            return;
+        }
+        helper0(root.left, sum - root.val, new ArrayList<>(path), res);
+        helper0(root.right, sum - root.val, new ArrayList<>(path), res);
+    }
+
     public static void main(String[] args) {
         TreeNode t1 = createBinaryTreeBreadthFirst(new Integer[]{1, 2, 3, 6, null, 5, -2, 2, 8, null, null, 7, 9});
-        log(pathSum5(t1, 9));
+        log(pathSum0(t1, 9));
         /*
          * expects [[1,3,-2,7], [1,3,5]].（注意 [1,2,6] 不是）
          *        1
@@ -187,7 +212,7 @@ public class L113_PathSumII {
          * */
 
         TreeNode t2 = createBinaryTreeBreadthFirst(new Integer[]{});
-        log(pathSum5(t2, 1));
+        log(pathSum0(t2, 1));
         /*
          * expects [].
          * */
