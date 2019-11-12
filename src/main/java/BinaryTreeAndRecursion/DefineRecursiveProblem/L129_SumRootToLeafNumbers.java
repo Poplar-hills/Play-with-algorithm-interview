775270cd84ac2e3ec9af3e2f8a2b6a7826180e04
Max Jiang <maxjiang23326@gmail.com>
2019-11-12 18:21:39 +0800

    docs: update comments

--- src/main/java/BinaryTreeAndRecursion/DefineRecursiveProblem/L129_SumRootToLeafNumbers.java
+++ src/main/java/BinaryTreeAndRecursion/DefineRecursiveProblem/L129_SumRootToLeafNumbers.java
@@ -19,14 +19,16 @@ import javafx.util.Pair;
 public class L129_SumRootToLeafNumbers {
     /*
      * 解法1：Recursion (DFS)
-     * - 思路：递归函数 f(n) 定义：返回以 n 为根的二叉树的所有 root-to-leaf numbers 之和。
+     * - 思路：从根节点开始逐层累积 pathNum，直到叶子节点开始逐层返回，具体返回的是左、右子树的所有 pathNum 之和。根据该思路
+     *   需将每层累积的 pathNum 向下层递归函数传递。
+     * - 总结：该思路属于 pre-order traversal，
      * - 时间复杂度 O(n)，空间复杂度 O(h)，其中 h 为树高（平衡树时 h=logn；退化为链表时 h=n）。
      * */
     public static int sumNumbers(TreeNode root) {
         return helper(root, 0);
     }
 
-    private static int helper(TreeNode root, int pathNum) {
+    private static int helper(TreeNode root, int pathNum) {  // 返回以 root 为根的二叉树的所有 pathNum 之和
         if (root == null) return 0;
         pathNum = pathNum * 10 + root.val;
         if (root.left == null && root.right == null) return pathNum;
@@ -34,15 +36,14 @@ public class L129_SumRootToLeafNumbers {
     }
 
     /*
-     * 解法2：Iteration (DFS)
-     * - 思路：使用 Stack 实现深度优先遍历，向下遍历的同时累加 pathSum。
+     * 解法2：Iteration (DFS) (解法1的非递归版)
      * - 同理：只需将 Stack 替换为 Queue 就得到了 BFS 解法。
      * - 时间复杂度 O(n)，空间复杂度 O(n)。
      * */
     public static int sumNumbers2(TreeNode root) {
         if (root == null) return 0;
         int sum = 0;
-        Stack<Pair<TreeNode, Integer>> stack = new Stack<>();
+        Stack<Pair<TreeNode, Integer>> stack = new Stack<>();  // 存储 <节点, 当前节点的 pathSum>
         stack.push(new Pair<>(root, root.val));
 
         while (!stack.isEmpty()) {
@@ -63,7 +64,7 @@ public class L129_SumRootToLeafNumbers {
 
     public static void main(String[] args) {
         TreeNode t1 = createBinaryTreeBreadthFirst(new Integer[]{1, 2, 3});
-        log(sumNumbers2(t1));
+        log(sumNumbers(t1));
         /*
          * expects 25. (12 + 13)
          *        1
@@ -72,7 +73,7 @@ public class L129_SumRootToLeafNumbers {
          * */
 
         TreeNode t2 = createBinaryTreeBreadthFirst(new Integer[]{4, 9, 0, 5, 1});
-        log(sumNumbers2(t2));
+        log(sumNumbers(t2));
         /*
          * expects 1026. (495 + 491 + 40)
          *        4
