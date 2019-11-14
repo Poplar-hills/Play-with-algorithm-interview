670c433356eba0b1308c5c8ec165189e5b347045
Max Jiang <maxjiang23326@gmail.com>
2019-11-14 16:26:19 +0800

    feat: add quesitons L93 and L131

---
+++ src/main/java/RecursionAndBackTracking/L131_PalindromePartitioning.java
@@ -0,0 +1,26 @@
+package RecursionAndBackTracking;
+
+import static Utils.Helpers.*;
+
+import java.util.List;
+
+/*
+ * Palindrome Partitioning
+ *
+ * - Given a string containing only digits, restore it by returning all possible valid IP address combinations.
+ * */
+
+public class L131_PalindromePartitioning {
+    /*
+     * 解法1：
+     * - 思路：
+     * - 时间复杂度 O()，空间复杂度 O()。
+     * */
+    public static List<List<String>> partition(String s) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        log(partition("aab"));  // expects [["aa","b"], ["a","a","b"]]
+    }
+}
---
+++ src/main/java/RecursionAndBackTracking/L93_RestoreIPAddresses.java
@@ -0,0 +1,26 @@
+package RecursionAndBackTracking;
+
+import static Utils.Helpers.*;
+
+import java.util.List;
+
+/*
+ * Restore IP Addresses
+ *
+ * - Given a string containing only digits, restore it by returning all possible valid IP address combinations.
+ * */
+
+public class L93_RestoreIPAddresses {
+    /*
+     * 解法1：
+     * - 思路：
+     * - 时间复杂度 O()，空间复杂度 O()。
+     * */
+    public static List<String> restoreIpAddresses(String s) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        log(restoreIpAddresses("25525511135"));  // expects ["255.255.11.135", "255.255.111.35"]
+    }
+}
