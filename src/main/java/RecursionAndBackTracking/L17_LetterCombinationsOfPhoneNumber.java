358e89189da15ca470fdc169946f3173a3cb0a75
Max Jiang <maxjiang23326@gmail.com>
2019-11-14 15:46:48 +0800

    feat: add a solution for L17

--- src/main/java/RecursionAndBackTracking/L17_LetterCombinationsOfPhoneNumber.java
+++ src/main/java/RecursionAndBackTracking/L17_LetterCombinationsOfPhoneNumber.java
@@ -25,7 +25,7 @@ import java.util.List;
  *   - “回溯”指的是递归结束后返回上一层的行为。
  *   - “回溯法”指的就是通过 递归->返回->递归->返回->…… 这样的方式来搜索解的一种算法思想（DFS 就是回溯法的具体例子）。
  *   - 回溯法的时间效率一般比较低 ∵ 要遍历到所有叶子节点（通常是指数级别，即 O(2^n)）。
- *   - 回溯法是暴力解法的一个主要实现方式，尤其是在不能使用循环遍历（不知道要循环几遍）的情况下（例如树形结构中）。
+ *   - 回溯法是暴力解法的一个主要实现方式，尤其是在不能简单使用循环遍历（不知道要循环几遍）的情况下（例如树形结构中）。
  *   - 回溯法可以通过“剪枝”避免到达所有叶子节点来优化时间效率。
  *   - 动态规划其实就是在回溯法的基础上改进的。
  * */
@@ -67,8 +67,56 @@ public class L17_LetterCombinationsOfPhoneNumber {
         }
     }
 
+    /*
+     * 解法2：Iteration (解法1的非递归版)
+     * - 思路：纯用循环遍历实现：对于 digits="23" 来说：
+     *                       res = [""]
+     *            / "a"  ->  ""+"a" -> temp=["a"]
+     *        "2" - "b"  ->  ""+"b" -> temp=["a", "b"]
+     *            \ "c"  ->  ""+"c" -> temp=["a", "b", "c"]
+     *
+     *                       res = ["a", "b", "c"]
+     *                  /->  "a"+"d" -> temp=["ad"]
+     *            / "d" -->  "b"+"d" -> temp=["ad", "bd"]
+     *           /      \->  "c"+"d" -> temp=["ad", "bd", "cd"]
+     *          /       /->  "a"+"e" -> temp=["ad", "bd", "cd", "ae"]
+     *        "3" - "e" -->  "b"+"e" -> temp=["ad", "bd", "cd", "ae", "be"]
+     *          \       \->  "c"+"e" -> temp=["ad", "bd", "cd", "ae", "be", "ce"]
+     *           \      /->  "a"+"f" -> temp=["ad", "bd", "cd", "ae", "be", "ce", "af"]
+     *            \ "f" -->  "b"+"f" -> temp=["ad", "bd", "cd", "ae", "be", "ce", "af", "bf"]
+     *                  \->  "c"+"f" -> temp=["ad", "bd", "cd", "ae", "be", "ce", "af", "bf", "cf"]
+     * - 时间复杂度 O(3^n * 4^m)，空间复杂度 O(1)。
+     * */
+    public static List<String> letterCombinations2(String digits) {
+        List<String> res = new ArrayList<>();
+        if (digits.isEmpty()) return res;
+        res.add("");                  // 注意这里要先放入一个 trigger 才能启动后面的逻辑填入数据
+
+        for (char d : digits.toCharArray()) {
+            List<String> temp = new ArrayList<>();
+            String letterStr = letterMap[d - '0'];
+
+            for (char l : letterStr.toCharArray())
+                for (String s : res)  // 将 res 中已有的字符串再拿出来拼接上 l
+                    temp.add(s + l);
+
+            res = temp;
+        }
+
+        return res;
+    }
+
+    /*
+     * 解法3：
+     * - 思路：
+     * - 时间复杂度 O()，空间复杂度 O()。
+     * */
+    public static List<String> letterCombinations3(String digits) {
+        return null;
+    }
+
     public static void main(String[] args) {
-        log(letterCombinations("23"));  // expects ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
-        log(letterCombinations(""));    // expects []
+        log(letterCombinations2("23"));  // expects ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
+        log(letterCombinations2(""));    // expects []
     }
 }
