08b4ef6a42cfe99f081372f823e5467394616298
Max Jiang <maxjiang23326@gmail.com>
2019-11-23 22:10:31 +0800

    feat: add a solution for L47

--- src/main/java/RecursionAndBackTracking/Permutations/L47_PermutationsII.java
+++ src/main/java/RecursionAndBackTracking/Permutations/L47_PermutationsII.java
@@ -47,11 +47,10 @@ public class L47_PermutationsII {
     /*
      * 解法2：Iteration
      * - 思路：在 L46_Permutations 解法2的基础上加入用于去重的 Set。
-     * - 时间复杂度 O(n^n)，空间复杂度 O(n^n)。
+     * - 时间复杂度 O(n * n!)，空间复杂度 O(n * n!)。
      * */
     public static List<List<Integer>> permuteUnique2(int[] nums) {
         if (nums.length == 0) return new ArrayList<>();
-        Set<List<Integer>> set = new HashSet<>();
         Queue<List<Integer>> q = new LinkedList<>();
         q.offer(new ArrayList<>());
 
@@ -66,13 +65,39 @@ public class L47_PermutationsII {
             }
         }
 
-        return new ArrayList<>(new HashSet<>(q));
+        return new ArrayList<>(new HashSet<>(q));  // 最后 Set 去重
+    }
+
+    /*
+     * 解法3：Iteration + In-place swap
+     * - 思路：在 L46_Permutations 解法4的基础上加入用于去重的 Set。
+     * - 时间复杂度 O(n!)，空间复杂度 O(n)。
+     * */
+    public static List<List<Integer>> permuteUnique3(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums.length == 0) return res;
+        helper3(nums, 0, res);
+        return new ArrayList<>(new HashSet<>(res));  // 最后 Set 去重
+    }
+
+    private static void helper3(int[] nums, int i, List<List<Integer>> res) {
+        if (i == nums.length) {
+            List<Integer> list = new ArrayList<>();
+            for (int n : nums) list.add(n);
+            res.add(list);
+            return;
+        }
+        for (int j = i; j < nums.length; j++) {
+            swap(nums, i, j);
+            helper3(nums, i + 1, res);
+            swap(nums, i, j);
+        }
     }
 
     public static void main(String[] args) {
-        log(permuteUnique2(new int[]{1, 1, 2}));  // expects [[1,1,2], [1,2,1], [2,1,1]]
-        log(permuteUnique2(new int[]{1, 2}));     // expects [[1,2], [2,1]]
-        log(permuteUnique2(new int[]{1}));        // expects [[1]]
-        log(permuteUnique2(new int[]{}));         // expects []
+        log(permuteUnique3(new int[]{1, 1, 2}));  // expects [[1,1,2], [1,2,1], [2,1,1]]
+        log(permuteUnique3(new int[]{1, 2}));     // expects [[1,2], [2,1]]
+        log(permuteUnique3(new int[]{1}));        // expects [[1]]
+        log(permuteUnique3(new int[]{}));         // expects []
     }
 }
