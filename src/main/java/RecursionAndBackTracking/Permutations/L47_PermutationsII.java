367dd6230881691a6bd964bcd062c80840c07a15
Max Jiang <maxjiang23326@gmail.com>
2019-11-25 23:57:27 +0800

    feat: add a solution for L47

--- src/main/java/RecursionAndBackTracking/Permutations/L47_PermutationsII.java
+++ src/main/java/RecursionAndBackTracking/Permutations/L47_PermutationsII.java
@@ -3,6 +3,7 @@ package RecursionAndBackTracking.Permutations;
 import static Utils.Helpers.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
@@ -17,7 +18,7 @@ import java.util.Set;
 
 public class L47_PermutationsII {
     /*
-     * 解法1：Recursion + Backtracking
+     * 解法1：Recursion + Backtracking + Set
      * - 思路：在 L46_Permutations 解法3的基础上加入用于去重的 Set。
      * - 时间复杂度 O(n^n)，空间复杂度 O(n^n)。
      * */
@@ -45,7 +46,7 @@ public class L47_PermutationsII {
     }
 
     /*
-     * 解法2：Iteration
+     * 解法2：Iteration + Set
      * - 思路：在 L46_Permutations 解法2的基础上加入用于去重的 Set。
      * - 时间复杂度 O(n * n!)，空间复杂度 O(n * n!)。
      * */
@@ -69,7 +70,7 @@ public class L47_PermutationsII {
     }
 
     /*
-     * 解法3：Iteration + In-place swap
+     * 解法3：Iteration + In-place swap + Set
      * - 思路：在 L46_Permutations 解法4的基础上加入用于去重的 Set。
      * - 时间复杂度 O(n!)，空间复杂度 O(n)。
      * */
@@ -94,10 +95,50 @@ public class L47_PermutationsII {
         }
     }
 
+    /*
+     * 解法4：Recursion + Backtracking + Sort
+     * - 思路：不同于解法1、2、3，该解法不使用 Set 去重，而是先观察 test case，例如对于 nums=[1,2,1] 来说：
+     *                              []
+     *                  1/          2|          1\
+     *               [1]            [2]            [1]
+     *           2/     1\       1/    1\        1/    2\
+     *         [1,2]   [1,1]   [2,1]   [2,1]   [1,1]   [1,2]
+     *          1|      2|      1|      1|      2|      1|
+     *        [1,2,1] [1,1,2] [2,1,1] [2,1,1] [1,1,2] [1,2,1]
+     *
+     *   可见在对 []、[2] 进行
+     *
+     * - 时间复杂度 O(nlogn + n!)，空间复杂度 O(n)。
+     * */
+    public static List<List<Integer>> permuteUnique4(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums.length == 0) return res;
+        Arrays.sort(nums);  // 先排序，后面才能跳过 nums[i] == nums[i-1] 的元素
+        helper4(nums, new ArrayList<>(), new boolean[nums.length], res);
+        return res;
+    }
+
+    private static void helper4(int[] nums, List<Integer> list, boolean[] used, List<List<Integer>> res) {
+        if (list.size() == nums.length) {
+            res.add(new ArrayList<>(list));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) continue;
+            if (i > 0 && nums[i] == nums[i - 1] && used[i - 1]) continue;  // 跳过重复元素（不添加到 list 中）
+            list.add(nums[i]);
+            used[i] = true;
+            helper4(nums, list, used, res);
+            list.remove(list.size() - 1);
+            used[i] = false;
+        }
+    }
+
     public static void main(String[] args) {
-        log(permuteUnique3(new int[]{1, 1, 2}));  // expects [[1,1,2], [1,2,1], [2,1,1]]
-        log(permuteUnique3(new int[]{1, 2}));     // expects [[1,2], [2,1]]
-        log(permuteUnique3(new int[]{1}));        // expects [[1]]
-        log(permuteUnique3(new int[]{}));         // expects []
+        log(permuteUnique4(new int[]{1, 1, 2}));  // expects [[1,1,2], [1,2,1], [2,1,1]]
+        log(permuteUnique4(new int[]{1, 2, 1}));  // expects [[1,1,2], [1,2,1], [2,1,1]]
+        log(permuteUnique4(new int[]{1, 2}));     // expects [[1,2], [2,1]]
+        log(permuteUnique4(new int[]{1}));        // expects [[1]]
+        log(permuteUnique4(new int[]{}));         // expects []
     }
 }
