3575c7c62b04e5607c9ddeb766520225d6fd33c6
Max Jiang <maxjiang23326@gmail.com>
2019-11-26 08:54:54 +0800

    feat: add question L77

---
+++ src/main/java/RecursionAndBackTracking/Combinations/L77_Combinations.java
@@ -0,0 +1,26 @@
+package RecursionAndBackTracking.Combinations;
+
+import static Utils.Helpers.*;
+
+import java.util.List;
+
+/*
+ * Combinations
+ *
+ * - Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
+ * */
+
+public class L77_Combinations {
+    /*
+     * 解法1：
+     * - 思路：
+     * - 时间复杂度 O()，空间复杂度 O()。
+     * */
+    public static List<List<Integer>> combine(int n, int k) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        log(combine(4, 2));  // expects [[2,4],[3,4],[2,3],[1,2],[1,3],[1,4]]
+    }
+}
