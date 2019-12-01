62c3374180acffebd9fd0d14501901ac00cdb13d
Max Jiang <maxjiang23326@gmail.com>
2019-12-01 19:53:15 +0800

    feat: add a solution for L216

--- src/main/java/RecursionAndBackTracking/Combinations/L216_CombinationSumIII.java
+++ src/main/java/RecursionAndBackTracking/Combinations/L216_CombinationSumIII.java
@@ -2,13 +2,14 @@ package RecursionAndBackTracking.Combinations;
 
 import static Utils.Helpers.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /*
  * Combination Sum III
  *
  * - Find all possible combinations of k numbers that add up to a number n, given that only numbers from 1 to
- *   9 can be used and each combination should be a unique set of numbers.
+ *   9 can be used and each combination should be a unique set of numbers. (从1-9中选出 k 个数字，使他们的和为 n)
  *
  * - Notes:
  *   1. All numbers will be positive integers.
@@ -18,15 +19,30 @@ import java.util.List;
 public class L216_CombinationSumIII {
     /*
      * 解法1：Recursion + Backtracking
-     * - 思路：
-     * - 时间复杂度 O()，空间复杂度 O()。
+     * - 时间复杂度 << O(n^9)，空间复杂度 O(n)。
      * */
     public static List<List<Integer>> combinationSum(int k, int n) {
-        return null;
+        List<List<Integer>> res = new ArrayList<>();
+        if (k <= 0 || n <= 0) return res;
+        helper(k, n, 1, new ArrayList<>(), res);
+        return res;
+    }
+
+    private static void helper(int k, int n, int i, List<Integer> list, List<List<Integer>> res) {
+        if (n == 0 && k == 0) {
+            res.add(new ArrayList<>(list));
+            return;
+        }
+        for (int j = i; j <= 9 && j <= n; j++) {
+            list.add(j);
+            helper(k - 1, n - j, j + 1, list, res);
+            list.remove(list.size() - 1);
+        }
     }
 
     public static void main(String[] args) {
-        log(combinationSum(3, 7));  // expects [[1,2,4]]
-        log(combinationSum(3, 9));  // expects [[1,2,6], [1,3,5], [2,3,4]]
+        log(combinationSum(3, 7));   // expects [[1,2,4]]
+        log(combinationSum(3, 9));   // expects [[1,2,6], [1,3,5], [2,3,4]]
+        log(combinationSum(2, 18));  // expects []
     }
 }
