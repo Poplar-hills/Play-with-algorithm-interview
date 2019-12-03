d7f7b9c49c3939ea6de230e4ca22306fb1389c64
Max Jiang <maxjiang23326@gmail.com>
2019-12-03 22:41:20 +0800

    feat: add a solution for L90

--- src/main/java/RecursionAndBackTracking/Combinations/L90_SubsetsII.java
+++ src/main/java/RecursionAndBackTracking/Combinations/L90_SubsetsII.java
@@ -50,9 +50,41 @@ public class L90_SubsetsII {
         }
     }
 
+    /*
+     * 解法2：Recursion + Backtracking
+     * - 思路：类似 L78_Subsets 解法2的思路，但加入了对重复元素的检测。
+     *   例如对于 [2,1,2] 来说，首先排序后为 [1,2,2]。最初有空集解 []，之后：
+     *     - 若选用1，则将1追加到 [] 中，得到解 [1]；若不选用1，则还是 []；
+     *       将两个解合并得到 [[],[1]]。
+     *     - 若选用2，则将2追加到 [[],[1]] 中的每个里，得到解 [[2],[1,2]]；若不选用2，则还是 [[],[1]]；
+     *       将两部分合并得到 [[],[1],[2],[1,2]]。
+     *     - 若选用2 ∵ 与上一个元素重复 ∴ 只将2追加到 [[],[1],[2],[1,2]] 中的后2个解里，得到解 [[2,2],[1,2,2]]，
+     *       将两部分合并得到 [[],[1],[2],[1,2],[2,2],[1,2,2]]。
+     * - 时间复杂度 O(2^n)，空间复杂度 O(1)。
+     * */
+    public static List<List<Integer>> subsetsWithDup2(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums == null || nums.length == 0) return res;
+        Arrays.sort(nums);
+        res.add(new ArrayList<>());
+
+        int size = 0, start;
+        for (int i = 0; i < nums.length; i++) {
+            start = (i >= 1 && nums[i] == nums[i - 1]) ? size : 0;  // 若出现重复元素，则跳过上次 res 中的所有 subset；若不是重复元素，则正常从0开始
+            size = res.size();
+            for (int j = start; j < size; j++) {  // 遍历 res 中从索引 start 处开始的 subset
+                List<Integer> list = new ArrayList<>(res.get(j));
+                list.add(nums[i]);
+                res.add(list);
+            }
+        }
+
+        return res;
+    }
+
     public static void main(String[] args) {
-        log(subsetsWithDup(new int[]{1, 2, 2}));  // expects [[2], [1], [1,2,2], [2,2], [1,2], []]
-        log(subsetsWithDup(new int[]{2, 1, 2}));  // expects [[2], [1], [1,2,2], [2,2], [1,2], []]
-        log(subsetsWithDup(new int[]{1, 2, 3}));  // expects [[3], [1], [2], [1,2,3], [1,3], [2,3], [1,2], []]
+        log(subsetsWithDup2(new int[]{1, 2, 2}));  // expects [[2], [1], [1,2,2], [2,2], [1,2], []]
+        log(subsetsWithDup2(new int[]{2, 1, 2}));  // expects [[2], [1], [1,2,2], [2,2], [1,2], []]
+        log(subsetsWithDup2(new int[]{1, 2, 3}));  // expects [[3], [1], [2], [1,2,3], [1,3], [2,3], [1,2], []]
     }
 }
