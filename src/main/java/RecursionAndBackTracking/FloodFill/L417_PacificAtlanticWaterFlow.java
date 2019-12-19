ba9fa972c2552de1a8488e37fec42f674e4f92ae
Max Jiang <maxjiang23326@gmail.com>
2019-12-19 22:25:46 +0800

    feat: add a solution for L417

--- src/main/java/RecursionAndBackTracking/FloodFill/L417_PacificAtlanticWaterFlow.java
+++ src/main/java/RecursionAndBackTracking/FloodFill/L417_PacificAtlanticWaterFlow.java
@@ -2,7 +2,11 @@ package RecursionAndBackTracking.FloodFill;
 
 import static Utils.Helpers.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /*
  * Pacific Atlantic Water Flow
@@ -26,10 +30,61 @@ public class L417_PacificAtlanticWaterFlow {
      *   该题中对”联通”的限制为：
      *     1. 联通的标志是要能到达左/上边界以及右/下边界；
      *     2. 联通的过程是只能由高水位到达同级或更低水位。
-     * - 时间复杂度 O()，空间复杂度 O()。
+     *   第二点很容易判断，只需在 Flood Fill 遍历每个格子的邻居时对水位进行判断即可；而第一点的判断时机则是在 Flood Fill 的
+     *   某个分支抵达边界时进行，若有单个分支能同时抵达左/上以及右/下，过多个分支分别抵达左/上以及右/下则说明该 Flood Fill 的
+     *   起始格子是一个有效解。
+     *
+     * - 实现：根据以上思路可设计程序：
+     *     1. 主程序遍历 matrix，对每个格进行 Flood Fill；
+     *     2. Flood Fill 中的递归函数定义为：f(m,n) 表示从格子 [m,n] 开始进行 Flood Fill，若能到达左/上以及右/下则返回
+     *   Set(1,0)，若只到达左/上则返回 Set(1)，若只到达右/下则返回 Set(0)；
+     *     3. 若某分支走到某格已经获得 Set(1,0)，则可以提前返回，而若 Set 中只有一个数字则还需继续走下去；
+     *     4. 若某分支已经返回 Set(1,0)，则上层递归无需在走完其他分支，可以提前返回；
+     *     5. 每次 Flood Fill 过程中一个格子只能经过一次（否则可能产生无限循环），而不同的 Flood Fill 过程之间互不相干。
+     *
+     * - 时间复杂度 O(l*w * l*w)，空间复杂度 O(l*w)。
      * */
+
+    private static int l, w;
+    private static int[][] directions = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+
     public static List<List<Integer>> pacificAtlantic(int[][] matrix) {
-        return null;
+        List<List<Integer>> res = new ArrayList<>();
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return res;
+        l = matrix.length;
+        w = matrix[0].length;
+
+        for (int m = 0; m < l; m++) {
+            for (int n = 0; n < w; n++) {
+                Set<Integer> set = new HashSet<>();
+                boolean[][] filled = new boolean[l][w];  // 在每次 Flood Fill 过程中记录经过的格子
+                reachSeas(matrix, m, n, set, filled);
+                if (set.size() == 2)
+                    res.add(Arrays.asList(m, n));
+            }
+        }
+        return res;
+    }
+
+    private static void reachSeas(int[][] matrix, int m, int n, Set<Integer> set, boolean[][] filled) {
+        filled[m][n] = true;
+        if (m == 0 || n == 0) set.add(1);           // 1代表抵达了左/上界
+        if (m == l - 1 || n == w - 1) set.add(0);   // 0代表抵达了右/下界
+        if (set.size() == 2) return;                // 当1和0都有时说明本次 Flood Fill 已经找到解了
+
+        for (int[] d : directions) {
+            int newM = m + d[0], newN = n + d[1];
+            if (canFlowTo(matrix, m, n, newM, newN) && !filled[newM][newN]) {
+                reachSeas(matrix, newM, newN, set, filled);
+                if (set.size() == 2) return;        // 若已经找到解则提前退出遍历
+            }
+        }
+    }
+
+    private static boolean canFlowTo(int[][] matrix, int m, int n, int newM, int newN) {
+        return newM >= 0 && newM < l
+            && newN >= 0 && newN < w
+            && matrix[m][n] >= matrix[newM][newN];
     }
 
     public static void main(String[] args) {
