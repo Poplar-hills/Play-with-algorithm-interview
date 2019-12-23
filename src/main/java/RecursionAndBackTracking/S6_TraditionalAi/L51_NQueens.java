11d3c7136c1b3ab4c01f36f0ac23faec834dee07
Max Jiang <maxjiang23326@gmail.com>
2019-12-23 14:15:54 +0800

    feat: add question L51

--- src/main/java/RecursionAndBackTracking/S6_TraditionalAi/L51_NQueens.java
+++ src/main/java/RecursionAndBackTracking/S6_TraditionalAi/L51_NQueens.java
@@ -8,7 +8,8 @@ import java.util.List;
  * N Queens
  *
  * - The n-queens puzzle is the problem of placing n queens on an n×n chessboard such that no two queens
- *   attack each other ().
+ *   attack each other (in horizontal, vertical or diagonal direction).
+ *
  * - Given an integer n, return all distinct solutions to the n-queens puzzle. Each solution contains a
  *   distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an
  *   empty space respectively.
@@ -27,7 +28,7 @@ public class L51_NQueens {
     public static void main(String[] args) {
         log(solveNQueens(4));
         /*
-         * expects [
+         * expects [   // 4皇后问题有两个解
          *   [".Q..",
          *    "...Q"
          *    "Q..."
