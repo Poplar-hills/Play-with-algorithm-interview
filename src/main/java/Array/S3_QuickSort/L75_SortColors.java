6f86e399df244a239b5b20449a63397bf252b8a4
Max Jiang <maxjiang23326@gmail.com>
2020-01-04 18:11:17 +0800

    docs: update comments

--- src/main/java/Array/S3_QuickSort/L75_SortColors.java
+++ src/main/java/Array/S3_QuickSort/L75_SortColors.java
@@ -11,11 +11,10 @@ import static Utils.Helpers.swap;
  * - Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
  * - Note: You are not suppose to use the library's sort function for this problem.
  *
- * - 此题本质上就是数组排序，但是不同于一般的排序，题中给出了更多信息：只有3种颜色，且3种颜色的值是固定的0, 1, 2 ∴ 有3种思路：
- *   1. 直接使用快排、归并排序等通用排序算法，则最低是 O(nlogn) 的复杂度；
- *   2. 从信息中可知数组元素的取值范围是固定的，且范围很小，因此适用计数排序，复杂度为 O(2n)（因为遍历数组2次）。
- *   3. 因为数组元素刚好只有3种取值，因此可采用三路排序的思路，将 == 0 的放到头部，== 1 的放到中间，== 2 的放到尾部。
- *      而且因为只有3种取值，因此只需遍历数组一遍即可排好，因此复杂度为 O(n)。
+ * - 分析：该题本质上就是数组排序，但是不同于一般的排序，题中给出了更多信息：只有3种颜色，且3种颜色的值是固定的0, 1, 2 ∴ 有3种思路：
+ *   1. 直接使用通用的快排、归并排序算法 ∴ 最低是 O(nlogn) 的复杂度；
+ *   2. 从信息中可知数组元素的取值范围是固定的，且范围很小 ∴ 适用计数排序，复杂度为 O(2n)（遍历数组2次）。
+ *   3. ∵ 数组元素刚好只有3种取值 ∴ 可在三路快排的基础上去掉递归过程，只遍历数组一遍即可排好 ∴ 复杂度为 O(n)。
  *
  * - 可见很多面试题实际上是对经典算法思想的应用，因此：
  *   1. 对经典算法思想要熟练掌握；
@@ -24,11 +23,29 @@ import static Utils.Helpers.swap;
 
 public class L75_SortColors {
     /*
-     * 解法1：计数排序
+     * 解法1：3-way quick sort
+     * - 思路：标准三路快排。
+     * - 时间复杂度 O(nlogn)，空间复杂度 O(1)。
+     * */
+    private static void sortColors(int[] arr) {
+
+    }
+
+    /*
+     * 解法2：Merge sort
+     * - 思路：标准归并排序。
+     * - 时间复杂度 O(nlogn)，空间复杂度 O(1)。
+     * */
+    private static void sortColors2(int[] arr) {
+
+    }
+
+    /*
+     * 解法3：Counting sort（计数排序）
      * - 时间复杂度 O(2n)，遍历数组2遍；
      * - 空间复杂度 O(1)。
      * */
-    private static void sortColors(int[] arr) {
+    private static void sortColors3(int[] arr) {
         int[] buckets = new int[3];               // 构造 bucket 数组，三个位置分别存储 arr 中0，1，2的个数（计数过程）
         for (int i = 0; i < arr.length; i++) {    // 遍历 arr 填充 bucket
             assert(arr[i] >= 0 && arr[i] <= 2);   // 在赋值之前先检元素的有效性
@@ -41,11 +58,12 @@ public class L75_SortColors {
     }
 
     /*
-     * 解法2：三路快速排序
-     * - 时间复杂度 O(n)，只遍历数组1遍；
+     * 解法4：Single round 3-way quick sort（单遍三路快排）
+     * - 思路：∵ arr 中只有三种值 ∴ 三路快排只需遍历一遍就可以完成排序，无需对每个分区进行递归。
+     * - 时间复杂度 O(n) ∵ 没有递归过程 ∴ 只遍历数组1遍 ∴ 复杂度是 O(n)；
      * - 空间复杂度 O(1)。
      * */
-    private static void sortColors2(int[] arr) {
+    private static void sortColors4(int[] arr) {
         int last0Index = -1;                  // 指向最后一个等于0的元素
         int first2Index = arr.length;         // 指向第一个等于2的元素
         for (int i = 0; i < first2Index; ) {  // 手动控制 i 的自增（∵ arr[i] == 2 时 i 不需要自增）
