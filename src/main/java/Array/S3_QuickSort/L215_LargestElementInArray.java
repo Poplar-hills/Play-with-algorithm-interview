c82bb1bd37813373a2f8cc43492527df48b4a20c
Max Jiang <maxjiang23326@gmail.com>
2020-01-06 22:20:21 +0800

    feat: add a solution for L215

--- src/main/java/Array/S3_QuickSort/L215_LargestElementInArray.java
+++ src/main/java/Array/S3_QuickSort/L215_LargestElementInArray.java
@@ -1,5 +1,6 @@
 package Array.S3_QuickSort;
 
+import java.util.Collections;
 import java.util.PriorityQueue;
 import java.util.Random;
 
@@ -12,7 +13,7 @@ import static Utils.Helpers.swap;
  * - Find the kth largest element in an unsorted array.
  *
  * - 分析：该问题也是个排序问题 ∴ 思路可以有：
- *   1. 先对数组进行整体排序，再取 k 个元素，若使用快排/归并/堆排序，则复杂度为 O(nlogn)。
+ *   1. 先对数组进行整体排序，再取 k 个元素，若使用快排 or 归并 or 堆排序。
  *   2. 不对数组进行整体排序，而是使用最小堆，在往堆中插入元素的过程中让堆大小一直保持在 k，这样堆中始终保留的是当前最大的 k 个
  *      元素，当遍历结束后堆顶元素就是第 k 大的元素。
  *   3. 采用三路快排的思路对数组不断进行分组（每次只需对 k 所在的分组进行递归），直到分组索引 == k 时即找到了第 k 大元素。
@@ -22,10 +23,22 @@ import static Utils.Helpers.swap;
 public class L215_LargestElementInArray {
     /*
      * 解法1：Heap sort（堆排序）
-     * - 思路：使用最大堆，extractMax n-k 次，若元素 > 堆顶元素
-     * - 时间复杂度 O((2n-k)logn)
+     * - 思路：即分析中的思路1。
+     * - 时间复杂度 O((n+k)logn)
      * */
     public static int kthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for (int n : nums) pq.add(n);            // ∵ PriorityQueue 没有 heapify 方法 ∴ 需要手动添加
+        for (int i = 1; i < k; i++) pq.poll();   // 将最大的 k-1 个元素从堆中移除
+        return pq.poll();
+    }
+
+    /*
+     * 解法2：Heap sort（堆排序）
+     * - 思路：
+     * - 时间复杂度 O((2n-k)logn)
+     * */
+    public static int kthLargest2(int[] nums, int k) {
         PriorityQueue<Integer> pq = new PriorityQueue<>(k + 1);
         for (int n : nums) {
             pq.add(n);
@@ -36,12 +49,12 @@ public class L215_LargestElementInArray {
     }
 
     /*
-     * 解法2：快排思路
+     * 解法3：快排思路
      * - 该快排是从大到小排序，这样才能满足"第几大"的需求（如果是从小到大排序，就只能找到"第几小"）
      * - 要从大到小排序，则需要：[v|--- > v ---|--- == v ---|...|--- < v ---]
      *                         l           gt             i   lt        r
      * */
-    public static int kthLargest2(int[] nums, int k) {
+    public static int kthLargest3(int[] nums, int k) {
         return quickSelect(nums, 0, nums.length - 1, k - 1);  // k-1 是为了让语义更自然（"第1小"就是最小，"第2小"就是第2小，没有"第0小"）
     }
 
