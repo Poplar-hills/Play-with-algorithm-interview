2c4c3abbfa00f38e5ba6cc62522dce932bf34b14
Max Jiang <maxjiang23326@gmail.com>
2020-01-07 14:08:02 +0800

    docs: update comments

--- src/main/java/Array/S4_TwoPointerCollision/L11_ContainerWithMostWater.java
+++ src/main/java/Array/S4_TwoPointerCollision/L11_ContainerWithMostWater.java
@@ -3,19 +3,31 @@ package Array.S4_TwoPointerCollision;
 import static Utils.Helpers.log;
 
 /*
-* Container With Most Water
-*
-* */
+ * Container With Most Water
+ *
+ * - Find two lines, which together with x-axis forms a container that contains the most water.
+ *
+ *         |                   |
+ *         |                   |       |
+ *         |   |               |       |
+ *         |   |       |       |       |
+ *         |   |       |   |   |       |
+ *         |   |       |   |   |   |   |
+ *         |   |   |   |   |   |   |   |
+ *     |   |   |   |   |   |   |   |   |
+ *   --------------------------------------   line 1 and line 8 forms the largest container (49 units)
+ *     0   1   2   3   4   5   6   7   8
+ * */
 
 public class L11_ContainerWithMostWater {
     /*
-    * 解法1：指针对撞
-    * - 思路：
-    *   - 面积取决于两个因素：1.两个元素的间距 2.两个元素本身的大小。
-    *   - 在两个指针不断靠近的过程中，只有不断寻找更大的元素才有可能获得更大的面积，对撞的过程就是在不断补短板的过程 —— 看左、右两边
-    *     哪边是短板，让短的一边向对面移动一步，看是否能获得更大的面积。
-    * - 时间复杂度 O(n)，空间复杂度 O(1)。
-    * */
+     * 解法1：指针对撞
+     * - 思路：
+     *   - 面积取决于两个因素：1.两个元素的间距 2.两个元素本身的大小。
+     *   - 在两个指针不断靠近的过程中，只有不断寻找更大的元素才有可能获得更大的面积，对撞的过程就是在不断补短板的过程 —— 看左、右两边
+     *     哪边是短板，让短的一边向对面移动一步，看是否能获得更大的面积。
+     * - 时间复杂度 O(n)，空间复杂度 O(1)。
+     * */
     public static int maxArea(int[] arr) {
         assert arr.length >= 2;
         int maxArea = 0;
@@ -32,11 +44,11 @@ public class L11_ContainerWithMostWater {
     }
 
     /*
-    * 解法2：解法1的优化版
-    * - 思路：在解法1的基础上，当每次找到短板时，不马上进入下一次循环求面积，而是先检查下一块板子是否比当前这块长，若还没当前
-    *   这块长，则继续寻找。
-    * - 时间复杂度 O(n)，空间复杂度 O(1)。
-    * */
+     * 解法2：解法1的优化版
+     * - 思路：在解法1的基础上，当每次找到短板时，不马上进入下一次循环求面积，而是先检查下一块板子是否比当前这块长，若还没当前
+     *   这块长，则继续寻找。
+     * - 时间复杂度 O(n)，空间复杂度 O(1)。
+     * */
     public static int maxArea2(int[] arr) {
         assert arr.length >= 2;
         int maxArea = 0;
