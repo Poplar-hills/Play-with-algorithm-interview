fd42187b6f0098dbc5e06e320d77927457eeab1e
Max Jiang <maxjiang23326@gmail.com>
2020-01-08 13:02:09 +0800

    docs: update comments

--- src/main/java/Array/S4_TwoPointerCollision/L344_ReverseString.java
+++ src/main/java/Array/S4_TwoPointerCollision/L344_ReverseString.java
@@ -9,14 +9,17 @@ import static Utils.Helpers.log;
  * - You must do this by modifying the input array in-place with O(1) extra memory.
  * - Assume all the characters are ASCII characters.
  *
- * - 不相关的注释：
+ * - 👉语法技巧：
  *   1. Java 中将 char[] 连接成一个 String 的方式：String str = new String(chars);
  *   2. Java 中 reverse 一个 String 的最快方式：new StringBuilder(str).reverse().toString();
+ *   3. 虽然 Utils/Helpers.java 中有 generic swap() 方法，但 ∵ 本题中的输入是原始类型 char[]，而泛型不支持原始类型
+ *      ∴ 无法使用。Java 中的泛型只存在于编译期，编译到字节码之后泛型会被擦除，即泛型不会进入到运行时阶段。而在进行泛型擦除时，
+ *      泛型值需要被转换成 Object ∴ 能被用作泛型的必须是能转成 Object 的类型，而原始类型是不支持这一点的。
  * */
 
 public class L344_ReverseString {
     /*
-     * 解法1：指针对撞，不断交换
+     * 解法1：指针对撞 + swap
      * - 时间复杂度 O(n)，空间复杂度 O(1)。
      * */
     public static void reverseString(char[] s) {
