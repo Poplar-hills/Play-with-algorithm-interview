c19467a0fd0ff702676e55e74667caf245179ba6
Max Jiang <maxjiang23326@gmail.com>
2020-02-16 17:29:48 +0800

    docs: update comments

--- src/main/java/HashTable/S2_ClassicHashTable/L15_3Sum.java
+++ src/main/java/HashTable/S2_ClassicHashTable/L15_3Sum.java
@@ -15,7 +15,7 @@ import static Utils.Helpers.log;
 
 public class L15_3Sum {
     /*
-     * 解法1：2Sum + 1-pass memoization + Set 去重
+     * 解法1：2Sum（1-pass memoization + Set 去重）
      * - 思路：首先大体思路是在遍历过程中，每次固定一个元素 nums[i]，然后在 (i..] 范围内进行 2Sum 搜索。但这样可能会得到重复
      *   的结果（如 test case 1 中 [-1,0,1]、[0,1,-1]）∴ 需要对解进行去重。∵ 解是使用 List 记录的 ∴ 若采用 Set 对 List
      *   去重，则需要所有 List 有序 ∴ 最简单的办法就是直接在最开始对 nums 排序，从而后面得到的每个解都是有序的，从而可以去重。
@@ -42,12 +42,11 @@ public class L15_3Sum {
     }
 
     /*
-     * 解法2：2Sum + 指针对撞 + Set 去重
-     * - 思路：同样也是通过在遍历时挨个固定元素，将 3Sum 化简为 2Sum 问题。
-     * - 实现：在“将 3Sum 转化为 2Sum 问题”的思路下可以采用 L1_TwoSum 中的任何一种实现来解决 2Sum 问题。本解法采用解法2的
-     *   指针对撞实现。而指针对撞的前提也是数组有序 ∴ 同样需要先对 nums 排序。
+     * 解法2：2Sum（指针对撞 + Set 去重）
+     * - 思路：与解法1一致，也是通过在遍历时挨个固定元素，将 3Sum 化简为 2Sum 问题。
+     * - 实现：在“将 3Sum 转化为 2Sum 问题”的思路下可以采用 L1_TwoSum 中的任何一种实现来解决 2Sum 问题。本解法采用 L1 中
+     *   解法2的指针对撞实现。而 ∵ 指针对撞的前提也是数组有序 ∴ 同样需要先对 nums 排序。
      * - 时间复杂度 O(n^2)，空间复杂度 O(n)。
-     * - 通过 Set 去重，代码更简洁，但效率低一些，因为重复结果会先存入 Set，再利用 Set 去重。
      * */
     public static List<List<Integer>> threeSum2(int[] nums) {
         if (nums == null || nums.length < 3) return new ArrayList<>();
@@ -69,10 +68,44 @@ public class L15_3Sum {
     }
 
     /*
-     * 解法3：2Sum + 指针对撞 + 手动去重
-     * - 时间复杂度 O(n^2)，空间复杂度 O(n)。
+     * 解法3：2Sum（二分查找 + Set 去重）
+     * - 思路：与解法1、2一致。
+     * - 实现：采用 L1 中解法3的 Binary Search。而 ∵ Binary Search 的前提也是数组有序 ∴ 同样需要先对 nums 排序。
+     * - 时间复杂度 O(n^2 * logn)，空间复杂度 O(1)。
      * */
     public static List<List<Integer>> threeSum3(int[] nums) {
+        if (nums == null || nums.length < 3) return new ArrayList<>();
+        Set<List<Integer>> resSet = new HashSet<>();
+        int n = nums.length;
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                int complement = 0 - nums[i] - nums[j];
+                int k = binarySearch(nums, j + 1, n - 1, complement);
+                if (k != -1) resSet.add(Arrays.asList(nums[i], nums[j], nums[k]));
+            }
+        }
+
+        return new ArrayList<>(resSet);
+    }
+
+    private static int binarySearch(int[] nums, int l, int r, int target) {
+        if (l > r) return -1;
+        int mid = (r - l) / 2 + l;
+        if (target < nums[mid]) return binarySearch(nums, l, mid - 1, target);
+        if (target > nums[mid]) return binarySearch(nums, mid + 1, r, target);
+        return mid;
+    }
+
+    /*
+     * 解法4：2Sum + 指针对撞 + 手动去重
+     * - 思路：与解法2一致。
+     * - 实现：解法1、2都是使用 Set 自动去重，虽然方便但是效率较低 ∵ 还是要先找到解，再通过 Set 检查是否重复。
+     * - 时间复杂度 O(n^2)，空间复杂度 O(n)。
+     * */
+    public static List<List<Integer>> threeSum4(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         if (nums == null || nums.length < 3) return res;
 
@@ -98,12 +131,12 @@ public class L15_3Sum {
     }
 
     /*
-     * 解法4：查找表 + 手动去重
+     * 解法5：查找表 + 手动去重
      * - 思路：类似 L1_TwoSum 中的解法4。
      * - 时间复杂度 O(n^2)，空间复杂度 O(n)。
      * - 还可以采用 查找表 + Set 去重的解法，这里不再赘述。
      * */
-    public static List<List<Integer>> threeSum4(int[] nums) {
+    public static List<List<Integer>> threeSum5(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         if (nums == null || nums.length < 3) return res;
         Map<Integer, Integer> map = new HashMap<>();
@@ -124,63 +157,6 @@ public class L15_3Sum {
             }
         }
         return res;
-    }
-
-    /*
-     * 解法5：查找表 + Set 去重
-     * - 思路：与解法3对照，解法2中是固定第一个元素后，内部指针对撞（同时改变两个变量）；而该解法是固定第一个元素后，再固定第二个元素，
-     *   内部只有一个变量，因此可以用 Map 进行查找。
-     * - 时间复杂度 O(n^2)，空间复杂度 O(n)。
-     * */
-    public static List<List<Integer>> threeSum5(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        if (nums == null || nums.length < 3) return res;
-
-        Map<Integer, Integer> map = new HashMap<>();
-        Set<List<Integer>> set = new HashSet<>();
-        Arrays.sort(nums);
-
-        for (int i = 0; i < nums.length; i++)
-            map.put(nums[i], i);  // 可能产生覆盖，但后添加的元素的索引一定比先添加的元素的索引大，因此在下面 map.get(complement) > j 处刚好不会有问题
-
-        for (int i = 0; i < nums.length - 2; i++) {          // 固定第一个元素
-            for (int j = i + 1; j < nums.length - 1; j++) {  // 固定第二个元素
-                int complement = 0 - nums[i] - nums[j];
-                if (map.containsKey(complement) && map.get(complement) > j)  // 在 map 中查找第三个元素，第三个元素的索引必须 > 第二个元素的索引（保证不使用重复元素）
-                    set.add(Arrays.asList(nums[i], nums[j], complement));
-            }
-        }
-        return new ArrayList<>(set);
-    }
-
-    /*
-    * 解法6：二分查找 + Set 去重
-    * - 思路：在固定前两个元素的情况下，不使用查找表，而是使用二分查找来在找到 complement（因为数组有序）。
-    * - 时间复杂度 O(n^2 * logn)，空间复杂度 O(1)。
-    * */
-    public static List<List<Integer>> threeSum6(int[] nums) {
-        if (nums == null || nums.length < 3) return new ArrayList<>();
-        Set<List<Integer>> res = new HashSet<>();
-        Arrays.sort(nums);
-
-        for (int i = 0; i < nums.length - 2; i++) {
-            for (int j = i + 1; j < nums.length - 1; j++) {
-                int complement = 0 - nums[i] - nums[j];
-                int k = binarySearch(nums, complement, j + 1, nums.length - 1);
-                if (k != -1) res.add(Arrays.asList(nums[i], nums[j], nums[k]));
-            }
-        }
-        return new ArrayList<>(res);
-    }
-
-    private static int binarySearch(int[] nums, int target, int l, int r) {
-        if (l > r) return -1;
-        int mid = (r - l) / 2 + l;
-        if (target < nums[mid])
-            return binarySearch(nums, target, l, mid - 1);
-        if (target > nums[mid])
-            return binarySearch(nums, target, mid + 1, r);
-        return mid;
     }
 
     public static void main(String[] args) {
