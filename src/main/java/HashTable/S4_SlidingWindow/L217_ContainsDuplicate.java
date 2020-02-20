1ee3b682d39eb22aac931e258bc8837d0e52f4e6
Max Jiang <maxjiang23326@gmail.com>
2020-02-20 13:10:36 +0800

    docs: update comments

--- src/main/java/HashTable/S4_SlidingWindow/L217_ContainsDuplicate.java
+++ src/main/java/HashTable/S4_SlidingWindow/L217_ContainsDuplicate.java
@@ -5,16 +5,16 @@ import java.util.*;
 import static Utils.Helpers.log;
 
 /*
-* Contains Duplicate
-*
-* - 给定一个整型数组，若数组中存在相同的元素则返回 true。
-* */
+ * Contains Duplicate
+ *
+ * - 给定一个整型数组，若数组中存在相同的元素则返回 true。
+ * */
 
 public class L217_ContainsDuplicate {
     /*
-    * 解法1：查找表
-    * - 时间复杂度 O(n)，空间复杂度 O(n)。
-    * */
+     * 解法1：查找表
+     * - 时间复杂度 O(n)，空间复杂度 O(n)。
+     * */
     public static boolean containsDuplicate(int[] nums) {
         Set<Integer> set = new HashSet<>();
         for (int n : nums) {
@@ -30,16 +30,15 @@ public class L217_ContainsDuplicate {
      * */
     public static boolean containsDuplicate2(int[] nums) {
         Arrays.sort(nums);
-        for (int i = 1; i < nums.length; i++) {
+        for (int i = 1; i < nums.length; i++)
             if (nums[i] == nums[i - 1])
                 return true;
-        }
         return false;
     }
 
     public static void main(String[] args) {
-        log(containsDuplicate2(new int[] {1, 2, 3, 1}));  // expects true
-        log(containsDuplicate2(new int[] {1, 2, 3, 4}));  // expects false
-        log(containsDuplicate2(new int[] {1, 1, 1, 3, 3, 4, 3, 2, 4, 2}));  // expects true
+        log(containsDuplicate2(new int[]{1, 2, 3, 1}));  // expects true
+        log(containsDuplicate2(new int[]{1, 2, 3, 4}));  // expects false
+        log(containsDuplicate2(new int[]{}));            // expects false
     }
 }
