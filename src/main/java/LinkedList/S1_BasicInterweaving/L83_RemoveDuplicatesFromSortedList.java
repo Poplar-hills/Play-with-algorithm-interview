cff9116552e5cb91b19cb6c2fd619e643568338c
Max Jiang <maxjiang23326@gmail.com>
2020-03-02 08:58:03 +0800

    feat: add a solution for L83

--- src/main/java/LinkedList/S1_BasicInterweaving/L83_RemoveDuplicatesFromSortedList.java
+++ src/main/java/LinkedList/S1_BasicInterweaving/L83_RemoveDuplicatesFromSortedList.java
@@ -30,12 +30,34 @@ public class L83_RemoveDuplicatesFromSortedList {
     /*
      * 解法2：递归
      * - 思路：与解法1一致。
+     * - 实现：在递归去程路上跳过下一个节点。
      * - 时间复杂度 O(n)，空间复杂度 O(1)。
      * */
     public static ListNode deleteDuplicates2(ListNode head) {
+        helper2(head);
+        return head;  // ∵ 在递归去程路上跳过下一个节点就无法返回链表头结点了 ∴ 递归返回类型为 void，并需要在外部单独返回头结点
+    }
+    public static void helper2(ListNode head) {
+        if (head == null || head.next == null)
+            return;
+        if (head.val == head.next.val) {
+            head.next = head.next.next;
+            helper2(head);
+        } else {
+            helper2(head.next);
+        }
+    }
+
+    /*
+     * 解法3：递归（解法2的简化版）
+     * - 思路：与解法1、2一致。
+     * - 实现：在递归回程路上跳过当前节点。
+     * - 时间复杂度 O(n)，空间复杂度 O(1)。
+     * */
+    public static ListNode deleteDuplicates3(ListNode head) {
         if (head == null || head.next == null) return head;
-        head.next = deleteDuplicates2(head.next);
-        return head.val == head.next.val ? head.next : head;
+        head.next = deleteDuplicates3(head.next);             // 先递归到底
+        return head.val == head.next.val ? head.next : head;  // 回程路上跳过/不跳过当前节点
     }
 
     public static void main(String[] args) {
