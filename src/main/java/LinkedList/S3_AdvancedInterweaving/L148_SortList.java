1a61e71ee9dac4fcdc1ccbde456d5e1f65bfbf7f
Max Jiang <maxjiang23326@gmail.com>
2020-03-09 23:01:45 +0800

    docs: update comment

--- src/main/java/LinkedList/S3_AdvancedInterweaving/L148_SortList.java
+++ src/main/java/LinkedList/S3_AdvancedInterweaving/L148_SortList.java
@@ -14,46 +14,49 @@ public class L148_SortList {
     /*
      * 解法1：Merge Sort (top-down)
      * - 注意：该解法的空间复杂度未达到题目要求。
-     * - 思路：因为要求用 O(nlogn) 的算法排序，而 Quick Sort 非常依赖于数组的随机访问能力，不适用于链表，而 Merge Sort 适用于链表。
-     *        要使用 Merge Sort 为链表排序，则需解决：1. 如何对链表进行二分（分成前半部分和后半部分）；2. 如何对各自有序的链表进行合并。
-     *        1. 采用类似 Floyd Cycle detection 的方法，指针 slow 每次走1步，指针 fast 每次走2步，当 fast 到头时，slow 一定指向
-     *           链表中点。这里有个小坑 —— 当 fast 和 slow 都到达各自位置后，需要断开 fast 最后一个节点和 slow 第一个节点之间的链接，
-     *           否则往下递归二分时 fast 仍然是整个链表，而非前半部分。
-     *        2. 没啥好说的。
+     * - 思路：∵ 要求 O(nlogn)，而 Quick Sort 非常依赖于对元素的随机访问能力 ∴ 不适用于链表，而 Merge Sort 则不存在这个
+     *   问题，对链表更加适用。
+     * - 实现：要使用 Merge Sort 为链表排序，需要能够：
+     *     1. 对链表进行二分（分成前半部分和后半部分）；
+     *     2. 对两个有序链表进行合并。
+     *   对于这两个需求，解法如下：
+     *     1. 采用 Floyd Cycle detection 的思路，指针 slow 每次走1步，指针 fast 每次走2步，当 fast 到头时，slow 一定
+     *        指向链表中点。这里有个小坑 —— 当 slow 到达中点后，需要断开 slow 的前一节点和 slow 之间的链接，否则往下递归
+     *        二分时处理的仍然是整个链表，而非前半部分。
+     *     2. 见 L21_MergeTwoSortedLists。
      * - 时间复杂度 O(nlogn)，空间复杂度 O(n)（递归深度是 logn，但任意时刻都需要存储 n 个元素，因此总体是 O(n)）。
      * */
     public static ListNode sortList(ListNode head) {
-        if (head == null || head.next == null)
-            return head;
+        if (head == null || head.next == null) return head;
 
-        ListNode slow = head, fast = head, prevOfSlow = null;
-        while (fast != null && fast.next != null) {
-            prevOfSlow = slow;
+        ListNode prevSlow = null, slow = head, fast = head;
+        while (fast != null && fast.next != null) {  // 直到 fast 走到最后一个节点才停止循环
+            prevSlow = slow;
             slow = slow.next;
             fast = fast.next.next;
         }
-        prevOfSlow.next = null;
+        prevSlow.next = null;  // 将链表的前半截和后半截断开，以便于后面分别递归前、后半段链表
 
-        return merge(sortList(head), sortList(slow));
+        return merge(sortList(head), sortList(slow));  // 继续二分，直到每个节点为一组时开始 merge
     }
 
     private static ListNode merge(ListNode l1, ListNode l2) {
         ListNode dummyHead = new ListNode(), prev = dummyHead;
-        ListNode n1 = l1, n2 = l2;
 
-        while (n1 != null && n2 != null) {
-            if (n1.val < n2.val) {
-                prev.next = n1;
-                n1 = n1.next;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
             } else {
-                prev.next = n2;
-                n2 = n2.next;
+                prev.next = l2;
+                l2 = l2.next;
             }
             prev = prev.next;
         }
 
-        if (n1 != null) prev.next = n1;
-        if (n2 != null) prev.next = n2;
+        if (l1 != null) prev.next = l1;
+        if (l2 != null) prev.next = l2;
+
         return dummyHead.next;
     }
 
@@ -164,12 +167,12 @@ public class L148_SortList {
 
     public static void main(String[] args) {
         ListNode l1 = createLinkedList(new int[]{5, 1, 6, 4, 2, 7, 3});
-        printLinkedList(sortList3(l1));  // expects 1->2->3->4->5->6->7->NULL
+        printLinkedList(sortList(l1));  // expects 1->2->3->4->5->6->7->NULL
 
         ListNode l2 = createLinkedList(new int[]{4, 2, 1, 3});
-        printLinkedList(sortList3(l2));  // expects 1->2->3->4->NULL
+        printLinkedList(sortList(l2));  // expects 1->2->3->4->NULL
 
         ListNode l3 = createLinkedList(new int[]{-1, 5, 3, 4, 0});
-        printLinkedList(sortList3(l3));  // expects -1->0->3->4->5->NULL
+        printLinkedList(sortList(l3));  // expects -1->0->3->4->5->NULL
     }
 }
