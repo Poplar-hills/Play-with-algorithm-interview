b629d81566aa771e15e4a6243e17e145211e50e9
Max Jiang <maxjiang23326@gmail.com>
2020-03-11 23:08:18 +0800

    refactor: rename

--- src/main/java/LinkedList/S5_DoublePointer/L19_RemoveNthNodeFromEndOfList.java
+++ src/main/java/LinkedList/S5_DoublePointer/L19_RemoveNthNodeFromEndOfList.java
@@ -12,59 +12,63 @@ import static Utils.Helpers.*;
 
 public class L19_RemoveNthNodeFromEndOfList {
     /*
-     * 解法1：两次遍历
-     * - 思路：先求得链表长度，再找到并删除目标节点。
+     * 解法1：获取长度（Two-pass）
+     * - 思路：先求链表长度，再找到并删除目标节点。
      * - 时间复杂度 O(n)，空间复杂度 O(1)。
-     * - 不足：∵ 需要遍历两遍 ∴ 是 two pass algorithm。
      * */
     public static ListNode removeNthFromEnd(ListNode head, int n) {
-        if (head == null || n < 0) return head;
+        if (head == null || n < 1) return null;
+        int len = getLength(head);
 
-        int len = 0;
-        for (ListNode curr = head; curr != null; curr = curr.next)  // 取得链表总长度
-            len++;
-        if (n > len) return head;  // 处理 n 越界的情况
+        if (len < n) return head;
+        int m = len - n;   // 从头开始第 m 个元素即是待删除元素（m 从 0 开始）
 
-        ListNode dummyHead = new ListNode();
-        ListNode prev = dummyHead, curr = head;
+        ListNode dummyHead = new ListNode();  // ∵ 待删除的可能是第一个节点 ∴ 需要 dummyHead
         dummyHead.next = head;
+        ListNode prev = dummyHead, curr = head;
 
-        while (len != n) {      // 找到指定节点（这种方法很巧，通过让 len-- 并与 n 比较来找到倒数第 n 个节点）
-            len--;
-            prev = curr;
+        while (m-- != 0) {
+            prev = prev.next;
             curr = curr.next;
         }
+        prev.next = curr.next;
 
-        prev.next = curr.next;  // 删除（跳过）n 处的节点
         return dummyHead.next;
     }
 
+    private static int getLength(ListNode head) {
+        int len = 0;
+        for ( ; head != null; head = head.next) len++;
+        return len;
+    }
+
     /*
-     * 解法2：双指针 + 一次遍历
-     * - 思路：通过双指针同时移动找到待删除节点的上一节点。例如对于 1->2->3->4->5->NULL，n=2 来说：
-     *   1. 要删除的节点是4，因此只要获得节点3即可完成删除；
-     *   2. 节点3与 NULL 的差距是 n+1，因此可以设置两个指针，并让他们的初始差距为 n+1，然后同时移动当右指针到达 NULL 时，左指针即
-     *      到达待删除节点的上一节点；
-     * - 时间复杂度 O(n)，空间复杂度 O(1)（∵ 但只遍历一遍 ∴ 是 one pass algorithm，比解法1更优）。
+     * 解法2：双指针（One-pass）
+     * - 思路：解法1中的瓶颈在于需要先获取链表长度。而另一种思路是通过双指针同时移动找到待删除节点的上一节点。例如：
+     *   对于 1->2->3->4->5->null，n=2 来说：
+     *   1. 待删除的节点4 ∴ 只要获得节点3即可完成删除；
+     *   2. 虽然不知道链表长度，但能知道节点3与 null 的差距是 n+1 ∴ 可以利用这个差距，设置两个初始差距为 n+1 的指针，然后
+     *      让他们同时移动，当右指针到达 null 时，左指针即指向节点3；
+     * - 时间复杂度 O(n)，空间复杂度 O(1)。
      * */
     public static ListNode removeNthFromEnd2(ListNode head, int n) {
         if (head == null || n < 0) return head;
 
-        ListNode dummnyHead = new ListNode();            // ∵ 待删除的可能是第一个节点 ∴ 需要 dummyHead
-        ListNode left = dummnyHead, right = dummnyHead;  // 要删除节点就一定需要虚拟头结点
+        ListNode dummnyHead = new ListNode();
         dummnyHead.next = head;
+        ListNode l = dummnyHead, r = dummnyHead;
 
-        for (int i = 0; i < n + 1; i++) {     // 先将 right 移动到距离 left 指针 n+1 位置的地方
-            if (right == null) return head;   // 处理 n 越界的情况
-            right = right.next;
+        for (int i = 0; i < n + 1; i++) {  // 先将 r 移动到距 l n+1 的位置
+            if (r == null) return head;    // 处理 n 越界的情况
+            r = r.next;
         }
 
-        while (right != null) {  // 同时移动两个指针，当 right 到达 null 时，left 就到了待删除节点的前一节点上了
-            left = left.next;
-            right = right.next;
+        while (r != null) {     // 同时移动两个指针，当 r 到达 null 时，l 就到了待删除节点的前一节点上了
+            l = l.next;
+            r = r.next;
         }
+        l.next = l.next.next;   // 删除节点
 
-        left.next = left.next.next;  // 删除节点
         return dummnyHead.next;
     }
 
