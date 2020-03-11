c93630ecc79e45eb4d3293816a3eb2cd83832db4
Max Jiang <maxjiang23326@gmail.com>
2020-03-11 13:52:05 +0800

    feat: add a solution to L61

--- src/main/java/LinkedList/S5_DoublePointer/L61_RotateList.java
+++ src/main/java/LinkedList/S5_DoublePointer/L61_RotateList.java
@@ -13,14 +13,14 @@ import static Utils.Helpers.*;
 public class L61_RotateList {
     /*
      * 解法1：截断移动
-     * - 思路：要得到 rotate 之后的链表，关键在于要确定从哪里截断并链到头节点上去。例如 0->1->2->3->4，k=7，rotate 之后
-     *   得到 3->4->0->1->2，即从原链表2节点之后截断，并把截断的部分放到链表头部。
+     * - 思路：要得到 rotate 之后的链表，关键在于要确定从哪里截断并链到头节点上去。例如 1->2->3->4->5，k=7，rotate 之后
+     *   得到 4->5->1->2->3，即从原链表2节点之后截断，并把截断的部分放到链表头部。
      * - 实现：要确定从哪里截断则需要知道去掉套圈后的剩余距离（上例中 k % len = 2），即说明原链表中最后2个节点要被截断并放到
      *   链表头部，至此问题转化成了类似 L19 中的问题。
      * - 时间复杂度 O(n)，空间复杂度 O(1)。
      * */
-    public static ListNode rotateRight2(ListNode head, int k) {
-        if (head == null || head.next == null || k == 0) return head;  // 若只有1个节点则直接返回
+    public static ListNode rotateRight(ListNode head, int k) {
+        if (head == null || k == 0) return head;  // 若只有1个节点则直接返回
 
         int len = getLength(head);
         int numToRotate = k % len;          // 去掉套圈后需要移动的节点个数
@@ -54,17 +54,17 @@ public class L61_RotateList {
      * 解法2：截断移动（双指针版本）
      * - 思路：与解法1一致。
      * - 实现：“找到待截断链表的上一节点”的另一个思路是使用双指针技巧，例如已知待截取的节点个数为2：
-     *       0 -> 1 -> 2 -> 3 -> 4
+     *       1 -> 2 -> 3 -> 4 -> 5
      *       l         r              - 设置两个指针 l,r，让 r 向右移动2步
-     *       0 -> 1 -> 2 -> 3 -> 4
+     *       1 -> 2 -> 3 -> 4 -> 5
      *                 l         r    - 让 l,r 保持同时移动，当 r 到达尾节点时 l 一定停在截断点的上一节点。
-     *       0 -> 1 -> 2    3 -> 4
+     *       1 -> 2    3 -> 4 -> 5
      *       ↑___________________|    - 让 r.next = head；h = l.next；l.next = null 从而得到新链表 h。
      *                 l    h    r
      * - 时间复杂度 O(n)，空间复杂度 O(1)。
      * */
-    public static ListNode rotateRight(ListNode head, int k) {
-        if (head == null) return null;
+    public static ListNode rotateRight2(ListNode head, int k) {
+        if (head == null || k == 0) return null;
         k %= getLength(head);
 
         ListNode l = head, r = head;
@@ -82,20 +82,61 @@ public class L61_RotateList {
         return head;
     }
 
+    /*
+     * 解法3：递归
+     * - 思路：与解法1、2一致。
+     * - 实现：∵ 使用递归 ∴ 要换一种思考方式，考虑每次递归应返回什么。例如已知待截取的节点个数为2：
+     *     1 -> 2 -> 3 -> 4 -> 5 -> null
+     *       →
+     *            →
+     *                 →
+     *                      →
+     *                           →
+     *                           ← 1->2->3->4->5  - 递归到底，返回原链表
+     *                      ← 5->1->2->3->4       - numToRotate=1，断开4->5，把5移动到链表头部
+     *                 ← 4->5->1->2->3            - numToRotate=0，断开3->4，把4移动到链表头部
+     *            ← 4->5->1->2->3                 - 直接返回
+     *       ← 4->5->1->2->3                      - 直接返回
+     *   可见：
+     *   1. 只要 numToRotate >= 0 就持续将尾节点移到链表头部；
+     *   2. 在移动时，需要获取：
+     *      - 待移动节点的上一个节点：可通过递归返回到上层时获得；
+     *      - 当前链表的头结点：可通过递归返回值获得。
+     * */
+
+    private static int numToRotate;
+
+    public static ListNode rotateRight3(ListNode head, int k) {
+        if (head == null || k == 0) return head;
+        numToRotate = k % getLength(head);
+        return helper(head, head, null);
+    }
+
+    private static ListNode helper(ListNode head, ListNode curr, ListNode prev) {
+        if (curr == null) return head;           // 递归到底时返回原链表
+
+        ListNode currHead = helper(head, curr.next, curr);
+        if (--numToRotate < 0) return currHead;  // 只要 numToRotate >= 0 就将尾节点移到链表头部
+
+        prev.next = null;                        // 断开最后一个节点与前一个节点的链接
+        curr.next = currHead;                    // 移动到链表头部
+        return curr;                             // 返回新链表
+    }
+
     public static void main(String[] args) {
         ListNode l1 = createLinkedList(new int[]{1, 2, 3, 4, 5});
-        printLinkedList(rotateRight2(l1, 2));   // expects 4->5->1->2->3->NULL
+        printLinkedList(rotateRight3(l1, 2));   // expects 4->5->1->2->3->NULL
 
         ListNode l2 = createLinkedList(new int[]{1, 2, 3, 4, 5});
-        printLinkedList(rotateRight2(l2, 7));   // expects 4->5->1->2->3->NULL
+        printLinkedList(rotateRight3(l2, 7));   // expects 4->5->1->2->3->NULL
 
         ListNode l3 = createLinkedList(new int[]{1, 2, 3, 4, 5});
-        printLinkedList(rotateRight2(l3, 0));   // expects 1->2->3->4->5->NULL
+        printLinkedList(rotateRight3(l3, 0));   // expects 1->2->3->4->5->NULL
 
         ListNode l4 = createLinkedList(new int[]{1});
-        printLinkedList(rotateRight2(l4, 1));   // expects 1->NULL
+        printLinkedList(rotateRight3(l4, 1));   // expects 1->NULL
 
         ListNode l5 = createLinkedList(new int[]{1});
-        printLinkedList(rotateRight2(l5, 99));  // expects 1->NULL
+        printLinkedList(rotateRight3(l5, 99));  // expects 1->NULL
     }
 }
