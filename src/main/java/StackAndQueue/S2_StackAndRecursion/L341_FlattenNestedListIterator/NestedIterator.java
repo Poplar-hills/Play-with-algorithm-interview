a4dbf3f9dfabfeb87d892c2e8a7c4206a632da18
Max Jiang <maxjiang23326@gmail.com>
2020-03-16 23:54:08 +0800

    refactor: simplify

--- src/main/java/StackAndQueue/S2_StackAndRecursion/L341_FlattenNestedListIterator/NestedIterator.java
+++ src/main/java/StackAndQueue/S2_StackAndRecursion/L341_FlattenNestedListIterator/NestedIterator.java
@@ -6,7 +6,7 @@ import java.util.*;
  * Flatten Nested List Iterator
  *
  * - Given a nested list of integers, implement an iterator to flatten it.
- * - Each element is either an integer, or a list -- whose elements may also be integers or other lists.
+ * - Each element is either an integer, or a list - whose elements may also be integers or other lists.
  *
  * - Your NestedIterator object will be instantiated and called as such:
  *       NestedIterator i = new NestedIterator(nestedList);
@@ -19,15 +19,14 @@ import java.util.*;
 
 
 /*
- * 解法1：Eager + recursive
- * - 思路：
- *   1. Eager 的 iterator 会在实例化时一次性将整个 nestedList 解析成可用的 integer list。这样每次调 hasNext/next
- *     方法时就可以直接检查/获得 integer list 中的下一个 integer 即可；
- *   2. ∵ iterator 的输出顺序应该与输入数据一致，只是将数据中的层级拍平而已 ∴ iterator 的基本数据结构应该是 queue；
- *   3. ∵ 输入数据中可能有无限层级的嵌套，这是个典型的递归结构 ∴ iterator 解析 nestedList 的逻辑应该是递归直到找到下
- *      一个 integer 为止。
- * - 缺点：对于大数据集存在性能问题 —— we are pre-computing and pre-loading everything into the queue, which
- *   is a big waste of resource.
+ * 解法1：Eager style + Recursion
+ * - 思路：根据题意中的调用方式，解法类中要实现一个有 hasNext、next 方法的 iterator。而 iterator 有两种基本形式：
+ *     1. Eager: 先将所有元素计算都预先计算出来，然后在调用方法时输出；
+ *     2. Lazy: 在每次调用方法时现进行计算并输出。
+ *   本解法中采用 eager iterator 的思路 —— 预先将整个 NestedList 解析成 Integer list。该过程中：
+ *     1. ∵ iterator 中元素的输出顺序应该与输入数据的元素顺序一致 ∴ iterator 的基本数据结构应该是 queue；
+ *     2. 输入数据中可能有无限层级的嵌套，这是个典型的可以递归的场景。
+ * - 时间复杂度 O(n)，空间复杂度 O(n)。
  * */
 class NestedIterator implements Iterator<Integer> {
     private Queue<Integer> queue = new LinkedList<>();
@@ -53,9 +52,10 @@ class NestedIterator implements Iterator<Integer> {
 }
 
 /*
- * 解法2：Eager + iterative
+ * 解法2：Eager style + Iteration
  * - 思路：解法1的迭代版
- * - 总结：将解法1的递归式改写为迭代式的关键在于用 stack + while 循环模拟系统调用栈（很好的练习）。
+ * - 总结：将解法1的递归式改写为迭代式的关键在于用 Stack + while 循环模拟系统调用栈（很好的练习）。
+ * - 时间复杂度 O(n)，空间复杂度 O(n)。
  * */
 class NestedIterator2 implements Iterator<Integer> {
     private Queue<Integer> queue = new LinkedList<>();
@@ -86,17 +86,21 @@ class NestedIterator2 implements Iterator<Integer> {
 }
 
 /*
- * 解法3：Lazy approach
- * - 思路：Lazy 的 iterator 弱化实例化时的计算逻辑，只做数据加载，而主要逻辑放在 hasNext/next 方法中。
+ * 解法3：Lazy style
+ * - 思路：解法1、2中的 eager style 的最大缺点就是，对于大数据集存在性能问题 —— we are pre-computing and pre-loading
+ *   everything into memory, which can be a big waste of resource。要解决这个问题就要使用 lazy style：Lazy 与 eager
+ *   的区别在于实例化时（构造器中）做哪些事情：
+ *   - Eager iterator 在实例化时要完成所有计算和加载工作；
+ *   - Lazy iterator 则弱化实例化时的计算逻辑，只加载数据，而主要计算逻辑放在 hasNext、next 方法中完成。
  * */
 class NestedIterator3 implements Iterator<Integer> {
-    private Stack<NestedInteger> stack = new Stack<>();
+    private Stack<NestedInteger> stack = new Stack<>();  // 加载的数据放在 Stack 中
 
     public NestedIterator3(List<NestedInteger> nestedList) {
-        pushInReverseOrder(nestedList, stack);
+        pushInReverseOrder(nestedList);
     }
 
-    private void pushInReverseOrder(List<NestedInteger> list, Stack<NestedInteger> stack) {
+    private void pushInReverseOrder(List<NestedInteger> list) {
         for (int i = list.size() - 1; i >= 0; i--)
             stack.push(list.get(i));
     }
@@ -105,7 +109,7 @@ class NestedIterator3 implements Iterator<Integer> {
     public boolean hasNext() {
         while (!stack.isEmpty()) {
             if (stack.peek().isInteger()) return true;
-            pushInReverseOrder(stack.pop().getList(), stack);  // 若栈顶元素不是 integer 的情况
+            pushInReverseOrder(stack.pop().getList());  // 若栈顶元素不是 integer 则持续寻找
         }
         return false;
     }
