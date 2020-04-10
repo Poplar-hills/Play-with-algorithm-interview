fee4ecf19cf948bc4b5b3a56f7309ee90a147fc8
Max Jiang <maxjiang23326@gmail.com>
2020-04-10 08:25:00 +0800

    refactor: rename

--- src/main/java/BinaryTreeAndRecursion/S1_Basics/L222_CountCompleteTreeNodes.java
+++ src/main/java/BinaryTreeAndRecursion/S1_Basics/L222_CountCompleteTreeNodes.java
@@ -83,12 +83,12 @@ public class L222_CountCompleteTreeNodes {
      * - 思路：不同于解法1、2、3，本解法利用了2个性质：
      *     1. 完美二叉树的节点总数：2^h-1，其中 h 为树高；
      *     2. 完全二叉树左、右子树中至少有一个是完美二叉树（见 test cases）；
-     *   根据这两个性质，可在解法1的基础上进行优化：在递归左右子树之前先计算树的左右深度，若左右深度相同则说明是完美二叉树，可
-     *   用性质1直接计算节点数，若树的左右深度不同，则再继续递归左右子树。
-     * - 👉 Bitwise operator 语法解释：
-     *     1. 1<<4 表示将1往左移4位，得到二进制的 0b10000，即十进制的 1 * 2^4；
-     *     2. 因此要表达 2^h - 1 就相当于 1*2^h - 1，即相当于 (1 << h) - 1，注意 << 优先级低于减号；
-     *     3. 另一种表达方式是 (int) Math.pow(2, h) - 1，注意 pow 返回 double 类型，需要强转。
+     *   根据这两个性质，可在解法1的基础上进行优化：在递归左右子树之前先计算树的左右深度，若左右深度相同则说明是完美二叉树，可直接
+     *   用性质1计算出节点数，若左右深度不同，则再继续递归左右子树。
+     * - 👉 实现：Bitwise operator 语法解释：
+     *     1. 1<<4 表示将1在二进制中左移4位，得到 0b10000，即十进制中的 1 * 2^4 ∴ 将 1<< 几位就是2的几次方；
+     *     2. ∴ 2^h - 1 就相当于 (1<<h) - 1（注意 << 优先级低于减号）；
+     *     3. 另一种表达方式是 (int) Math.pow(2, h) - 1（注意 pow 返回 double 类型，需要强转）。
      * - 时间复杂度 O(logn^2)：∵ 左、右子树中至少有一个是完美二叉树，且完美的一边直接得到结果，复杂度为 O(1) ∴ 整个递归过程
      *   相当于单边递归（与 getLeftDepth 一样）∴ 复杂度为 O(logn)。而又因为每次递归中有 getLeftDepth、getRightDepth
      *   ∴ 整体复杂度为 O(logn^2)。
@@ -96,30 +96,31 @@ public class L222_CountCompleteTreeNodes {
      * */
     public static int countNodes4(TreeNode root) {
         if (root == null) return 0;
-        int leftDepth = leftDepth(root);
-        int rightDepth = rightDepth(root);
-        return leftDepth == rightDepth
-            ? (1 << leftDepth) - 1                                   // 若是完美二叉树，则总节点数为 2^h-1
-            : 1 + countNodes3(root.left) + countNodes3(root.right);  // 若不完美则递归左右子树（其中至少有一个是完美的）
+        int lDepth = getLeftDepth(root);
+        int rDepth = getRightDepth(root);
+        return lDepth == rDepth
+            ? (1 << lDepth) - 1                                      // 若是完美二叉树，则总节点数为 2^h-1
+            : countNodes4(root.left) + countNodes4(root.right) + 1;  // 若不完美则递归左右子树（其中至少有一个是完美的）
     }
 
-    private static int leftDepth(TreeNode root) {
-        return (root == null) ? 0 : 1 + leftDepth(root.left);
+    private static int getLeftDepth(TreeNode root) {
+        return (root == null) ? 0 : getLeftDepth(root.left) + 1;
     }
 
-    private static int rightDepth(TreeNode root) {
-        return (root == null) ? 0 : 1 + rightDepth(root.right);
+    private static int getRightDepth(TreeNode root) {
+        return (root == null) ? 0 : getRightDepth(root.right) + 1;
     }
 
     /*
      * 解法5：Recursion
-     * - 思路：也是利用上述的完全二叉树的两个性质进行优化，比解法3更精简，但逻辑也更绕一些。
+     * - 思路：与解法4一致。
+     * - 实现：也是利用上述的完全二叉树的两个性质进行优化，比解法3更精简，但逻辑也更绕一些。
      * - 时间复杂度 O(logn^2)，空间复杂度 O(logn)。
      * */
     public static int countNodes5(TreeNode root) {
-        int h = leftDepth(root);               // 先计算整棵树的左侧高度 h
+        int h = getLeftDepth(root);               // 先计算整棵树的左侧高度 h
         if (h == 0)	return 0;
-        return leftDepth(root.right) == h - 1  // 若右子树的左侧高度 = h-1，即左右子树的左侧高度相等，此时左子树一定完美，右子树不一定（test case 1,2）
+        return getLeftDepth(root.right) == h - 1  // 若右子树的左侧高度 = h-1，即左右子树的左侧高度相等，此时左子树一定完美，右子树不一定（test case 1,2）
             ? (1 << h - 1) + countNodes4(root.right)  // 用公式得到左子树的节点数，再继续递归计算右子树
     		: (1 << h - 2) + countNodes4(root.left);  // 若左右子树的左侧高度不相等，说明树的最后一个节点在左子树上，此时
     }                                                 // 右子树一定完美，左子树不一定（test case 3,4）
