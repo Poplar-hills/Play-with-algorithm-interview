cc7393fd42186a13a7ab6a671b011a1bb7507cb4
Max Jiang <maxjiang23326@gmail.com>
2020-04-10 08:59:14 +0800

    feat: add a solution for L226

--- src/main/java/BinaryTreeAndRecursion/S1_Basics/L226_InvertBinaryTree.java
+++ src/main/java/BinaryTreeAndRecursion/S1_Basics/L226_InvertBinaryTree.java
@@ -12,19 +12,35 @@ import java.util.Stack;
 
 public class L226_InvertBinaryTree {
     /*
-     * 解法1：Recursion
+     * 解法1：DFS
      * - 思路：观察 test cases 可知，要反转一棵二叉树实际上就要为树上的每个节点交换左右子树，例如：
      *               4                    4                    4
      *             /   \                /   \                /   \
      *            2     7    ----->    2     7    ----->    7     2
      *           / \   / \            / \   / \            / \   / \
      *          1   3 6   9          3   1 9   6          9   6 3   1
-     *   上面过程中：1.先分别交换2的左右子树1、3，和7的左右子树6、9；
-     *             2.再交换4的左右子树2、7，得到最终结果。
-     *   ∴ 可见 Invert Binary Tree 是一个自然的递归操作，可用递归方式求解。
+     * - 实现：根据以上思路，可以采用 DFS，先交换当前节点的左、右子节点，然后再分别递归交换后的左右子节点。
      * - 时间复杂度 O(n)，空间复杂度 O(h)，其中 n 为节点数，h 为树高。
      * */
     public static TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+
+    /*
+     * 解法2：DFS
+     * - 思路：与解法1一致。
+     * - 实现：与解法1不同之处在于，先分别递归左、右子树，然后在回程路上交换左右子节点。例如解法1中的例子：
+     *   - 4的左子树 = invert 之后的4的右子树，即 4.left = invertTree(7)；
+     *   - 4的右子树 = invert 之后的4的左子树，即 4.right = invertTree(2)。
+     * - 时间复杂度 O(n)，空间复杂度 O(h)，其中
+     * */
+    public static TreeNode invertTree2(TreeNode root) {
         if (root == null) return null;
         TreeNode temp = root.left;
         root.left = invertTree(root.right);
@@ -33,12 +49,12 @@ public class L226_InvertBinaryTree {
     }
 
     /*
-     * 解法2：Iteration (BFS)
+     * 解法3：Iteration (BFS)
      * - 思路：在标准的层序遍历上，将访问节点的逻辑用交换左右子树代替即可。
      * - 时间复杂度 O(n)，其中 n 为节点数。
      * - 空间复杂度 O(n)，∵ q 中同时最多容纳 n/2 个节点（即完美二叉树的最后一行）∴ 是 O(n) 级别。
      * */
-    public static TreeNode invertTree2(TreeNode root) {
+    public static TreeNode invertTree3(TreeNode root) {
         if (root == null) return null;
         Queue<TreeNode> q = new LinkedList<>();
         q.offer(root);
@@ -58,12 +74,12 @@ public class L226_InvertBinaryTree {
     }
 
     /*
-     * 解法3：Iteration (DFS)
-     * - 思路：逻辑与解法2几乎完全一致，只是数据结构改为了 Stack。
+     * 解法4：Iteration (DFS)
+     * - 思路：逻辑与解法3几乎完全一致，只是数据结构改为了 Stack。
      * - 💎 总结：可见使用 Queue 或 Stack 决定了节点的访问顺序，即 BFS 或 DFS。
      * - 时间复杂度 O(n)，空间复杂度 O(n)。
      * */
-    public static TreeNode invertTree3(TreeNode root) {
+    public static TreeNode invertTree4(TreeNode root) {
         if (root == null) return null;
         Stack<TreeNode> stack = new Stack<>();
         stack.push(root);
@@ -78,6 +94,10 @@ public class L226_InvertBinaryTree {
         }
 
         return root;
+    }
+
+    public static TreeNode invertTree0(TreeNode root) {
+        return null;
     }
 
     public static void main(String[] args) {
