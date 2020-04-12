86c48538b1e0481d6fe16e6f46c224b50c7df0b4
Max Jiang <maxjiang23326@gmail.com>
2020-04-12 12:02:11 +0800

    docs: update comments

--- src/main/java/BinaryTreeAndRecursion/S3_DefineRecursiveProblem/L129_SumRootToLeafNumbers.java
+++ src/main/java/BinaryTreeAndRecursion/S3_DefineRecursiveProblem/L129_SumRootToLeafNumbers.java
@@ -19,30 +19,48 @@ public class L129_SumRootToLeafNumbers {
     /*
      * 解法1：DFS (Recursion)
      * - 思路：从根节点开始从上到下逐层累积当前 path 的 pathNum，当到达叶子节点时加到外部的 sum 上去。
+     *            4                    f(4,0)
+     *           / \                  ↙      ↘
+     *          9   0    --->     f(9,40)  f(0,40)    - sum += 40
+     *         / \                ↙    ↘
+     *        5   1         f(5,490) f(1,490)         - sum += 495; sum += 491;
+     *
      * - 时间复杂度 O(n)，空间复杂度 O(h)，其中 h 为树高（平衡树时 h=logn；退化为链表时 h=n）。
      * */
     private static int sum;
 
     public static int sumNumbers(TreeNode root) {
         sum = 0;
-        helper(root, root.val);
+        helper(root, 0);
         return sum;
     }
 
     private static void helper(TreeNode root, int pathNum) {
         if (root == null) return;
+        pathNum = pathNum * 10 + root.val;
+
         if (root.left == null && root.right == null) {
             sum += pathNum;
             return;
         }
-        helper(root.left, pathNum * 10 + root.left.val);
-        helper(root.right, pathNum * 10 + root.right.val);
+        helper(root.left, pathNum);
+        helper(root.right, pathNum);
     }
 
     /*
-     * 解法2：Recursion (DFS)
-     * - 思路：从根节点开始逐层累积当前 path 的 pathNum，直到叶子节点开始逐层返回，具体返回的是左、右子树的所有 pathNum 之和。
-     *   根据该思路，需将每层累积的 pathNum 当做参数传递给下层递归函数。
+     * 解法2：DFS (Recursion) (解法1的简化版)
+     * - 思路：与解法1一致，也是从上到下在每个节点上累积当前 path 的 pathNum。
+     * - 实现：与解法1不同，该解法不使用外部变量记录，而是让每个递归函数 f(n, num) 都返回以当前节点 n 为根的二叉树的
+     *   sum of root to leaf numbers，即每个递归函数是一个完整的子问题，从而最终从下到上递推出原问题的解：
+     *            4                   f(4,0)                     1026
+     *           / \                 ↙      ↘                   ↗    ↖
+     *          9   0    --->    f(9,40)  f(0,40)    --->     986     40
+     *         / \               ↙    ↘                      ↗   ↖
+     *        5   1         f(5,49)  f(1,49)               495   491
+     *
+     * - 💎 总结：相比解法1，该解法更加函数式：
+     *   1. ∵ 没有外部遍历 ∴ 无副作用；
+     *   2. ∵ 递归函数的语义中描述了返回值 ∴ 每个递归函数都是原问题的一个完整的子问题，是“真递归”。
      * - 时间复杂度 O(n)，空间复杂度 O(h)，其中 h 为树高（平衡树时 h=logn；退化为链表时 h=n）。
      * */
     public static int sumNumbers2(TreeNode root) {
