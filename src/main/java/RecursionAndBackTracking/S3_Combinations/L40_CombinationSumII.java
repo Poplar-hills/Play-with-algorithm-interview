8cb7c256bd6c009017034ed7a01c815cfe410807
Max Jiang <maxjiang23326@gmail.com>
2020-04-27 12:34:49 +0800

    docs: update comments

--- src/main/java/RecursionAndBackTracking/S3_Combinations/L40_CombinationSumII.java
+++ src/main/java/RecursionAndBackTracking/S3_Combinations/L40_CombinationSumII.java
@@ -21,7 +21,8 @@ import java.util.List;
 public class L40_CombinationSumII {
     /*
      * 解法1：Recursion + Backtracking
-     * - 思路：L39_CombinationSum 中的限制是不能产生重复解，而该题中除了该限制之外还多了不能重复使用 candidate 的限制。因此：
+     * - 思路：将组合问题转化为树形问题，采用回溯搜索求解。
+     * - 实现：L39_CombinationSum 中的限制是不能产生重复解，而该题中除了该限制之外还多了不能重复使用 candidate 的限制。因此：
      *   - 为了不重复使用 candidate，需在向下递归的过程中，让每层在遍历 candidates 时的起始位置+1；
      *   - 为了不产生重复解，还需对 candidates 排序，并在每次遍历时跳过相同的元素。
      *   例如，对于 cancdidates=[2,5,2,1,2], target=5 来说，排序后的 cancdidates=[1,2,2,2,5]，于是有：
@@ -38,11 +39,11 @@ public class L40_CombinationSumII {
         List<List<Integer>> res = new ArrayList<>();
         if (candidates == null || candidates.length == 0) return res;
         Arrays.sort(candidates);
-        helper(candidates, target, 0, new ArrayList<>(), res);
+        backtrack(candidates, target, 0, new ArrayList<>(), res);
         return res;
     }
 
-    private static void helper(int[] candidates, int target, int i, List<Integer> list, List<List<Integer>> res) {
+    private static void backtrack(int[] candidates, int target, int i, List<Integer> list, List<List<Integer>> res) {
         if (target == 0) {
             res.add(new ArrayList<>(list));
             return;
@@ -51,7 +52,7 @@ public class L40_CombinationSumII {
             int c = candidates[j];
             if (j > i && c == candidates[j - 1]) continue;     // 跳过可能产生重复解的元素
             list.add(c);
-            helper(candidates, target - c, j + 1, list, res);  // 下一层递归从第 j+1 个元素开始遍历，以保证不使用已用过的元素
+            backtrack(candidates, target - c, j + 1, list, res);  // 下一层递归从第 j+1 个元素开始遍历，以保证不使用已用过的元素
             list.remove(list.size() - 1);
         }
     }
