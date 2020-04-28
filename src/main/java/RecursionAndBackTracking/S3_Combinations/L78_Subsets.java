2eaef9f38ff6d1578cf74dfb3e1ff66787a64d2e
Max Jiang <maxjiang23326@gmail.com>
2020-04-28 09:01:31 +0800

    refactor: simplify code

--- src/main/java/RecursionAndBackTracking/S3_Combinations/L78_Subsets.java
+++ src/main/java/RecursionAndBackTracking/S3_Combinations/L78_Subsets.java
@@ -8,7 +8,7 @@ import java.util.List;
 /*
  * Subsets
  *
- * - Given a set of distinct integers, return all possible subsets (the power set).
+ * - Given a set of distinct integers, return all possible subsets (the power set 幂集).
  *
  * - Note: The solution set must not contain duplicate subsets (e.g. [1,2] and [2,1] are duplicate subsets).
  * */
@@ -16,7 +16,7 @@ import java.util.List;
 public class L78_Subsets {
     /*
      * 解法1：Recursion + Backtracking
-     * - 思路：分析题目可知，题目的另一种描述是使用 nums 中的元素填充空列表，一共有多少种不重复的填法。举例分析，对于 [1,2,3]：
+     * - 思路：求幂集就是求用 nums 中的元素填充空列表，一共有多少种不重复的填法。例如对于 [1,2,3]：
      *                          []
      *                 1/       2|       3\
      *                [1]       [2]      [3]
@@ -24,27 +24,23 @@ public class L78_Subsets {
      *            [1,2] [1,3]  [2,3]
      *             3|
      *           [1,2,3]
-     *   由此可见：
-     *     - 在填充过程中得到的所有中间结果和最终结果都是有效解；
-     *     - 递归填充过程的结束时机是到达3，即 nums 的最后一个元素。
-     *
+     *   可见，在填充过程中得到的所有中间结果和最终结果都是有效解。
+     * - 实现：∵ 要收集所有中间、最终结果 ∴ 该实现中可以没有显示的递归退出条件，而是当遍历 j == nums.length 时会自动结束。
      * - 时间复杂度 O(2^n)：C(n,1) + C(n,2) + ... + C(n,n) = 2^n - C(n,0) = O(2^n)；
      * - 空间复杂度 O(n)。
      * */
     public static List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         if (nums == null || nums.length == 0) return res;
-        res.add(new ArrayList<>());          // 空列表解
         helper(nums, 0, new ArrayList<>(), res);
         return res;
     }
 
     private static void helper(int[] nums, int i, List<Integer> list, List<List<Integer>> res) {
-        if (i == nums.length) return;        // 到达 nums 尾部时递归结束
+        res.add(new ArrayList<>(list));          // 收集所有中间、最终结果
         for (int j = i; j < nums.length; j++) {
             list.add(nums[j]);
-            res.add(new ArrayList<>(list));  // 收集所有填充结果
-            helper(nums, j + 1, list, res);
+            backtrack0(nums, j + 1, list, res);
             list.remove(list.size() - 1);
         }
     }
@@ -83,7 +79,7 @@ public class L78_Subsets {
     }
 
     public static void main(String[] args) {
-        log(subsets2(new int[]{1, 2, 3}));  // expects [[3], [1], [2], [1,2,3], [1,3], [2,3], [1,2], []]
-        log(subsets2(new int[]{3, 2, 1}));  // expects [[3], [1], [2], [1,2,3], [1,3], [2,3], [1,2], []]
+        log(subsets(new int[]{1, 2, 3}));  // expects [[], [1], [1,2], [1,2,3], [1,3], [2], [2,3], [3]]
+        log(subsets(new int[]{3, 2, 1}));  // expects [[], [3], [3,2], [3,2,1], [3,1], [2], [2,1], [1]]
     }
 }
