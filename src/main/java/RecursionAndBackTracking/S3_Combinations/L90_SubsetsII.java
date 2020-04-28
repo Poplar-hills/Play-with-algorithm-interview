b3face8648303c912c2c4d771ccb1a7c499c3857
Max Jiang <maxjiang23326@gmail.com>
2020-04-28 17:57:03 +0800

    docs: update comments

--- src/main/java/RecursionAndBackTracking/S3_Combinations/L90_SubsetsII.java
+++ src/main/java/RecursionAndBackTracking/S3_Combinations/L90_SubsetsII.java
@@ -51,14 +51,13 @@ public class L90_SubsetsII {
 
     /*
      * 解法2：Recursion + Backtracking
-     * - 思路：类似 L78_Subsets 解法2的思路，但加入了对重复元素的检测。
-     *   例如对于 [2,1,2] 来说，首先排序后为 [1,2,2]。最初有空集解 []，之后：
-     *     - 若选用1，则将1追加到 [] 中，得到解 [1]；若不选用1，则还是 []；
-     *       将两个解合并得到 [[],[1]]。
-     *     - 若选用2，则将2追加到 [[],[1]] 中的每个里，得到解 [[2],[1,2]]；若不选用2，则还是 [[],[1]]；
-     *       将两部分合并得到 [[],[1],[2],[1,2]]。
-     *     - 若选用2 ∵ 与上一个元素重复 ∴ 只将2追加到 [[],[1],[2],[1,2]] 中的后2个解里，得到解 [[2,2],[1,2,2]]，
-     *       将两部分合并得到 [[],[1],[2],[1,2],[2,2],[1,2,2]]。
+     * - 思路：类似 L78_Subsets 解法2，但加入了对重复元素的检测。例如对于 [2,1,2] 来说，排序后为 [1,2,2]。放入空集解 [] 之后：
+     *   1. 若用1，则将1追加到 [] 中，得到解 [1]；若不用1，则还是 []；
+     *      将两个解合并得到 [[],[1]]。
+     *   2. 若用2，则将2追加到 [[],[1]] 中的每个里，得到解 [[2],[1,2]]；若不用2，则还是 [[],[1]]；
+     *      将两部分合并得到 [[],[1],[2],[1,2]]。
+     *   3. 若用2 ∵ 与上一个元素重复 ∴ 只将2追加到第2步中新得到的解 [2]、[1,2] 里，得到解 [[2,2],[1,2,2]]，
+     *      将两部分合并得到 [[],[1],[2],[1,2],[2,2],[1,2,2]]。
      * - 时间复杂度 O(2^n)，空间复杂度 O(1)。
      * */
     public static List<List<Integer>> subsetsWithDup2(int[] nums) {
@@ -69,8 +68,8 @@ public class L90_SubsetsII {
 
         int size = 0, start;
         for (int i = 0; i < nums.length; i++) {
-            start = (i >= 1 && nums[i] == nums[i - 1]) ? size : 0;  // 若出现重复元素，则跳过上次 res 中的所有 subset；若不是重复元素，则正常从0开始
-            size = res.size();
+            start = (i > 0 && nums[i] == nums[i - 1]) ? size : 0;  // 若元素重复，则使用上轮遍历之前（上上轮遍历之后）记录的 res.size
+            size = res.size();                                     // 作为遍历起点；若不是重复元素，则正常从0开始
             for (int j = start; j < size; j++) {  // 遍历 res 中从索引 start 处开始的 subset
                 List<Integer> list = new ArrayList<>(res.get(j));
                 list.add(nums[i]);
@@ -82,8 +81,8 @@ public class L90_SubsetsII {
     }
 
     public static void main(String[] args) {
-        log(subsetsWithDup(new int[]{1, 2, 2}));  // expects [[2], [1], [1,2,2], [2,2], [1,2], []]
-        log(subsetsWithDup(new int[]{2, 1, 2}));  // expects [[2], [1], [1,2,2], [2,2], [1,2], []]
-        log(subsetsWithDup(new int[]{1, 2, 3}));  // expects [[3], [1], [2], [1,2,3], [1,3], [2,3], [1,2], []]
+        log(subsetsWithDup2(new int[]{1, 2, 2}));  // expects [[], [1], [1,2], [1,2,2], [2], [2,2]]
+        log(subsetsWithDup2(new int[]{2, 1, 2}));  // expects [[], [1], [1,2], [1,2,2], [2], [2,2]]
+        log(subsetsWithDup2(new int[]{1, 2, 3}));  // expects [[], [1], [1,2], [1,2,3], [1,3], [2], [2,3], [3]]
     }
 }
