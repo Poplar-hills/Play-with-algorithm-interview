eb007eeb6601af4f9d85981be193a47096f809ff
Max Jiang <maxjiang23326@gmail.com>
2020-05-21 22:46:00 +0800

    refactor: rename

--- src/main/java/RecursionAndBackTracking/S6_TraditionalAi/L37_SudokuSolver.java
+++ src/main/java/RecursionAndBackTracking/S6_TraditionalAi/L37_SudokuSolver.java
@@ -60,14 +60,14 @@ public class L37_SudokuSolver {
     }
 
     /*
-     * 解法2：Recursion + Backtracking (时间优化版)
-     * - 思路：大体思路与解法1一致，仍然采用回溯搜索求解。
-     * - 实现：解法1用 isValid 方法实时判断某一数字 c 是否能填入某一格 [i,j]。而若借鉴 L51_NQueens 解法1中的3个 boolean[]，
-     *   则可以对该检查过程进行优化，通过空间换时间的方式以 O(1) 的复杂度完成检查。但具体实现不同于 L51，该问题中需要对每行、
-     *   每列、每个 block 都维护单独的 boolean[] 以保证他们各自中都没有重复 ∴ 需要创建的是3个 9*9 的二维数组：
-     *     1. row[i][n] 表示第 i 行中是否已有数字 n，其中 i ∈ [0,8], n ∈ [1,9]；
-     *     2. col[i][n] 表示第 i 列中是否已有数字 n，其中 i ∈ [0,8], n ∈ [1,9]；
-     *     3. block[i][n] 表示第 i 个 block 中是否已有数字 n，其中 i ∈ [0,8], n ∈ [1,9]。
+     * 解法2：Backtracking (解法1的时间优化版)
+     * - 思路：与解法1一致。
+     * - 实现：借鉴 L51_NQueens 解法1中的3个 boolean[]，对解法1中的 isValid 方法进行优化，以空间换时间的方式用 O(1) 的
+     *   复杂度完成检查。但不同于 L51，该问题中需要对每行、每列、每个 block 都维护单独的 boolean[] 以保证他们各自中没有重复
+     *   的数字 ∴ 需要创建3个 9*9 的二维数组：
+     *     1. row[r][n] 表示第 r 行中是否已有数字 n，其中 r ∈ [0,8], n ∈ [1,9]；
+     *     2. col[c][n] 表示第 c 列中是否已有数字 n，其中 c ∈ [0,8], n ∈ [1,9]；
+     *     3. block[b][n] 表示第 b 个 block 中是否已有数字 n，其中 b ∈ [0,8], n ∈ [1,9]。
      *   基于该思路可得到程序的整体逻辑：
      *     1. 先遍历 board 上已有的数字，并将 row、col、block 对应的位置置为 true；
      *     2. 再用解法1中的方式，对 board 上的空格进行回溯填充。
@@ -78,40 +78,41 @@ public class L37_SudokuSolver {
 
     public static void solveSudoku2(char[][] board) {
         if (board == null || board.length == 0 || board[0].length == 0) return;
+        init(board);
+        solve2(board, 0);  // 再次遍历 board，对空格进行回溯填充
+    }
+
+    private static void init(char[][] board) {
         row = new boolean[9][10];
         col = new boolean[9][10];
         block = new boolean[9][10];
 
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (board[i][j] != '.') {       // 先遍历 board 上已有的数字
-                    int n = board[i][j] - '0';  // 将 char 转化为 int（'5'-'0' = 5，即在 ASCII 中 '5' 与 5 相差48）
-                    row[i][n] = true;
-                    col[j][n] = true;
-                    block[i / 3 * 3 + j / 3][n] = true;  // i/3 ∈ [0,1,2]；i/3*3 ∈ [0,3,6]；i/3*3+j/3 ∈ [0,1,2,3,4,5,6,7,8]
-                }
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                if (board[r][c] != '.') {       // 先遍历 board 上已有的数字
+                    int n = board[r][c] - '0';  // 将 char 转化为 int（'5'-'0' = 5，即在 ASCII 中 '5' 与 5 相差48）
+                    row[r][n] = col[c][n] = block[r / 3 * 3 + c / 3][n] = true;
+                }                               // r/3 ∈ [0,1,2]；r/3*3 ∈ [0,3,6]；r/3*3+c/3 ∈ [0,1,2,3,4,5,6,7,8]
             }
         }
-
-        dfs(board, 0);  // 再次遍历 board，对空格进行回溯填充
     }
 
-    private static boolean dfs(char[][] board, int cellIdx) {
+    private static boolean solve2(char[][] board, int cellIdx) {
         if (cellIdx == 81) return true;
-        int i = cellIdx / 9;
-        int j = cellIdx % 9;
-        int k = i / 3 * 3 + j / 3;
+        int r = cellIdx / 9;
+        int c = cellIdx % 9;
+        int b = r / 3 * 3 + c / 3;
 
-        if (board[i][j] != '.')
-            return dfs(board, cellIdx + 1);
+        if (board[r][c] != '.')
+            return solve2(board, cellIdx + 1);
 
         for (int n = 1; n <= 9; n++) {
-            if (!row[i][n] && !col[j][n] && !block[k][n]) {
-                board[i][j] = (char) (n + '0');  // 将 int 转化为 char（不能直接强转，需要加 '0'，即 ASCII 中 5 与 '5' 相差 48）
-                row[i][n] = col[j][n] = block[k][n] = true;
-                if (dfs(board, cellIdx + 1)) return true;
-                board[i][j] = '.';
-                row[i][n] = col[j][n] = block[k][n] = false;
+            if (!row[r][n] && !col[c][n] && !block[b][n]) {
+                board[r][c] = (char) (n + '0');  // 将 int 转化为 char（不能直接强转，需要加'0'，即 ASCII 中5与'5'相差48）
+                row[r][n] = col[c][n] = block[b][n] = true;
+                if (solve2(board, cellIdx + 1)) return true;
+                row[r][n] = col[c][n] = block[b][n] = false;
+                board[r][c] = '.';
             }
         }
         return false;
@@ -131,7 +132,7 @@ public class L37_SudokuSolver {
             {'.', '.', '.',   '4', '1', '9',   '.', '.', '5'},
             {'.', '.', '.',   '.', '8', '.',   '.', '7', '9'}
         };
-        solveSudoku(board);
+        solveSudoku2(board);
         log(board);
         /*
          * expects {
