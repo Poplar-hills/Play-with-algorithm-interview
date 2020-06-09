58e16863b195b9ab5f02118145f4fa2ad1e667c1
Max Jiang <maxjiang23326@gmail.com>
2020-06-09 21:46:31 +0800

    refactor: simplify

--- src/main/java/DP/S1_Basics/L64_MinimumPathSum.java
+++ src/main/java/DP/S1_Basics/L64_MinimumPathSum.java
@@ -222,22 +222,22 @@ public class L64_MinimumPathSum {
 
     public static void main(String[] args) {
         log(minPathSum4(new int[][]{
-            new int[]{1, 3, 1},
-            new int[]{1, 5, 1},
-            new int[]{4, 2, 1}
+            {1, 3, 1},
+            {1, 5, 1},
+            {4, 2, 1}
         }));  // expects 7. (1->3->1->1->1)
 
         log(minPathSum4(new int[][]{
-            new int[]{1, 3, 4},
-            new int[]{1, 2, 1},
+            {1, 3, 4},
+            {1, 2, 1},
         }));  // expects 5. (1->1->2->1)
 
         log(minPathSum4(new int[][]{
-            new int[]{1, 2, 3}
+            {1, 2, 3}
         }));  // expects 6.
 
         log(minPathSum4(new int[][]{
-            new int[]{0}
+            {0}
         }));  // expects 0.
     }
 }
