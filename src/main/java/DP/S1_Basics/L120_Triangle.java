8512c9301e412282fcafdf5a0a0154fde50c41cf
Max Jiang <maxjiang23326@gmail.com>
2020-06-18 14:23:08 +0800

    chore: upgrade to JDK11

--- src/main/java/DP/S1_Basics/L120_Triangle.java
+++ src/main/java/DP/S1_Basics/L120_Triangle.java
@@ -8,8 +8,10 @@ import static Utils.Helpers.log;
  * Triangle
  *
  * - Given a triangle, find the minimum path sum from top to bottom.
+ *
  * - Note: Each step you may move to adjacent numbers on the row below (比如下面的 test case 2 中，第2行的2只能移动
  *   到第3行中的1或-1上，而不能移动到-3上，因此不是从每行中找到最小值就行).
+ *
  * - Bonus point if using only O(n) extra space, where n is the number of rows in the triangle.
  * */
 
@@ -196,20 +198,20 @@ public class L120_Triangle {
         return dp[0];
     }
     public static void main(String[] args) {
-        log(minimumTotal3(Arrays.asList(
-            Arrays.asList(2),
-            Arrays.asList(3, 4),
-            Arrays.asList(6, 5, 7),
+        log(minimumTotal3(List.of(
+               Arrays.asList(2),
+              Arrays.asList(3, 4),
+             Arrays.asList(6, 5, 7),
             Arrays.asList(4, 1, 8, 3)
         )));  // expects 11 (2 + 3 + 5 + 1)
 
-        log(minimumTotal3(Arrays.asList(
-            Arrays.asList(-1),
-            Arrays.asList(2, 3),
+        log(minimumTotal3(List.of(
+               Arrays.asList(-1),
+              Arrays.asList(2, 3),
             Arrays.asList(1, -1, -3)
-        )));  // expects -1 (-1 + 3 + -3) 注意不是从每行中找到最小值就行，如：(-1 + 2 + -3)
+        )));  // expects -1 (-1 + 3 + -3) 注意不是从每行中找到最小值就行
 
-        log(minimumTotal3(Arrays.asList(
+        log(minimumTotal3(List.of(
             Arrays.asList(-10)
         )));  // expects -10
     }
