8c8aa6551926876f3cebea42162329ce0ea8bbab
Max Jiang <maxjiang23326@gmail.com>
2020-06-19 12:52:11 +0800

    refactor: simplify & rename

--- src/main/java/DP/S2_OverlappingSubproblems/L62_UniquePaths.java
+++ src/main/java/DP/S2_OverlappingSubproblems/L62_UniquePaths.java
@@ -8,45 +8,45 @@ import java.util.Queue;
 /*
  * Unique Paths
  *
- * - A robot is located at the top-left corner of a m x n grid (m 行 n 列). The robot can only move either down
- *   or right at any point in time. The robot is trying to reach the bottom-right corner of the grid. How many
- *   possible unique paths are there?
+ * - A robot is located at the top-left corner of a m x n grid (m 行 n 列). The robot can only move either
+ *   down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid.
+ *   How many possible unique paths are there?
+ *
  * - Note: m and n will be at most 100.
  *
- * - 与 L64_MinimumPathSum 的区别是 L64 是求最小成本的路径，而该题是求有多少种不同路径。
+ * - 与 L64_MinimumPathSum 的区别是 L64 是求最小成本的路径，而该题是求不同的路径数。
  * */
 
 public class L62_UniquePaths {
     /*
-     * 超时解：BFS 全搜索
+     * 超时解1：BFS
      * - 思路：类似 L64 解法1，图论建模：
      *        ■ → ■ → ■
      *        ↓   ↓   ↓
      *        ■ → ■ → ■
      * - 时间复杂度 O(2^n)，空间复杂度 O(n)，n 为节点个数。
      * */
-    public static int uniquePaths(int m, int n) {
-        int res = 0;
-        if (m == 0 || n == 0) return res;
+    public static int uniquePaths_1(int m, int n) {
+        int numOfPath = 0;
+        if (m == 0 || n == 0) return numOfPath;
 
-        Queue<Pair<Integer, Integer>> q = new LinkedList<>();
-        q.offer(new Pair<>(0, 0));
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{0, 0});
 
         while (!q.isEmpty()) {
-            Pair<Integer, Integer> pair = q.poll();
-            int x = pair.getKey();
-            int y = pair.getValue();
+            int[] pos = q.poll();
+            int r = pos[0], c = pos[1];
 
-            if (x == m - 1 && y == n - 1) {
-                res++;
+            if (r == m - 1 && c == n - 1) {
+                numOfPath++;
                 continue;
             }
 
-            if (x + 1 < m) q.offer(new Pair<>(x + 1, y));
-            if (y + 1 < n) q.offer(new Pair<>(x, y + 1));
+            if (r + 1 < m) q.offer(new int[]{r + 1, c});
+            if (c + 1 < n) q.offer(new int[]{r, c + 1});
         }
 
-        return res;
+        return numOfPath;
     }
 
     /*
@@ -177,8 +177,8 @@ public class L62_UniquePaths {
     }
 
     public static void main(String[] args) {
-        log(uniquePaths6(2, 3));  // expects 3. (R->R->D, R->D->R, D->R->R)
-        log(uniquePaths6(3, 3));  // expects 6.
-        log(uniquePaths6(7, 3));  // expects 28.
+        log(uniquePaths_1(2, 3));  // expects 3. (R->R->D, R->D->R, D->R->R)
+        log(uniquePaths_1(3, 3));  // expects 6.
+        log(uniquePaths_1(7, 3));  // expects 28.
     }
 }
