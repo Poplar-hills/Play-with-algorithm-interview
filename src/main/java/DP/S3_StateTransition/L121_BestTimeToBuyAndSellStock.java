8361c152304512c2eb8bbb209f90fd46a726ee65
Max Jiang <maxjiang23326@gmail.com>
2020-07-02 17:18:02 +0800

    feat: add a solution for L121

--- src/main/java/DP/S3_StateTransition/L121_BestTimeToBuyAndSellStock.java
+++ src/main/java/DP/S3_StateTransition/L121_BestTimeToBuyAndSellStock.java
@@ -8,14 +8,28 @@ import static Utils.Helpers.*;
  * - Say you have an array for which the ith element is the price of a given stock on day i. Design an
  *   algorithm to find the maximum profit.
  *
- * - If you were only permitted to complete at most 1 transaction (i.e., buy one and sell one share of the stock),
+ * - If you were only permitted to complete at most 1 transaction (i.e., buy one and sell one share of the stock)
  *
  * - Note that you cannot sell a stock before you buy one.
  * */
 
 public class L121_BestTimeToBuyAndSellStock {
+    /*
+     * 解法1：
+     * - 思路：
+     * - 时间复杂度 O(n^2)，空间复杂度 O(1)。
+     * */
     public static int maxProfit(int[] prices) {
-        return 0;
+        if (prices == null || prices.length < 2) return 0;
+
+        int maxProfit = 0;
+
+        for (int b = 0; b < prices.length; b++)
+            for (int s = b + 1; s < prices.length; s++)
+                if (prices[s] > prices[b])
+                    maxProfit = Math.max(maxProfit, prices[s] - prices[b]);
+
+        return maxProfit;
     }
 
     public static void main(String[] args) {
