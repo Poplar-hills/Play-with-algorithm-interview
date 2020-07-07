7e5c434bca729e7cdec4f051b350fbf3efc95534
Max Jiang <maxjiang23326@gmail.com>
2020-07-08 00:57:13 +0800

    feat: add a solution for L122

--- src/main/java/DP/S3_StateTransition/L122_BestTimeToBuyAndSellStockII.java
+++ src/main/java/DP/S3_StateTransition/L122_BestTimeToBuyAndSellStockII.java
@@ -2,6 +2,8 @@ package DP.S3_StateTransition;
 
 import static Utils.Helpers.*;
 
+import java.util.Arrays;
+
 /*
  * Best Time to Buy and Sell Stock II
  *
@@ -16,24 +18,43 @@ import static Utils.Helpers.*;
 
 public class L122_BestTimeToBuyAndSellStockII {
     /*
-     * 解法1：Peak Valley
-     * - 思路：将数据可视化出来，例如 prices=[7,1,5,3,6,4]：
+     * 解法1：Peak Valley（Kadane's Algorithm）
+     * - 思路：先将数据可视化出来，例如 prices=[7,1,5,3,6,4]：
      *      |
-     *    7 | *                      p(j)
-     *    6 |  ·         p(i)         *
-     *    5 |   ·         *         ·    ·
-     *    4 |    ·      ·    ·    ·         *
-     *    3 |     ·    ·        * v(j)
-     *    2 |      ·  ·
-     *    1 |       * v(i)
+     *    7 | *                     peak(j)
+     *    6 |  •        peak(i)       *
+     *    5 |   •         *         •    •
+     *    4 |    •      •    •    •         *
+     *    3 |     •    •        * valley(j)
+     *    2 |      •  •
+     *    1 |       * valley(i)
      *    0 + - - - - - - - - - - - - - - - - -
      *        0     1     2     3     4     5
-     *   ∵ 可以交易任意多次 ∴ 最大利润 = ∑(每次上涨的 peak - 涨之前的 valley)，即 maxProfit = sum(peak(i) - valley(i))。
-     *   ∴ 思路就是找出 prices 中所有相邻且处于上涨的元素之差，差值之和即是解。这里的关键是“前后相邻” —— ∵ 可以交易任意多次
-     *   ∴ sum(peak(i) - valley(i)) 一定 > peak(j) - valley(i)。
+     *   ∵ 可以交易任意多次 ∴ 最大利润 = 所有单调上涨的幅度之和，即 maxProfit = sum(peak(i) - valley(i))。
+     *   这里的关键是“前后相邻” —— ∵ 可以交易任意多次 ∴ sum(peak(i) - valley(i)) 一定 > peak(j) - valley(i)。
+     * - 实现：借助 Kadane's Algorithm（即最大子序列问题）—— 先求出所有前后两元素的差值，再过滤掉其中的负差值，剩下的
+     *   正差值之和即是原问题的解。
      * - 时间复杂度 O(n)，空间复杂度 O(1)。
      * */
     public static int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) return 0;
+        int[] diffs = new int[prices.length];
+
+        for (int i = 1; i < prices.length; i++)
+            diffs[i] = prices[i] - prices[i - 1];
+
+        return Arrays.stream(diffs)
+            .filter(n -> n > 0)
+            .reduce(0, Integer::sum);
+	}
+
+    /*
+     * 解法2：Peak Valley（解法1的简化版）
+     * - 思路：与解法1的思路一致。
+     * - 实现：求出所有前后两元素的差值其实不需要计算所有的 diff，只需在遍历时判断后面元素是否 > 前面元素即可。
+     * - 时间复杂度 O(n)，空间复杂度 O(1)。
+     * */
+    public static int maxProfit2(int[] prices) {
         if (prices == null || prices.length == 0) return 0;
         int maxProfit = 0;
 
@@ -42,15 +63,6 @@ public class L122_BestTimeToBuyAndSellStockII {
                 maxProfit += prices[i] - prices[i - 1];
 
         return maxProfit;
-	}
-
-    /*
-     * 解法2：DP Top-down
-     * - 思路：
-     * - 时间复杂度 O(n)，空间复杂度 O(1)。
-     * */
-    public static int maxProfit2(int[] prices) {
-		return 0;
 	}
 
 	/*
