24eb3a828c9fd849516595a2f8b9e3119a3334e0
Max Jiang <maxjiang23326@gmail.com>
2020-07-17 17:35:54 +0800

    doc: update comments

--- src/main/java/DP/S4_KnapsackProblem/L714_BestTimeToBuyAndSellStockWithTransactionFee.java
+++ src/main/java/DP/S4_KnapsackProblem/L714_BestTimeToBuyAndSellStockWithTransactionFee.java
@@ -12,6 +12,8 @@ import static Utils.Helpers.*;
  * - You may complete as many transactions as you like, but you need to pay the transaction fee for each
  *   transaction. You may not buy more than 1 share of a stock at a time (ie. you must sell the stock share
  *   before you buy again.)
+ *
+ * - 💎总结：该题融合了 L123_BestTimeToBuyAndSellStockIII、L122_BestTimeToBuyAndSellStockII 两种经典思路，多琢磨！
  * */
 
 public class L714_BestTimeToBuyAndSellStockWithTransactionFee {
