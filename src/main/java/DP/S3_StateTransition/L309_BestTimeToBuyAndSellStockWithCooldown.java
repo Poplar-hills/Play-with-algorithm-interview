5280a57f23bab36c1592e2a61f3b9977946a9dff
Max Jiang <maxjiang23326@gmail.com>
2020-07-23 19:20:59 +0800

    docs: update comments

--- src/main/java/DP/S3_StateTransition/L309_BestTimeToBuyAndSellStockWithCooldown.java
+++ src/main/java/DP/S3_StateTransition/L309_BestTimeToBuyAndSellStockWithCooldown.java
@@ -14,20 +14,20 @@ import static Utils.Helpers.*;
 public class L309_BestTimeToBuyAndSellStockWithCooldown {
     /*
      * 解法1：DP（多状态递推）
-     * - 思路：∵ 该问题具有明显的状态转移特点，且有4种行为能使状态发生改变 ∴ 可以采用 L198_HouseRobber 解法4的多状态递推的思路，
-     *   即通过梳理不同行为对状态的影响写出不同状态的递推表达式。由题中可知，共有4种行为：buy、sell、hold1、hold0。由此进行梳理：
+     * - 思路：∵ 该问题具有明显的状态转移特点，且有4种行为能使状态发生改变 ∴ 可采用 L198_HouseRobber 解法4的多状态递推的思路，
+     *   即通过梳理不同行为对状态的影响写出不同状态的递推表达式。由题可知，共有4种行为：buy、sell、hold1、hold0，由此进行梳理：
      *                               +-----> sell <------+
      *                               |        |          |  ↗—↘
      *                              buy ------|------> hold1   |
      *                               ↑        ↓  ↗—↘        ↖_↙
      *                               +----- hold0   |
      *                                           ↖_↙
-     *   - 另外该问题符合最优子结构性质 —— 通过求前 i-1 天的最大收益可获得前 i 天的最大收益。
-     *   - 由以上两点可写出状态递推表达式：
-     *       buy[i] = hold0[i-1] - prices[i]                  // 第i天买入后的最大收益 = 前一天持有后的收益 - 第i天的股价
-     *       sell[i] = max(hold1[i-1], buy[i-1]) + prices[i]  // 第i天卖出后的最大收益 = max(前一天持有后的收益, 前一天买入后的收益) + 第i天的股价
-     *       hold1[i] = max(hold1[i-1], buy[i-1])
-     *       hold0[i] = max(hold0[i-1], sell[i-1])
+     *   - 另外该问题符合最优子结构性质 —— 通过求前 d-1 天的最大收益可获得前 d 天的最大收益。
+     *   - 基于以上两点可写出递推表达式：
+     *     buy[d] = hold0[d-1] - prices[d]                  // 第d天买入后的最大收益 = 前一天持有后的最大收益 - 第d天的股价
+     *     sell[d] = max(hold1[d-1], buy[d-1]) + prices[d]  // 第d天卖出后的最大收益 = max(前一天持有后的最大收益, 前一天买入后的最大收益) + 第d天的股价
+     *     hold1[d] = max(hold1[d-1], buy[d-1])
+     *     hold0[d] = max(hold0[d-1], sell[d-1])
      *
      * - 时间复杂度 O(n)，空间复杂度 O(n)。
      * */
@@ -40,24 +40,25 @@ public class L309_BestTimeToBuyAndSellStockWithCooldown {
         int[] hold0 = new int[n];
         int[] hold1 = new int[n];
 
-        buy[0] = -prices[0];    // 第0天买入后的收益
-        sell[0] = 0;            // 第0天卖出后的收益（∵ 不可能第0天就卖出 ∴ 设为0）
-        hold0[0] = 0;           // 第0天什么都不做的收益
-        hold1[0] = -prices[0];  // 第0天持有1股的收益（∵ 不可能第0天就持有股票 ∴ 设为负的第0天的股价）
+        buy[0] = -prices[0];    // 第0天买入后的最大收益
+        sell[0] = 0;            // 第0天卖出后的最大收益（∵ 不可能第0天就卖出 ∴ 为0）
+        hold0[0] = 0;           // 第0天什么都不做的最大收益
+        hold1[0] = -prices[0];  // 第0天持有的最大收益
 
-        for (int i = 1; i < n; i++) {  // 通过前一天的值递推后一天的值
-            buy[i] = hold0[i - 1] - prices[i];
-            sell[i] = Math.max(hold1[i - 1], buy[i - 1]) + prices[i];
-            hold1[i] = Math.max(hold1[i - 1], buy[i - 1]);
-            hold0[i] = Math.max(hold0[i - 1], sell[i - 1]);
+        for (int d = 1; d < n; d++) {
+            buy[d] = hold0[d - 1] - prices[d];
+            sell[d] = Math.max(hold1[d - 1], buy[d - 1]) + prices[d];
+            hold1[d] = Math.max(hold1[d - 1], buy[d - 1]);
+            hold0[d] = Math.max(hold0[d - 1], sell[d - 1]);
         }
 
-        return Math.max(sell[n - 1], hold0[n - 1]);  // 若要获得最大收益，则最后一天只能卖出或处于已经卖出状态
+        return Math.max(sell[n - 1], hold0[n - 1]);  // 最大收益可能来自最后一天卖出或已卖出状态
     }
 
     /*
      * 解法2：DP（解法1的空间优化版）
-     * - 思路：在解法1的基础上 ∵ 每个数组都只用到 i-1 处的值 ∴ 不需要维护整个数组，只需记录单个值即可。
+     * - 思路：与解法1一致。
+     * - 实现：∵ 解法1中的递推表达式都只用到 d-1 处的值 ∴ 不需要维护整个数组，只需记录 d-1 天（即前一天）的值即可。
      * - 时间复杂度 O(n)，空间复杂度 O(1)。
      * */
     public static int maxProfit2(int[] prices) {
@@ -65,8 +66,8 @@ public class L309_BestTimeToBuyAndSellStockWithCooldown {
 
         int lastBuy = -prices[0];
         int lastSell = 0;
-        int lastHold1 = -prices[0];
         int lastHold0 = 0;
+        int lastHold1 = -prices[0];
 
         for (int price : prices) {
             int buy = lastHold0 - price;
@@ -109,7 +110,7 @@ public class L309_BestTimeToBuyAndSellStockWithCooldown {
             dp[d][1][0] = dp[d-1][0][1] + prices[d];
         }
 
-        return Math.max(dp[n-1][0][0], dp[n-1][1][0]);
+        return Math.max(dp[n-1][0][0], dp[n-1][1][0]);  // 取得最大收益时既有可能在 cooldown 也有可能不在
     }
 
     public static void main(String[] args) {
