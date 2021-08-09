8bdc8f7a5e3ab07e423927cb91a3dd4469003569
Max Jiang <maxjiang23326@gmail.com>
2021-08-09 13:47:04 +0800

    refactor: add enum type

--- src/main/java/StackAndQueue/S2_StackAndRecursion/L94_BinaryTreeInorderTraversal.java
+++ src/main/java/StackAndQueue/S2_StackAndRecursion/L94_BinaryTreeInorderTraversal.java
@@ -12,7 +12,8 @@ import static Utils.Helpers.*;
 
 public class L94_BinaryTreeInorderTraversal {
     /*
-     * 解法1：递归
+     * 解法1：DFS
+     * - 实现：最简单常见的 DFS 就是使用递归实现。
      * - 时间复杂度 O(n)，空间复杂度 O(h)，其中 h 是树高。
      * */
     public static List<Integer> inorderTraversal(TreeNode root) {
@@ -21,16 +22,17 @@ public class L94_BinaryTreeInorderTraversal {
         return res;
     }
 
-    private static void inorderTraversal(TreeNode node, List<Integer> list) {
+    private static void inorderTraversal(TreeNode node, List<Integer> res) {
         if (node == null) return;
-        inorderTraversal(node.left, list);
-        list.add(node.val);
-        inorderTraversal(node.right, list);
+        inorderTraversal(node.left, res);
+        res.add(node.val);
+        inorderTraversal(node.right, res);
     }
 
     /*
-     * 解法2：迭代
-     * - 思路：与 L144 解法3思路类似，区别在于访问节点的时机 —— 向左递归到底的路上先不访问节点，而是到底之后开始出栈时再访问节点。
+     * 解法2：DFS
+     * - 思路：用迭代的方式实现 DFS —— 与 L144 解法3类似，区别在于访问节点的时机 —— 先向左迭代到底，一路上只入栈而不不访问节点，
+     *   到底之后开始出栈时再访问节点。
      * - 时间复杂度 O(n)，空间复杂度 O(h)，其中 h 是树高。
      * */
     public static List<Integer> inorderTraversal2(TreeNode root) {
@@ -51,8 +53,8 @@ public class L94_BinaryTreeInorderTraversal {
     }
 
     /*
-     * 解法3：迭代（解法2的变种）
-     * - 思路：与解法1一致。
+     * 解法3：迭代（解法2的另一种写法）
+     * - 思路：与解法2一致。
      * - 时间复杂度 O(n)，空间复杂度 O(h)，其中 h 是树高。
      * */
     public static List<Integer> inorderTraversal3(TreeNode root) {
@@ -86,10 +88,14 @@ public class L94_BinaryTreeInorderTraversal {
      * - 优势：这种解法虽然繁琐一点，但是更加灵活，只需极少的改动即可变为中序或后续遍历（SEE: L144 的解法5、L145 的解法5）。
      * - 时间复杂度 O(n)，空间复杂度 O(h)，其中 h 是树高。
      * */
+    public enum CmdType {
+        TRAVERSE, VISIT
+    }
+
     private static class Command {
-        String type;     // Enum ("traverse" or "vist")
+        CmdType type;
         TreeNode node;
-        Command(String type, TreeNode node) {
+        Command(CmdType type, TreeNode node) {
             this.type = type;
             this.node = node;
         }
@@ -99,19 +105,19 @@ public class L94_BinaryTreeInorderTraversal {
         List<Integer> res = new ArrayList<>();
         if (root == null) return res;
         Stack<Command> stack = new Stack<>();  // 栈中存的是 Command（将节点和指令的 pair）
-        stack.push(new Command("traverse", root));
+        stack.push(new Command(CmdType.TRAVERSE, root));
 
         while (!stack.isEmpty()) {
             Command cmd = stack.pop();
             TreeNode node = cmd.node;
-            if (cmd.type.equals("visit"))
+            if (cmd.type == CmdType.VISIT)
                 res.add(node.val);
             else {
                 if (node.right != null)
-                    stack.push(new Command("traverse", node.right));
-                stack.push(new Command("visit", node));
+                    stack.push(new Command(CmdType.TRAVERSE, node.right));  // 先入栈右子节点
+                stack.push(new Command(CmdType.VISIT, node));               // 访问父节点
                 if (node.left != null)
-                    stack.push(new Command("traverse", node.left));
+                    stack.push(new Command(CmdType.TRAVERSE, node.left));   // 再入栈左子节点
             }
         }
 
@@ -120,7 +126,7 @@ public class L94_BinaryTreeInorderTraversal {
 
     public static void main(String[] args) {
         TreeNode t1 = createBinaryTreeDepthFirst(new Integer[]{5, 3, 1, null, null, 4, null, null, 7, 6});
-        log(inorderTraversal(t1));
+        log(inorderTraversal4(t1));
         /*
          * expects [1, 3, 4, 5, 6, 7].（若是 BST，则中序遍历结果一定是有序的）
          *         5
@@ -131,7 +137,7 @@ public class L94_BinaryTreeInorderTraversal {
          * */
 
         TreeNode t2 = createBinaryTreeDepthFirst(new Integer[]{1, null, 2, 3});
-        log(inorderTraversal(t2));
+        log(inorderTraversal4(t2));
         /*
          * expects [1, 3, 2]
          *      1
@@ -142,7 +148,7 @@ public class L94_BinaryTreeInorderTraversal {
          * */
 
         TreeNode t3 = createBinaryTreeDepthFirst(new Integer[]{});
-        log(inorderTraversal(t3));
+        log(inorderTraversal4(t3));
         /*
          * expects []
          * */
