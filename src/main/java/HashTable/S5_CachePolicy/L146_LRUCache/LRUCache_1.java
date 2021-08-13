762b0e1808e8f522f2fdc27cc146843e5e383203
Max Jiang <maxjiang23326@gmail.com>
2021-08-13 21:41:09 +0800

    feat: add L460

--- src/main/java/HashTable/S5_CachePolicy/L146_LRUCache/LRUCache_1.java
+++ src/main/java/HashTable/S5_CachePolicy/L146_LRUCache/LRUCache_1.java
@@ -93,15 +93,15 @@ public class LRUCache_1 {
     }
 
     public static void main(String[] args) {
-        LRUCache_1 lRUCache = new LRUCache_1(2);
-        lRUCache.put(1, 1);    // cache is {1:1}
-        lRUCache.put(2, 2);    // cache is {1:1, 2:2}
-        log(lRUCache.get(1));  // return 1
-        lRUCache.put(3, 3);    // LRU key was 2, evicts key 2, cache is {1:1, 3:3}
-        log(lRUCache.get(2));  // returns -1 (not found)
-        lRUCache.put(4, 4);    // LRU key was 1, evicts key 1, cache is {4:4, 3:3}
-        log(lRUCache.get(1));  // return -1 (not found)
-        log(lRUCache.get(3));  // return 3
-        log(lRUCache.get(4));  // return 4
+        LRUCache_1 cache = new LRUCache_1(2);
+        cache.put(1, 1);    // cache is {1:1}
+        cache.put(2, 2);    // cache is {1:1, 2:2}
+        log(cache.get(1));  // return 1
+        cache.put(3, 3);    // LRU key was 2, evicts key 2, cache is {1:1, 3:3}
+        log(cache.get(2));  // returns -1 (not found)
+        cache.put(4, 4);    // LRU key was 1, evicts key 1, cache is {4:4, 3:3}
+        log(cache.get(1));  // return -1 (not found)
+        log(cache.get(3));  // return 3
+        log(cache.get(4));  // return 4
     }
 }
