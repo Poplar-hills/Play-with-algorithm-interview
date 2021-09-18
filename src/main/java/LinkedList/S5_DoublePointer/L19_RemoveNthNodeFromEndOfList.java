f29fd28dac48f61da54af8ac586890066b04ebf1
Max Jiang <maxjiang23326@gmail.com>
2021-09-18 13:33:34 +0800

    fix: L19 typo in name

--- src/main/java/LinkedList/S5_DoublePointer/L19_RemoveNthNodeFromEndOfList.java
+++ src/main/java/LinkedList/S5_DoublePointer/L19_RemoveNthNodeFromEndOfList.java
@@ -18,9 +18,8 @@ public class L19_RemoveNthNodeFromEndOfList {
      * */
     public static ListNode removeNthFromEnd(ListNode head, int n) {
         int l = getLength(head);
-
         if (l < n) return head;
-        int m = l - n;   // 从头开始第 m 个元素即是待删除元素（m 从 0 开始）
+        int m = l - n;   // 从头开始第 m 个节点即是待删除节点（m 从 0 开始）
 
         ListNode dummyHead = new ListNode();  // ∵ 待删除的可能是第一个节点 ∴ 需要 dummyHead
         dummyHead.next = head;
@@ -30,15 +29,15 @@ public class L19_RemoveNthNodeFromEndOfList {
             prev = prev.next;
             curr = curr.next;
         }
-        prev.next = curr.next;
+        prev.next = curr.next;  // 当找到第 m 个节点时，跳过它
 
         return dummyHead.next;
     }
 
     private static int getLength(ListNode head) {
-        int len = 0;
-        for ( ; head != null; head = head.next) len++;
-        return len;
+        int count = 0;
+        for ( ; head != null; head = head.next) count++;
+        return count;
     }
 
     /*
@@ -48,15 +47,15 @@ public class L19_RemoveNthNodeFromEndOfList {
      *   1. 待删除的节点4 ∴ 只要获得节点3即可完成删除；
      *   2. 虽然不知道链表长度，但能知道节点3与 null 的差距是 n+1 ∴ 可以利用这个差距，设置两个初始差距为 n+1 的指针，然后
      *      让他们同时移动，当右指针到达 null 时，左指针即指向节点3；
-     * - 💎 技巧：👆这个利用节点与 null 之间的距离差移动指针，来找到待删除节点的前一节点，这个技巧很妙。
+     * - 💎 技巧：👆利用节点与 null 之间的距离差移动指针来找到待删除节点的前一节点，这个技巧很妙。
      * - 时间复杂度 O(n)，空间复杂度 O(1)。
      * */
     public static ListNode removeNthFromEnd2(ListNode head, int n) {
         if (head == null || n < 0) return head;
 
-        ListNode dummnyHead = new ListNode();
-        dummnyHead.next = head;
-        ListNode l = dummnyHead, r = dummnyHead;
+        ListNode dummyHead = new ListNode();
+        dummyHead.next = head;
+        ListNode l = dummyHead, r = dummyHead;
 
         for (int i = 0; i < n + 1; i++) {  // 先将 r 移动到距 l n+1 的位置
             if (r == null) return head;    // 处理 n 越界的情况
@@ -69,14 +68,14 @@ public class L19_RemoveNthNodeFromEndOfList {
         }
         l.next = l.next.next;   // 删除节点
 
-        return dummnyHead.next;
+        return dummyHead.next;
     }
 
     /*
      * 解法3：递归
      * - 思路：先递归到底，在递归回程上的第 n 个节点就是链表的倒数第 n 个节点。
      * - 实现：为了在回程时数出第 n 个节点，每层递归函数一个 Pair<新链表的头结点, 该头结点是倒数第几个节点>。
-     * - 时间复杂度 O(n)，空间复杂度 O(1)。
+     * - 时间复杂度 O(n)，空间复杂度 O(n)。
      * */
     public static ListNode removeNthFromEnd3(ListNode head, int n) {
         Pair<ListNode, Integer> p = helper3(head, n);
@@ -99,15 +98,15 @@ public class L19_RemoveNthNodeFromEndOfList {
 
     public static void main(String[] args) {
         ListNode l1 = createLinkedList(new int[]{1, 2, 3, 4, 5});
-        printLinkedList(removeNthFromEnd3(l1, 2));  // expects 1->2->3->5->NULL
+        printLinkedList(removeNthFromEnd(l1, 2));  // expects 1->2->3->5->NULL
 
         ListNode l2 = createLinkedList(new int[]{1, 2, 3});
-        printLinkedList(removeNthFromEnd3(l2, 3));  // expects 2->3->NULL
+        printLinkedList(removeNthFromEnd(l2, 3));  // expects 2->3->NULL
 
         ListNode l3 = createLinkedList(new int[]{1});
-        printLinkedList(removeNthFromEnd3(l3, 2));  // expects 1->NULL (n 越界的 case)
+        printLinkedList(removeNthFromEnd(l3, 2));  // expects 1->NULL (n 越界的 case)
 
         ListNode l4 = createLinkedList(new int[]{});
-        printLinkedList(removeNthFromEnd3(l4, 2));  // expects NULL
+        printLinkedList(removeNthFromEnd(l4, 2));  // expects NULL
     }
 }
