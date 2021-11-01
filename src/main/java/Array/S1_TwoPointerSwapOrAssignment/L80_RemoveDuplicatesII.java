5f114be67b50c8bc1891fd452f1d2ca0bc3cc00f
Max Jiang <maxjiang23326@gmail.com>
2021-11-01 17:35:55 +0800

    feat: L80 add solution

--- src/main/java/Array/S1_TwoPointerSwapOrAssignment/L80_RemoveDuplicatesII.java
+++ src/main/java/Array/S1_TwoPointerSwapOrAssignment/L80_RemoveDuplicatesII.java
@@ -23,15 +23,18 @@ public class L80_RemoveDuplicatesII {
      *              v  i       - arr[i] != arr[v-2], v != i, arr[v] = arr[i], v++, i++
      *     1, 1, 2, 2, 2, 3
      *                 v  i    - arr[i] != arr[v-2], v != i, arr[v] = arr[i], v++, i++
-     * - 实现：L26_RemoveDuplicates 的实现稍微难懂，而本实现中的逻辑更符合直觉。
-     * - 👉 技巧：在有序数组中判断一个元素是否连续出现3次的方法（与上上个元素比较）。
+     * - 实现：
+     *   - 注意这里比较的是 arr[i] 与 arr[v-2]，而非 arr[i] 与 arr[i-2]，这是 ∵ 若要将 arr[i] 复制到 arr[v] 上，则
+     *     需要保证 arr[i] != arr[v-2]，否则复制后仍会出现三个 duplicates 的情况。
+     *   - L26_RemoveDuplicates 的实现稍微难懂，而本实现中的逻辑更符合直觉。
+     *   - 👉 与上上个元素比较是在有序数组中判断一个元素是否连续出现3次的方法。
      * - 时间复杂度 O(n)，空间复杂度 O(1)。
      * */
     public static int removeDuplicates(int[] arr) {
         if (arr == null || arr.length <= 2) return arr.length;
         int nextValidIdx = 2;
-        for (int i = 2; i < arr.length; i++) {  // 从第3个元素开始遍历
-            if (arr[i] == arr[nextValidIdx - 2])
+        for (int i = 2; i < arr.length; i++) {    // 从第3个元素开始遍历
+            if (arr[i] == arr[nextValidIdx - 2])  // 注意比较的是 arr[i] 与 arr[v-2]，而非 arr[i] 与 arr[i-2]
                 continue;           // 若发现有元素连续出现3次，则只让 i++，nextValidIdx 不变
             if (nextValidIdx != i)
                 arr[nextValidIdx] = arr[i];
@@ -42,16 +45,35 @@ public class L80_RemoveDuplicatesII {
 
     /*
      * 解法2：双指针 + Swap
-     * - 思路：与解法1类似，但在判断条件上与 L26_RemoveDuplicates 一致，两边都是使用 i 取值 ∴ 更符合直觉。
-     * - 实现：使用 swap 代替复制。注意 ∵ [1,1,1,2,2,3] 在 swap 之后会变成 [1,1,2,1,2,3] ∴ 不能只判断 arr[i] == arr[i-2]，
-     *   而是必须判断 arr[i], arr[i-1], arr[i-2] 3个元素都相等才行。
+     * - 思路：与解法1类似，只是由直接赋值改为了 swap。
      * - 时间复杂度 O(n)，空间复杂度 O(1)。
      * */
     public static int removeDuplicates2(int[] arr) {
         if (arr == null || arr.length <= 2) return arr.length;
         int nextValidIdx = 2;
         for (int i = 2; i < arr.length; i++) {
-            if (arr[i] == arr[i - 1] && arr[i - 1] == arr[i - 2])  // 注意 ∵ 下面使用 swap ∴ 这里必须判断3个元素都相等
+            if (arr[i] == arr[nextValidIdx - 2])
+                continue;
+            if (i != nextValidIdx)
+                swap(arr, i, nextValidIdx);  // 与解法1唯一区别就是改成了 swap
+            nextValidIdx++;
+        }
+        return nextValidIdx;
+    }
+
+    /*
+     * 解法3：双指针 + Swap
+     * - 思路：与解法2一致。
+     * - 实现：与解法2唯一不同是判断条件改为了 arr[i]、arr[i-1]、arr[i-2] 之间的比较 ∵ [1,1,1,2,2,3] 在 swap 之后会
+     *   变成 [1,1,2,1,2,3] ∴ 不能只判断 arr[i] == arr[i-2]，而是必须判断 arr[i], arr[i-1], arr[i-2] 3个元素都
+     *   相等才行。
+     * - 时间复杂度 O(n)，空间复杂度 O(1)。
+     * */
+    public static int removeDuplicates3(int[] arr) {
+        if (arr == null || arr.length <= 2) return arr.length;
+        int nextValidIdx = 2;
+        for (int i = 2; i < arr.length; i++) {
+            if (arr[i] == arr[i - 1] && arr[i - 1] == arr[i - 2])
                 continue;
             if (i != nextValidIdx)
                 swap(arr, i, nextValidIdx);
