1a577278b3aa876853e2c2cdb6dcea2fe250ae38
Max Jiang <maxjiang23326@gmail.com>
2021-11-01 19:01:39 +0800

    doc: L341 add comments

--- src/main/java/StackAndQueue/S2_StackAndRecursion/L341_FlattenNestedListIterator/NestedIterator.java
+++ src/main/java/StackAndQueue/S2_StackAndRecursion/L341_FlattenNestedListIterator/NestedIterator.java
@@ -18,11 +18,11 @@ import java.util.*;
 
 /*
  * 解法1：Eager style + Recursion
- * - 思路：根据题意中的调用方式，解法类中要实现一个有 hasNext、next 方法的 iterator。而 iterator 有两种基本形式：
+ * - 💎 思路：根据题意中的调用方式，解法类中要实现一个有 hasNext、next 方法的 iterator。而 iterator 有两种基本形式：
  *     1. Eager: 先将所有元素都预先计算出来并放入 Queue，然后在调用方法时逐个输出；
- *     2. Lazy: 先将所有元素都倒序加载到调用栈 callstack 里，然后在每次调用方法时现进行计算并输出。
+ *     2. Lazy: 先将所有元素都倒序加载到调用栈（call stack）里，然后在每次调用方法时现进行计算并输出。
  *   本解法中采用 eager iterator 的思路 —— 预先将整个 NestedList 解析成 Integer list。该过程中：
- *     1. 💎 ∵ iterator 中元素的输出顺序应该与输入数据的元素顺序一致 ∴ iterator 的基本数据结构应该是 queue；
+ *     1. ∵ iterator 中元素的输出顺序应该与输入数据的元素顺序一致 ∴ iterator 的基本数据结构应该是 queue；
  *     2. 输入数据中可能有无限层级的嵌套，这是个典型的可以递归的场景。
  * - 时间复杂度 O(n)，空间复杂度 O(n)。
  * */
@@ -86,12 +86,14 @@ class NestedIterator2 implements Iterator<Integer> {
 
 /*
  * 解法3：Lazy style
- * - 思路：解法1、2中的 eager style 的最大缺点就是，对于大数据集存在性能问题 —— we are pre-computing and pre-loading
- *   everything into memory, which can be a big waste of resource。要解决这个问题可使用 lazy style：Lazy 与 eager
- *   的区别在于实例化时（构造器中）做的事情：
- *   - Eager iterator 在实例化时要完成所有计算和加载工作；
- *   - 💎 Lazy iterator 在实例化时只将数据加载到调用栈（将 nestedList 的所有元素入栈，但不解析），而等到真正消费时（hasNext、next）
- *     再去解析（找到下一个可用的 integer）。
+ * - 💎 思路：解法1、2中的 eager style 的最大缺点就是，对于大数据集存在性能问题 —— we are pre-computing and
+ *   pre-loading everything into memory, which can be a big waste of resource。要解决这个问题可使用 lazy style：
+ *   Lazy 与 eager 的区别在于：
+ *   1. Eager 的数据结构是 Queue；Lazy 的数据结构是 Stack。
+ *   2. 实例化时（构造器中）做的事情：
+ *      - Eager iterator 在实例化时要完成所有计算和加载工作；
+ *      - Lazy iterator 在实例化时只将数据加载到调用栈（将 nestedList 的所有元素入栈，但不解析），而等到真正消费时
+ *        （hasNext、next）再去解析（找到下一个可用的 integer）。
  * - 实现：与解法2类似，使用 stack + while 模拟调用栈：
  *   1. 在 constructor 中将数据加载到调用栈中；
  *   2. 在 next() 时去消费 stack 中的数据时去计算。
@@ -110,7 +112,7 @@ class NestedIterator3 implements Iterator<Integer> {
 
     @Override
     public boolean hasNext() {
-        while (!callStack.isEmpty()) {  // 在栈内循环查找 int
+        while (!callStack.isEmpty()) {         // 在栈内循环查找下一个可用的 int
             if (callStack.peek().isInteger())  // 若是 int 则放在那等待消费，不再解析更多元素
                 return true;
             pushInReverseOrder(callStack.pop().getList());  // 否则将元素加载到调用栈中进行解析
