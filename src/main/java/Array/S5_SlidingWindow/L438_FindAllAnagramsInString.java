949906cd1a87af6d576344d9cd7f3e9e935f4197
Max Jiang <maxjiang23326@gmail.com>
2021-11-16 13:18:46 +0800

    doc: L438 add comments

--- src/main/java/Array/S5_SlidingWindow/L438_FindAllAnagramsInString.java
+++ src/main/java/Array/S5_SlidingWindow/L438_FindAllAnagramsInString.java
@@ -54,8 +54,7 @@ public class L438_FindAllAnagramsInString {
 
     /*
      * 解法2：滑动窗口（解法1的简化版，🥇最优解）
-     * - 思路：与解法1一致。
-     * - 实现：与 L76_MinimumWindowSubstring 解法2一致。
+     * - 思路：与 L76_MinimumWindowSubstring 解法2一致。
      * - 时间复杂度 O(n)，空间复杂度 O(n)。
      * */
     public static List<Integer> findAnagrams2(String s, String p) {
