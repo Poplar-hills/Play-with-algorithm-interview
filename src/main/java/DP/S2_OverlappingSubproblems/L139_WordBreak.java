2801a2e367544ab101b6fe16879b65638299f4d2
Max Jiang <maxjiang23326@gmail.com>
2021-12-07 00:17:19 +0800

    fix & doc: L139 incorrect indexes and update comments

--- src/main/java/DP/S2_OverlappingSubproblems/L139_WordBreak.java
+++ src/main/java/DP/S2_OverlappingSubproblems/L139_WordBreak.java
@@ -41,8 +41,8 @@ public class L139_WordBreak {
      *                    a/ ... apple\
      *                    x           ""
      *   ∴ 可知：
-     *     - 子问题定义：f(s,i) 表示“从索引 i 开始到末尾的字符串 s[i..n) 能否由 wordDict 中的单词拼接而成”；
-     *     - 递推表达式：f(s,i) = any(s[i..j) && f(s,j))，其中 i ∈ [0,n)，j ∈ [i+1,n]。
+     *     - 子问题定义：f(i) 表示“从索引 i 开始到末尾的字符串 s[i..n) 能否由 wordDict 中的单词拼接而成”；
+     *     - 递推表达式：f(i) = any(s[i..j] && f(j))，其中 i ∈ [0,n)，j ∈ [i+1,n]。
      * - 说明：该解法其实就是回溯搜索。
      * - 时间复杂度 O(n^n)，空间复杂度 O(n)。
      * */
@@ -53,7 +53,7 @@ public class L139_WordBreak {
 
     private static boolean dfs_1(String s, int i, Set<String> set) {
         if (i == s.length()) return true;                           // f("") 的情况返回 true
-        for (int j = i + 1; j < s.length(); j++) {
+        for (int j = i; j < s.length(); j++) {
             String firstHalf = s.substring(i, j + 1);
             if (set.contains(firstHalf) && dfs_1(s, j + 1, set))  // 若前后两段都在 set 中，说明找到解
                 return true;
@@ -84,7 +84,7 @@ public class L139_WordBreak {
         if (i == s.length()) return true;
         if (cache[i] != null) return cache[i];
 
-        for (int j = i + 1; j < s.length(); j++)
+        for (int j = i; j < s.length(); j++)
             if (set.contains(s.substring(i, j + 1)) && dfs(s, j + 1, set, cache))
                 return cache[i] = true;
 
@@ -94,23 +94,23 @@ public class L139_WordBreak {
     /*
      * 解法2：DP
      * - 思路：将解法1直接转换为 DP 的写法（其实本质思路与解法1是一样的 —— 都是自上而下分解任务），子问题定义和递推表达式不变：
-     *   - f(i) 表示“从索引 i 开始到末尾的字符串 s[i..n) 是否能由 wordDict 中的单词拼接而成”；
+     *   - f(i) 表示“从索引 i 开始到末尾的字符串 s[i..n) 能否由 wordDict 中的单词拼接而成”；
      *   - f(i) = any(s[i..j) && f(j))，其中 i ∈ [0,n)，j ∈ [i+1,n]。
      * - 时间复杂度 O(n^2)，空间复杂度 O(n)。
      * */
     public static boolean wordBreak2(String s, List<String> wordDict) {
         if (s == null || s.length() == 0) return false;
 
-        Set<String> set = new HashSet<>(wordDict);
         int n = s.length();
+        Set<String> set = new HashSet<>(wordDict);
         boolean[] dp = new boolean[n + 1];
-        dp[n] = true;                           // dp[n] 存储 f("") 的解
+        dp[n] = true;                       // dp[n] 存储 f("") 的解
 
-        for (int i = n - 1; i >= 0; i--) {      // 从后往前递推
-            for (int j = i + 1; j <= n; j++) {  // 在 s[i..n) 中尝试不同的截取方式
-                if (set.contains(s.substring(i, j)) && dp[j]) {  // 若 s[i..j)、s[j..n) 都在 set 中则说明 f(i) 有解
+        for (int i = n - 1; i >= 0; i--) {  // 从后往前递推
+            for (int j = i; j < n; j++) {   // 在 s[i..n) 中尝试不同的截取方式
+                if (set.contains(s.substring(i, j + 1)) && dp[j + 1]) {  // 若 s[i..j]、s(j..n) 都在 set 中则说明 f(i) 有解
                     dp[i] = true;
-                    break;                      // 若 f(i) 已经有解，则无需再尝试其他截取方式
+                    break;                  // 若 f(i) 已经有解，则无需再尝试其他截取方式
                 }
             }
         }
@@ -147,7 +147,7 @@ public class L139_WordBreak {
 
     /*
      * 解法4：DFS + Recursion + 前缀匹配
-     * - 实现：不同于解法1，本解法对 s 的分段方式不再是逐个字符分段检测，而是采用头部单词匹配。理由是，若 s 能由 wordDict 中的
+     * - 思路：不同于解法1，本解法对 s 的分段方式不再是逐个字符分段检测，而是采用头部单词匹配。理由是，若 s 能由 wordDict 中的
      *   词组成，则一定是由其中某一个词开头的（反之若 s 不是由 wordDict 中的词开头的，则可直接返回 false），找到这个词之后再对
      *   其后半段继续这样的匹配检测，从而形成递归结构。
      * - 注意：startWith() 方法的第二个参数指定匹配的起始索引，好用。
@@ -164,7 +164,7 @@ public class L139_WordBreak {
         if (cache[i] != null) return cache[i];
 
         for (String word : wordDict)
-            if (s.startsWith(word, i) && dfs4(s, i + word.length(), wordDict, cache))  // 若前、后两段都存在于 wordDict 中
+            if (s.startsWith(word, i) && dfs4(s, i + word.length(), wordDict, cache))
                 return cache[i] = true;
 
         return cache[i] = false;
