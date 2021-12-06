15066a5ff086ccb180419e6decddc3e0abe6c949
Max Jiang <maxjiang23326@gmail.com>
2021-12-06 14:26:57 +0800

    feat: L63 add solution

--- src/main/java/DP/S2_OverlappingSubproblems/L63_UniquePathsII.java
+++ src/main/java/DP/S2_OverlappingSubproblems/L63_UniquePathsII.java
@@ -10,39 +10,38 @@ import static Utils.Helpers.log;
 
 public class L63_UniquePathsII {
     /*
-     * 超时解：Recursion + Memoizaiton
+     * 解法1：Recursion + Memoizaiton
      * - 思路：在 L62_UniquePaths 解法1的基础上加上对障碍物的判断条件即可。
      *        ■ → ■ → ■         2 ← 1 ← 1
      *        ↓       ↓         ↑       ↑
      *        ■   □   ■   -->   1   0   1
      *        ↓       ↓         ↑       ↑
      *        ■ → ■ → ■         1 ← 1 ← 1
-     * - 时间复杂度 O(m*n)，空间复杂度 O(m*n)。∵ Recursion 是自顶向下递归到底后再自底向上返回顶部 ∴ 用时会比只自底向上遍历的解法2慢。
+     * - 时间复杂度 O(m*n)，空间复杂度 O(m*n)。
      * */
     public static int uniquePathsWithObstacles(int[][] grid) {
         if (grid == null || grid[0] == null) return 0;
         int[][] cache = new int[grid.length][grid[0].length];
-        return helper(grid, 0, 0, cache);
+        return dfs(grid, 0, 0, cache);
     }
 
-    private static int helper(int[][] grid, int r, int c, int[][] cache) {
-        int m = grid.length;
-        int n = grid[0].length;
+    private static int dfs(int[][] grid, int r, int c, int[][] cache) {
+        int m = grid.length, n = grid[0].length;
 
-        if (grid[r][c] == 1) return 0;             // 若是障碍物节点则直接返回0
-        if (r == m - 1 && c == n - 1) return 1;    // 递归到底
+        if (grid[r][c] == 1) return 0;           // 若是障碍物节点则直接返回0
+        if (r == m - 1 && c == n - 1) return 1;  // 递归到底
         if (cache[r][c] != 0) return cache[r][c];
 
-        int res = 0;
-        if (r != m - 1) res += helper(grid, r + 1, c, cache);
-        if (c != n - 1) res += helper(grid, r, c + 1, cache);
+        int pathCount = 0;
+        if (r != m - 1) pathCount += dfs(grid, r + 1, c, cache);
+        if (c != n - 1) pathCount += dfs(grid, r, c + 1, cache);
 
-        return cache[r][c] = res;
+        return cache[r][c] = pathCount;
     }
 
     /*
      * 解法1：DP
-     * - 思路：在 L62_UniquePaths 解法2的基础上加上对障碍物的判断即可。
+     * - 思路：自上而下的 DP 方式，即是在 L62_UniquePaths 解法2的基础上加上对障碍物的判断。
      * - 时间复杂度 O(m*n)，空间复杂度 O(m*n)。
      * */
     public static int uniquePathsWithObstacles1(int[][] grid) {
@@ -68,10 +67,43 @@ public class L63_UniquePathsII {
 
     /*
      * 解法2：DP
-     * - 思路：类似 L62_UniquePaths 解法4
+     * - 思路：自下而上的 DP 方式，类似 L62_UniquePaths 解法3。
+     *      0  0  1  0      1 → 1 → 0 → 0
+     *                      ↓   ↓   ↓   ↓
+     *      0  0  0  0      1 → 2 → 2 → 2
+     *                      ↓   ↓   ↓   ↓
+     *      1  1  1  0      0 → 0 → 0 → 2
+     *                      ↓   ↓   ↓   ↓
+     *      0  0  0  0      0 → 0 → 0 → 2
      * - 时间复杂度 O(m*n)，空间复杂度 O(m*n)。
      * */
     public static int uniquePathsWithObstacles2(int[][] grid) {
+        if (grid == null || grid[0] == null) return 0;
+
+        int m = grid.length, n = grid[0].length;
+        int[][] dp = new int[m][n];
+        dp[0][0] = 1;
+
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                if (grid[r][c] == 1) {
+                    dp[r][c] = 0;
+                    continue;
+                }
+                if (r != 0) dp[r][c] += dp[r - 1][c];
+                if (c != 0) dp[r][c] += dp[r][c - 1];
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
+
+    /*
+     * 解法3：DP
+     * - 思路：与解法2一致，也是自下而上的 DP 方式，类似 L62_UniquePaths 解法4。
+     * - 时间复杂度 O(m*n)，空间复杂度 O(m*n)。
+     * */
+    public static int uniquePathsWithObstacles3(int[][] grid) {
         if (grid == null || grid[0] == null) return 0;
 
         int m = grid.length, n = grid[0].length;
@@ -92,12 +124,12 @@ public class L63_UniquePathsII {
     }
 
     /*
-     * 解法3：DP + 一维数组
-     * - 思路：类似 L62_UniquePaths 解法6的思路，但不同点在于 ∵ grid 中有障碍物 ∴ L62 解法4、5、6里的
+     * 解法4：DP + 一维数组
+     * - 思路：类似 L62_UniquePaths 解法6，但不同点在于 ∵ grid 中有障碍物 ∴ L62 解法4、5、6里的
      *   "if (r==0 || c==0) dp[c] = 1;" 不再成立（∵ dp[障碍物] = 0）∴ 只能用 L62_UniquePaths 解法3的方式实现。
      * - 时间复杂度 O(m*n)，空间复杂度 O(n)。
      * */
-    public static int uniquePathsWithObstacles3(int[][] grid) {
+    public static int uniquePathsWithObstacles4(int[][] grid) {
         if (grid == null || grid[0] == null) return 0;
 
         int m = grid.length;
