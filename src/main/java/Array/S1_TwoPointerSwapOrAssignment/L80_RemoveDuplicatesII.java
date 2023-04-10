41cf66253f801f22ea74c070083c74d912f038b5
Max Jiang <Poetry_In_Motion@192.168.0.114>
2023-04-10 22:39:44 +0800

    fix: null check

--- src/main/java/Array/S1_TwoPointerSwapOrAssignment/L80_RemoveDuplicatesII.java
+++ src/main/java/Array/S1_TwoPointerSwapOrAssignment/L80_RemoveDuplicatesII.java
@@ -16,13 +16,13 @@ public class L80_RemoveDuplicatesII {
      * - 思路：与 L26_RemoveDuplicates 一致，只是判断条件不同，L26 的判断条件是 arr[i] == arr[i-1]，两边都是用 i 取值，
      *   而该解法中需要使用 arr[i] == arr[v-2] 比较：
      *     1, 1, 1, 2, 2, 3
-     *           vi            - arr[i] == arr[v-2], v stays, i++
+     *           vi            - arr[i]==arr[v-2], v stays, i++
      *     1, 1, 1, 2, 2, 3
-     *           v  i          - arr[i] != arr[v-2], v != i, arr[v] = arr[i], v++, i++
+     *           v  i          - arr[i]!=arr[v-2], v!=i, arr[v]=arr[i], v++, i++
      *     1, 1, 2, 2, 2, 3
-     *              v  i       - arr[i] != arr[v-2], v != i, arr[v] = arr[i], v++, i++
+     *              v  i       - arr[i]!=arr[v-2], v!=i, arr[v]=arr[i], v++, i++
      *     1, 1, 2, 2, 2, 3
-     *                 v  i    - arr[i] != arr[v-2], v != i, arr[v] = arr[i], v++, i++
+     *                 v  i    - arr[i]!=arr[v-2], v!=i, arr[v]=arr[i], v++, i++
      * - 实现：
      *   - 注意这里比较的是 arr[i] 与 arr[v-2]，而非 arr[i] 与 arr[i-2]，这是 ∵ 若要将 arr[i] 复制到 arr[v] 上，则
      *     需要保证 arr[i] != arr[v-2]，否则复制后仍会出现三个 duplicates 的情况。
@@ -31,7 +31,9 @@ public class L80_RemoveDuplicatesII {
      * - 时间复杂度 O(n)，空间复杂度 O(1)。
      * */
     public static int removeDuplicates(int[] arr) {
-        if (arr == null || arr.length <= 2) return arr.length;
+        if (arr == null) return 0;
+        if (arr.length <= 2) return arr.length;
+
         int nextValidIdx = 2;
         for (int i = 2; i < arr.length; i++) {    // 从第3个元素开始遍历
             if (arr[i] == arr[nextValidIdx - 2])  // 注意比较的是 arr[i] 与 arr[v-2]，而非 arr[i] 与 arr[i-2]
@@ -49,7 +51,9 @@ public class L80_RemoveDuplicatesII {
      * - 时间复杂度 O(n)，空间复杂度 O(1)。
      * */
     public static int removeDuplicates2(int[] arr) {
-        if (arr == null || arr.length <= 2) return arr.length;
+        if (arr == null) return 0;
+        if (arr.length <= 2) return arr.length;
+
         int nextValidIdx = 2;
         for (int i = 2; i < arr.length; i++) {
             if (arr[i] == arr[nextValidIdx - 2])
@@ -64,13 +68,15 @@ public class L80_RemoveDuplicatesII {
     /*
      * 解法3：双指针 + Swap
      * - 思路：与解法2一致。
-     * - 实现：与解法2唯一不同是判断条件改为了 arr[i]、arr[i-1]、arr[i-2] 之间的比较 ∵ [1,1,1,2,2,3] 在 swap 之后会
-     *   变成 [1,1,2,1,2,3] ∴ 不能只判断 arr[i] == arr[i-2]，而是必须判断 arr[i], arr[i-1], arr[i-2] 3个元素都
-     *   相等才行。
+     * - 实现：与解法1、2的不同是判断条件不再比较 arr[i] 与 arr[nextValidIdx]，而是判断条件的两边都改用 i 取值。但这样一来，
+     *   [1,1,1,2,2,3] 在 swap 之后会变成 [1,1,2,1,2,3] ∴ 不能只判断 arr[i] == arr[i-2]，而是必须判断
+     *   arr[i], arr[i-1], arr[i-2] 3个元素都相等才行。
      * - 时间复杂度 O(n)，空间复杂度 O(1)。
      * */
     public static int removeDuplicates3(int[] arr) {
-        if (arr == null || arr.length <= 2) return arr.length;
+        if (arr == null) return 0;
+        if (arr.length <= 2) return arr.length;
+
         int nextValidIdx = 2;
         for (int i = 2; i < arr.length; i++) {
             if (arr[i] == arr[i - 1] && arr[i - 1] == arr[i - 2])
