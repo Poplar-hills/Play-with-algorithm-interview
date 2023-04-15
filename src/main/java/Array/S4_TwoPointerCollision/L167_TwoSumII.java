7afda88a0b47fa851acf9e5e3d3006fc0adee3d4
Max Jiang <maxjiang23326@gmail.com>
2023-04-15 14:42:33 +0800

    feat: remove invalid solution

--- src/main/java/Array/S4_TwoPointerCollision/L167_TwoSumII.java
+++ src/main/java/Array/S4_TwoPointerCollision/L167_TwoSumII.java
@@ -45,7 +45,7 @@ public class L167_TwoSumII {
      *         i               - nums[i]=-5 ∴ binary search target+5 within [1,n]
      *             i           - nums[i]=-2 ∴ binary search target+2 within [2,n]
      *                i        - nums[i]=0  ∴ binary search target-1 within [3,n], found solution [2,4]
-     * - 时间复杂度 O(nlogn)，空间复杂度 O(logn)。
+     * - 👉🏻时间复杂度 O(nlogn)，空间复杂度 O(logn)。∵ 二分搜索要进行递归 ∴ 空间复杂度为 O(logn)。
      * */
     public static int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
@@ -70,39 +70,37 @@ public class L167_TwoSumII {
      * - 思路：与 L11_ContainerWithMostWater 类似，可以采用指针对撞 —— 每次移动左右中的一个指针，具体移动哪个则由当前计算结果
      *   若当前计算结果 < 目标值，则左指针++来增大计算结果，否则右指针--来减小计算结果。又 ∵ nums 是有序的 ∴ 计算过程就是比较
      *   nums[l] + nums[r] 与 target 的大小。
+     * - 对于 [ -5, -2, 1, 3, 4 ], target 4 来说：
+     *          l            r   - nums[l] + nums[r] = -1 < target ∴ l++
+     *              l        r   - nums[l] + nums[r] = -2 < target ∴ l++
+     *                 l     r   - nums[l] + nums[r] = 5 > target ∴ r--
+     *                 l  r      - nums[l] + nums[r] = 5 == target, found solution
      * - 注：两路、三路快排实际上也是指针对撞的一种应用。
      * - 时间复杂度 O(n)，空间复杂度 O(1)。
      * */
     public static int[] twoSum2(int[] nums, int target) {
-        int i = 0, j = nums.length - 1;
-        while (i != j) {
-            int sum = nums[i] + nums[j];
-            if (sum < target) i++;
-            else if (sum > target) j--;
-            else return new int[] {i + 1, j + 1};
+        int l = 0, r = nums.length - 1;
+        while (l != r) {
+            int sum = nums[l] + nums[r];
+            if (sum < target) l++;
+            else if (sum > target) r--;
+            else return new int[] {l + 1, r + 1};
         }
         return null;
     }
 
     /*
-     * 解法3：指针对撞（解法2的简化版）
+     * 解法3：查找表
+     * - 思路：一遍遍历，一遍构建 index map，一遍查找当前元素的 complement。
+     * - 对于 [-2, -2, 1, 3, 4], target=5 来说：
+     *         i                - target-nums[i]=7, not in Map ∴ Map(-2:0)
+     *             i            - target-nums[i]=7, not in Map ∴ Map(-2:1)
+     *                i         - target-nums[i]=4, not in Map ∴ Map(-2:1, 1:2)
+     *                   i      - target-nums[i]=2, not in Map ∴ Map(-2:1, 1:2, 3:3)
+     *                      i   - target-nums[i]=1, found in Map ∴ return [map.get(1)+1, i+1]
      * - 时间复杂度 O(n)，空间复杂度 O(1)。
      * */
     public static int[] twoSum3(int[] nums, int target) {
-        int i = 0, j = nums.length - 1;
-        while (nums[i] + nums[j] != target) {
-            if (nums[i] + nums[j] < target) i++;
-            else j--;
-        }
-        return new int[] {i + 1, j + 1};
-    }
-
-    /*
-     * 解法4：查找表
-     * - 思路：一遍遍历，一遍构建 index map，一遍查找当前元素的 complement。
-     * - 时间复杂度 O(n)，空间复杂度 O(1)。
-     * */
-    public static int[] twoSum4(int[] nums, int target) {
         Map<Integer, Integer> indexMap = new HashMap<>();  // Map<num, index>
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
@@ -113,21 +111,12 @@ public class L167_TwoSumII {
         return null;
     }
 
-
-    public static int[] twoSum_(int[] nums, int target) {
-        for (int i = 0; i < nums.length - 1; i++) {
-            int searchTarget = target - nums[i];
-            int r = binarySearch_(nums, searchTarget, i + 1, nums.length - 1);  // binary search within [i+1,n]
-            if (r != -1) return new int[]{i+1, r+1};
-        }
-        return null;
-    }
-
     public static void main(String[] args) {
-        log(twoSum_(new int[]{-5, -2, 1, 3, 4}, 5));  // expects [2, 4]（注意返回的是从1开始的元素序号）
-        log(twoSum_(new int[]{2, 7, 11, 15}, 9));     // expects [1, 2]
-        log(twoSum_(new int[]{2, 3, 4}, 6));          // expects [1, 3]
-        log(twoSum_(new int[]{-3, -2, 2, 3}, 0));     // expects [1, 4] or [2, 3]
-        log(twoSum_(new int[]{-1, 0}, -1));           // expects [1, 2]
+        log(twoSum3(new int[]{-5, -2, 1, 3, 4}, 5));  // expects [3, 5]（注意返回的是从1开始的元素序号）
+        log(twoSum3(new int[]{2, 7, 11, 15}, 9));     // expects [1, 2]
+        log(twoSum3(new int[]{2, 3, 4}, 6));          // expects [1, 3]
+        log(twoSum3(new int[]{-3, -2, 2, 3}, 0));     // expects [1, 4] or [2, 3]
+        log(twoSum3(new int[]{-1, 0}, -1));           // expects [1, 2]
+        log(twoSum3(new int[]{1, 2, 3, 4}, -1));      // expects null
     }
 }
