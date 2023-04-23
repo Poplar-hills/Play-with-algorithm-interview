de7ca2327aa916fd4140a3dfac50c2d89fcba4fb
max.jiang <max.jiang@disney.com>
2023-04-23 15:30:14 +0800

    feat: L1763 refactor

--- src/main/java/Array/S5_SlidingWindow/L1763_LongestNiceSubstring.java
+++ src/main/java/Array/S5_SlidingWindow/L1763_LongestNiceSubstring.java
@@ -11,7 +11,7 @@ import static Utils.Helpers.log;
  *   and lowercase. For example, "abABB" is nice because 'A' and 'a' appear, and 'B' and 'b' appear. However,
  *   "abA" is not because 'b' appears, but 'B' does not.
  * - Given a string s, return the longest substring of s that is nice. If there are multiple, return the
- *   substring of the earliest occurrence. If there are none, return "-1".
+ *   substring of the earliest occurrence. If there are none, return an empty string.
  *
  * - Following question: Instead of returning the longest, return the shortest nice substring of s.
  * */
@@ -48,29 +48,55 @@ public class L1763_LongestNiceSubstring {
         String result = "";
 
         for (int l = 0; l < s.length(); l++) {
-            for (int r = l + 2; r <= s.length(); r++) {  // r 从 l+2 开始 ∵ nice substring 至少要有两个字符
+            for (int r = l + 2; r <= s.length(); r++) {  // ∵ nice substring 至少要有两个字符 ∴ r 要从 l+2 开始，且 r <= n
                 String sub = s.substring(l, r);
                 if (result.length() < sub.length() && isNice(sub))
                     result = sub;
             }
         }
-        return result.isEmpty() ? "-1" : result;
+        return result;
     }
 
     public static boolean isNice(String str) {
-        for (char c : str.toCharArray())
-            if (!str.contains(Character.toUpperCase(c) + "") || !str.contains(Character.toLowerCase(c) + ""))
+        char[] chars = str.toCharArray();
+        Set<Character> set = new HashSet<>();
+        for (char c : chars) set.add(c);
+
+        for (char c : chars)
+            if (!set.contains(Character.toLowerCase(c)) || !set.contains(Character.toUpperCase(c)))
                 return false;
+
         return true;
     }
 
     /*
      * 解法2：Divide & Conquer + 递归
-     * - 思路：采用分治思想 TODO: 未完全理解。
+     * - 思路：采用分治思想。遍历 [0,n)，若其中某一字符 c 在 s 中没有对应的大/小写字符，则将其 exclude 掉，然后在其左右子字符串
+     *   中继续递归查找。
+     * - 例："Y  a  z  a  A  a  y"
+     *       i                    - 有配对，continue
+     *          i                 - 有配对，continue
+     *             i              - 无配对，在"Ya", "aAay"中继续查找，前者返回""，后者返回"aAa"
+     *                i           - 有配对，continue
+     *                   i        - 有配对，continue
+     *                      i     - 有配对，continue
+     *                         i  - 有配对，continue，最终返回"aAa"
+     *       -> "Y  a"
+     *           i     - 无配对，在"", "a"中继续查找，都返回""
+     *       -> "a  A  a  y"
+     *           i           - 有配对，continue
+     *              i        - 有配对，continue
+     *                 i     - 有配对，continue
+     *                    i  - 无配对，在"aAa", ""中继续查找，前者返回"aAa"
+     *          -> "a  A  a"
+     *              i           - 有配对，continue
+     *                 i        - 有配对，continue
+     *                    i     - 有配对，continue，返回"aAa"
+     *
      * - 时间复杂度 O(n^2)，空间复杂度 O(n)。
      * */
     public static String longestNiceSubstring2(String s) {
-        if (s.length() < 2) return "-1";
+        if (s.length() < 2) return "";
         char[] chars = s.toCharArray();
 
         Set<Character> set = new HashSet<>();
@@ -78,11 +104,11 @@ public class L1763_LongestNiceSubstring {
 
         for (int i = 0; i < chars.length; i++) {
             char c = chars[i];
-            if (set.contains(Character.toUpperCase(c)) && set.contains(Character.toLowerCase(c)))
-                continue;
-            String sub1 = longestNiceSubstring2(s.substring(0, i));
-            String sub2 = longestNiceSubstring2(s.substring(i + 1));
-            return sub1.length() >= sub2.length() ? sub1 : sub2;
+            if (!set.contains(Character.toUpperCase(c)) || set.contains(Character.toLowerCase(c))) { // 若 c 在 s 中没有配对
+                String sub1 = longestNiceSubstring2(s.substring(0, i));             // 在 c 左边的字符串中查找
+                String sub2 = longestNiceSubstring2(s.substring(i + 1));  // 在 c 右边的字符串中查找
+                return sub1.length() >= sub2.length() ? sub1 : sub2;
+            }
         }
 
         return s;
@@ -96,24 +122,15 @@ public class L1763_LongestNiceSubstring {
      * - 时间复杂度 O(n^3)，空间复杂度 O(n)。
      * */
     public static String shortestNiceSubstring(String s) {
-        String res = "-1";
+        String res = "";
         if (s == null || s.isEmpty()) return res;
         char[] chars = s.toCharArray();
         int minLen = s.length() + 1;
 
         for (int l = 0; l < s.length(); l++) {
             for (int r = l + 1; r < s.length(); r++) {
-                Set<Character> lowerSet = new HashSet<>(), upperSet = new HashSet<>();
-
-                for (int i = l; i <= r; i++) {            // 遍历 substring 的每一个字符
-                    if (Character.isLowerCase(chars[i]))  // 若是小写，放入小写 Set 中
-                        lowerSet.add(chars[i]);
-                    else                                  // 若是大写，先转换成小写后放入大写 Set 中
-                        upperSet.add(Character.toLowerCase(chars[i]));
-                }
-
-                if (lowerSet.equals(upperSet) && r - l + 1 < minLen) {  // 比较2个 Set 中的元素来判断 substring
-                    res = s.substring(l, r + 1);                        // 是否 nice
+                if (isNice2(chars, l, r) && r - l + 1 < minLen) {
+                    res = s.substring(l, r + 1);
                     minLen = r - l + 1;
                 }
             }
@@ -122,18 +139,31 @@ public class L1763_LongestNiceSubstring {
         return res;
     }
 
+    public static boolean isNice2(char[] chars, int l, int r) {
+        Set<Character> lowerSet = new HashSet<>(), upperSet = new HashSet<>();
+
+        for (int i = l; i <= r; i++) {            // 遍历 substring 的每一个字符
+            if (Character.isLowerCase(chars[i]))  // 若是小写，放入小写 Set 中
+                lowerSet.add(chars[i]);
+            else                                  // 若是大写，先转换成小写后放入大写 Set 中
+                upperSet.add(Character.toLowerCase(chars[i]));
+        }
+
+        return lowerSet.equals(upperSet);
+    }
+
     public static void main(String[] args) {
         log(longestNiceSubstring("YazaAay"));      // expects "aAa"
         log(longestNiceSubstring("deDEE"));        // expects "deDEE"
         log(longestNiceSubstring("Bb"));           // expects "Bb"
-        log(longestNiceSubstring("c"));            // expects "-1"
-        log(longestNiceSubstring("abc"));          // expects "-1"
+        log(longestNiceSubstring("c"));            // expects ""
+        log(longestNiceSubstring("abc"));          // expects ""
 
         log(shortestNiceSubstring("azABaabza"));   // expects "ABaab"
         log(shortestNiceSubstring("CATattac"));    // expects "ATat"
         log(shortestNiceSubstring("CATattaA"));    // expects "aA"
-        log(shortestNiceSubstring("TacoCat"));     // expects "-1"
-        log(shortestNiceSubstring("Madam"));       // expects "-1"
+        log(shortestNiceSubstring("TacoCat"));     // expects ""
+        log(shortestNiceSubstring("Madam"));       // expects ""
         log(shortestNiceSubstring("AcZCbaBz"));    // expects "AcZCbaBz"
         log(shortestNiceSubstring("aZABcabbCa"));  // expects "ABcabbC"
     }
