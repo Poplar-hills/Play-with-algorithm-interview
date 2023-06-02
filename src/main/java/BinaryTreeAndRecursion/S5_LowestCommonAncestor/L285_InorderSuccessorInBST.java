57a8e9abcb7b89dd0aee8970c472428a3a4aef1b
max.jiang <max.jiang@disney.com>
2023-06-02 16:47:40 +0800

    feat: L285 add solution

--- src/main/java/BinaryTreeAndRecursion/S5_LowestCommonAncestor/L285_InorderSuccessorInBST.java
+++ src/main/java/BinaryTreeAndRecursion/S5_LowestCommonAncestor/L285_InorderSuccessorInBST.java
@@ -2,6 +2,9 @@ package BinaryTreeAndRecursion.S5_LowestCommonAncestor;
 
 import Utils.Helpers.TreeNode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static Utils.Helpers.*;
 
 /*
@@ -18,7 +21,36 @@ import static Utils.Helpers.*;
 
 public class L285_InorderSuccessorInBST {
     /*
-     * 解法1：迭代
+     * 解法1：利用 BST 中序遍历的顺序性
+     * - 思路：先收集所有节点，然后再遍历查找 p 的下一个节点。
+     * - 时间复杂度 O(n)，空间复杂度 O(logn)，在 BST 不平衡的情况下，最坏会退化成 O(n)。
+     * */
+    private static TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        // 1. Collect nodes via inorder traversal
+        List<TreeNode> list = new ArrayList<>();
+        dfs(root, p, list);
+
+        // 2. Search for the next node of p
+        return binarySearchNext(list, p, 0, list.size() - 1);
+    }
+
+    private static void dfs(TreeNode root, TreeNode p, List<TreeNode> list) {
+        if (root == null) return;
+        dfs(root.left, p, list);
+        list.add(root);
+        dfs(root.right, p, list);
+    }
+
+    private static TreeNode binarySearchNext(List<TreeNode> list, TreeNode p, int l, int r) {
+        if (l > r) return null;
+        int mid = (r - l) / 2 + l;
+        if (p.val < list.get(mid).val) return binarySearchNext(list, p, l, mid - 1);
+        if (p.val > list.get(mid).val) return binarySearchNext(list, p,mid + 1, r);
+        return mid == list.size() - 1 ? null : list.get(mid + 1);  // 若 p 是 list 中的最后一个元素，则无 successor
+    }
+
+    /*
+     * 解法2：迭代
      * - 思路：先找到 successor 节点的规律：
      *   1. 若 p 有右子树，则其右子树上的最左节点就是 successor；
      *   2. 若 p 没有右子树，则：
@@ -27,7 +59,7 @@ public class L285_InorderSuccessorInBST {
      * - 时间复杂度 O(logn)，在 BST 不平衡的情况下，最坏会退化成 O(n)。
      * - 空间复杂度 O(1)。
      * */
-    private static TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+    private static TreeNode inorderSuccessor2(TreeNode root, TreeNode p) {
         if (root == null || p == null) return null;
         if (p.right != null) return getMin(p.right);  // 若 p 有右子树，则其右子树上的最左节点就是 successor
         TreeNode successor = null;
@@ -55,36 +87,22 @@ public class L285_InorderSuccessorInBST {
      * - 思路：？？
      * - 时间复杂度 O(logn)，空间复杂度 O(logn)，在 BST 不平衡的情况下，最坏会退化成 O(n)。
      * */
-    private static TreeNode inorderSuccessor2(TreeNode root, TreeNode p) {
+    private static TreeNode inorderSuccessor3(TreeNode root, TreeNode p) {
         if (root == null || p == null) return null;
         if (p.val < root.val) {
-            TreeNode successor = inorderSuccessor2(root.left, p);
+            TreeNode successor = inorderSuccessor3(root.left, p);
             return successor == null ? root : successor;
         }
-        return inorderSuccessor2(root.right, p);
-    }
-
-    /*
-     * 解法3：利用 BST 中序遍历的顺序性
-     * - 思路：
-     * - 时间复杂度 O(logn)，空间复杂度 O(logn)，在 BST 不平衡的情况下，最坏会退化成 O(n)。
-     * */
-    private static TreeNode inorderSuccessor3(TreeNode root, TreeNode p) {
-        return null;
+        return inorderSuccessor3(root.right, p);
     }
 
     public static void main(String[] args) {
         TreeNode t = createBinaryTreeBreadthFirst(new Integer[]{6, 3, 7, 2, 4, null, null, 1, null, null, 5});
-        TreeNode successor1 = inorderSuccessor2(t, t.get(2));
-        log(successor1);                                        // expects 3
-        TreeNode successor2 = inorderSuccessor2(t, t.get(4));
-        log(successor2);                                        // expects 5
-        TreeNode successor3 = inorderSuccessor2(t, t.get(5));
-        log(successor3);                                        // expects 6
-        TreeNode successor4 = inorderSuccessor2(t, t.get(6));
-        log(successor4);                                        // expects 7
-        TreeNode successor5 = inorderSuccessor2(t, t.get(7));
-        log(successor5);                                        // expects null. (无 successor 的情况)
+        log(inorderSuccessor(t, t.get(2)));  // expects 3
+        log(inorderSuccessor(t, t.get(4)));  // expects 5
+        log(inorderSuccessor(t, t.get(5)));  // expects 6
+        log(inorderSuccessor(t, t.get(6)));  // expects 7
+        log(inorderSuccessor(t, t.get(7)));  // expects null. (无 successor 的情况)
         /*
          *           6
          *         /   \
