415a05fd62f908e2583bf9ac0ee3b39d3e95b450
Max Jiang <maxjiang23326@gmail.com>
2023-06-08 21:19:24 +0800

    doc: L16 update comments

--- src/main/java/HashTable/S2_ClassicHashTable/L16_3SumClosest.java
+++ src/main/java/HashTable/S2_ClassicHashTable/L16_3SumClosest.java
@@ -34,9 +34,9 @@ public class L16_3SumClosest {
     /*
      * 解法2：转化为 2Sum（指针对撞）
      * - 💎 思路：这道题本质上与 L15_3Sum 是类似的，只是判断条件由等于 target 变为了最接近 target ∴ 同样可以用 3Sum 的思路
-     *   求解 —— 通过在遍历中挨个固定元素，将 3Sum 化简为 2Sum 问题。而一点问题变成了 2Sum，那么久可以考虑 2Sum 的三种经典
-     *   思路：1. 指针对撞；2. Binary Search；3. 查找表。但由于本题条件是最接近 target，而非等于 target ∴ 无法使用
-     *   Binary Search、查找表这样进行精确搜索的办法，只能使用指针对撞来对逼近 target，并沿途判断哪个是最接近的解。
+     *   求解 —— 通过在遍历中挨个固定元素，将 3Sum 化简为 2Sum 问题。而一但问题变成了 2Sum，就可以考虑 2Sum 的三种经典思路：
+     *   1. 指针对撞；2. 二分查找；3. 查找表。由于本题条件是最接近 target，而非等于 target ∴ 无法使用二分查找、查找表这类精确
+     *   搜索的方式，只能使用指针对撞来对逼近 target，并沿途判断哪个是最接近的解。
      * - 💎 经验：该题很好的说明了在什么场景下不适合使用查找表的题目 —— 查找表是用来进行精确匹配的，不适用于这种搜索 closest 的场景。
      * - 时间复杂度 O(n^2)，空间复杂度 O(1)。
      * */
@@ -44,7 +44,7 @@ public class L16_3SumClosest {
         if (nums == null || nums.length < 3) return -1;
 
         Arrays.sort(nums);
-        int closest = nums[0] + nums[1] + nums[2];
+        int closest = nums[0] + nums[1] + nums[2];  // 也可以初始化为 Integer.MAX_VALUE
         int n = nums.length;
 
         for (int i = 0; i < n - 2; i++) {  // 固定一个元素将问题化简为 2Sum，之后在 (i,..] 范围内做指针对撞
